13bb04fa0170e3c82bdc5b5227f3c8726923c178
stmexx <stmexx@mail.ru>
2020-03-11 15:49:57 +0300

    сделанный урок 1

--- Урок 1. Практическое задание/task_4.py
+++ Урок 1. Практическое задание/task_4.py
@@ -14,3 +14,35 @@
 Функцию random() использовать можно
 Опирайтесь на пример к уроку
 """
+
+from random import random
+
+try:
+    numbers = input('Введите два целых числа через пробел:')
+    n1, n2 = numbers.split(' ')
+    n1 = int(n1)
+    n2 = int(n2)
+    rand_numb = int(random() * (n2 - n1)) + n1
+    print(rand_numb)
+except ValueError:
+    print('Введите корректные ЧИСЛА')
+
+try:
+    numbers = input('Введите нижнюю и верхнюю границу через пробел:')
+    n1, n2 = numbers.split(' ')
+    n1 = float(n1)
+    n2 = float(n2)
+    rand_numb = random() * (n2 - n1) + n1
+    print(round(rand_numb, 3))
+except ValueError:
+    print('Введите ЧИСЛА')
+
+try:
+    letters = input('Введите две разные буквы через пробел:')
+    n1, n2 = letters.split(' ')
+    n1 = ord(n1)
+    n2 = ord(n2)
+    rand_letter = int(random() * (n2 - n1 + 1)) + n1
+    print(chr(rand_letter))
+except BaseException:
+    print('Введите ДВЕ БУКВЫ')
