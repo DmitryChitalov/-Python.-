409d904c6898867d7467ea9c0f6464550ffee4c5
Sin93 <zinovev.maxim93@mail.ru>
2020-03-14 18:45:08 +0300

    Дополнение - пятая задача

--- Урок 1. Практическое задание/task_5.py
+++ Урок 1. Практическое задание/task_5.py
@@ -11,4 +11,27 @@
 В обоих случаях он 24, но никак не -24
 """
 
-# До начала занятия доделаю, немного не успел(
+LATIN_ALPHABET = 96
+FIRST_LETTER = input("Введите первый символ латинского алфавита: ")
+SECOND_LETTER = input("Введите второй символ латинского алфавита: ")
+# Проверка на количество букв, которые введены (не должно быть больше 1)
+if len(FIRST_LETTER) != 1 or len(SECOND_LETTER) != 1:
+    print('Необходимо вводить один символ!')
+# Проверка, что введённые символы входят в латинский алфавит
+elif ord(FIRST_LETTER) > 122 or ord(FIRST_LETTER) < 97 \
+        or ord(SECOND_LETTER) > 122 or ord(SECOND_LETTER) < 97:
+    print('Необходимо вводить символы латинского алфавита!')
+else:
+    # Приводим введённые буквы к их номеру в Юникоде
+    FIRST_LETTER = ord(FIRST_LETTER)
+    SECOND_LETTER = ord(SECOND_LETTER)
+    # меняем местами буквы, если левая пограничная буква больше правой
+    if FIRST_LETTER > SECOND_LETTER:
+        FIRST_LETTER, SECOND_LETTER = SECOND_LETTER, FIRST_LETTER
+        print(f'''Место в алфавите первой буквы: {SECOND_LETTER - LATIN_ALPHABET}
+        Место в алфавите второй буквы {FIRST_LETTER - LATIN_ALPHABET}
+        Между этими буквами {FIRST_LETTER - SECOND_LETTER - 1}''')
+    else:
+        print(f'''Место в алфавите первой буквы: {FIRST_LETTER - LATIN_ALPHABET}
+Место в алфавите второй буквы {SECOND_LETTER - LATIN_ALPHABET}
+Между этими буквами {SECOND_LETTER - FIRST_LETTER - 1}''')
