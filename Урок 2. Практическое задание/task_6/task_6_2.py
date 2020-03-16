78818d105c5636cdd209d8c89f4f7cbd612a1fab
Kirill Ivanov <kirusha1991@gmail.com>
2020-03-16 23:44:33 +0500

    Тестовое рекурсивное решение

--- Урок 2. Практическое задание/task_6/task_6_2.py
+++ Урок 2. Практическое задание/task_6/task_6_2.py
@@ -7,3 +7,25 @@
 
 ЗДЕСЬ ДОЛЖНА БЫТЬ РЕАЛИЗАЦИЯ ЧЕРЕЗ РЕКУРСИЮ
 """
+from random import randint
+
+
+ANSWER = randint(1, 100)
+
+
+def recur_count(i, user_answer):
+    if i == 0:
+        print("Попытки кончились!")
+    if user_answer == ANSWER:
+        print("Вы угадали!")
+    if user_answer != ANSWER:
+        if user_answer > ANSWER:
+            print("Меньше!")
+        else:
+            print("Больше!")
+        i = i - 1
+        user_answer = int(input("Введите число: "))
+        return recur_count(i, user_answer)
+
+
+recur_count(10, 50)
