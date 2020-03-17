6175bda78fb7f41e4368ecaaee6b40c827f4014c
megokahn <kah2008@mail.ru>
2020-03-17 22:10:00 +0300

    сделан цикл

--- Урок 2. Практическое задание/task_6/task_6_1.py
+++ Урок 2. Практическое задание/task_6/task_6_1.py
@@ -7,3 +7,25 @@
 
 ЗДЕСЬ ДОЛЖНА БЫТЬ РЕАЛИЗАЦИЯ ЧЕРЕЗ ЦИКЛ
 """
+
+import random
+numbergame = int(random.randint(1,100))
+attempt = 10
+numberusers = int(input("я загадал число от 1 до 100 попробуйте отгадать, у вас 10 попыток--"))
+while attempt > 1:
+    if numberusers == numbergame:
+        print("молодец отгада, число было загадано число", numbergame)
+        break
+    elif numberusers > numbergame:
+        attempt -= 1
+        print("ты ввел больше загаданного числа")
+        numberusers = int(input(f"попробуй еще раз, у вас {attempt} попыток--"))
+    elif numberusers < numbergame:
+        attempt -= 1
+        print("ты ввел меньше загаданного числа")
+        numberusers = int(input(f"попробуй еще раз, у вас {attempt} попыток--"))
+if numberusers == numbergame:
+    print("конец игре")
+else:
+    print("попытки кончились, загаданно число ", numbergame)
+
