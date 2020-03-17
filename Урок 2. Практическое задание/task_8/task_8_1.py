9585cc8cc7636b33e87cdb6b667b2e8653be1bf9
megokahn <kah2008@mail.ru>
2020-03-17 22:09:54 +0300

    сделан цикл

--- Урок 2. Практическое задание/task_8/task_8_1.py
+++ Урок 2. Практическое задание/task_8/task_8_1.py
@@ -12,3 +12,13 @@
 
 ЗДЕСЬ ДОЛЖНА БЫТЬ РЕАЛИЗАЦИЯ ЧЕРЕЗ ЦИКЛ
 """
+
+NUMBERUSER = int(input("Сколько будет чисел?"))
+NUMBERUSER1 = input("Какую цифру считать?")
+SISTEMCOUNT = 0
+NUMBERCOUNT = 0
+while SISTEMCOUNT < NUMBERUSER:
+    NUMBERUSERI = input("Введите число--")
+    NUMBERCOUNT += NUMBERUSERI.count(NUMBERUSER1)
+    SISTEMCOUNT += 1
+print(f'Было введено {NUMBERCOUNT} цифр {NUMBERUSER1}')
