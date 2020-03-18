fc2e309870e18c3c9955efc49670d64114c97bf9
megokahn <kah2008@mail.ru>
2020-03-18 22:04:54 +0300

    выполнено

--- Урок 2. Практическое задание/task_9/task_9_1.py
+++ Урок 2. Практическое задание/task_9/task_9_1.py
@@ -10,3 +10,24 @@
 
 ЗДЕСЬ ДОЛЖНА БЫТЬ РЕАЛИЗАЦИЯ ЧЕРЕЗ ЦИКЛ
 """
+
+NUMBERUSER = int(input("Сколько будет чисел?"))
+SISTEMCOUNT = 0
+NUMBERCOUNT = 0
+DIVIDERNUM = 0
+ASER = 0
+LOTNUMBER = 0
+WHATNUMBER = 0
+while SISTEMCOUNT < NUMBERUSER:
+    NUMBERUSERINPUT = str(input("Введите очередное число: ?"))
+    SISTEMCOUNT += 1
+    for i in str(NUMBERUSERINPUT):
+        NUMBERCOUNT += int(NUMBERUSERINPUT[ASER])
+        ASER += 1
+    if NUMBERCOUNT >= DIVIDERNUM:
+        DIVIDERNUM = NUMBERCOUNT
+        LOTNUMBER = NUMBERUSERINPUT
+        WHATNUMBER += 1
+    ASER = 0
+    NUMBERCOUNT = 0
+print(f'Cумма цифра равна {DIVIDERNUM} у числа {LOTNUMBER} и оно большее из введеных')
