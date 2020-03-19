52b0f6803ef8ad84bec211a5bbf35c181fa74754
Iuliia-Fedkovych <Julia_Fedkovich@mail.ru>
2020-03-19 22:21:14 +0300

    task_4_2

--- Урок 2. Практическое задание/task_4/task_4_2.py
+++ Урок 2. Практическое задание/task_4/task_4_2.py
@@ -8,3 +8,24 @@
 
 ЗДЕСЬ ДОЛЖНА БЫТЬ РЕАЛИЗАЦИЯ ЧЕРЕЗ РЕКУРСИЮ
 """
+
+
+def row_sum(k, s, m):
+    if k > 1:
+        m = - m / 2
+        s = s + m
+        k -= 1
+        return row_sum(k, s, m)
+    else:
+        return s
+
+
+while True:
+    try:
+        K = int(input('Введите колличество элементов: '))
+        break
+    except ValueError:
+        print('Некорректный ввод. Введите целое число')
+S = 1
+M = 1
+print(f'сумма элементов: {row_sum(K, S, M)}')
