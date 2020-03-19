13f1418cef4346d30707305149300e8f48a1c6c7
Iuliia-Fedkovych <Julia_Fedkovich@mail.ru>
2020-03-19 22:20:46 +0300

    task_3_2

--- Урок 2. Практическое задание/task_3/task_3_2.py
+++ Урок 2. Практическое задание/task_3/task_3_2.py
@@ -14,3 +14,26 @@
 
 ЗДЕСЬ ДОЛЖНА БЫТЬ РЕАЛИЗАЦИЯ ЧЕРЕЗ РЕКУРСИЮ
 """
+
+
+def revertion(n, n2):
+    if n // 10 != 0:
+        k = n % 10
+        n = n // 10
+        n2 = n2 * 10 + k
+        return revertion(n, n2)
+    else:
+        n2 = n2 * 10 + n
+        return n2
+
+
+while True:
+    try:
+        N1 = int(input('Введите число: '))
+        break
+    except ValueError:
+        print('Некорректный ввод. Введите целое число')
+
+N2 = 0
+
+print(f'перевернутое число: {revertion(N1, N2)}')
