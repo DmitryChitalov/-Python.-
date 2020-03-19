34c6e4c3cbaa50d17edc14166dbede2f86414bbb
antoonponomarenko <antoon.ponomarenko@yandex.ru>
2020-03-19 15:27:21 +0300

    Решение задачи 5

--- Урок 1. Практическое задание/task_5.py
+++ Урок 1. Практическое задание/task_5.py
@@ -10,3 +10,20 @@
 В обоих случаях программа должна вывести корректный результат.
 В обоих случаях он 24, но никак не -24
 """
+
+try:
+    ER_STRING = 'Ошибка ввода!'
+    CODE_A = 97
+    CODE_Z = 122
+
+    first_letter_code = ord(str(input('Введите первую букву: ')).lower())
+    second_letter_code = ord(str(input('Введите вторую букву: ')).lower())
+
+    if first_letter_code < CODE_A or first_letter_code > CODE_Z  or second_letter_code < CODE_A or second_letter_code > CODE_Z :
+        print(ER_STRING)
+    elif first_letter_code == second_letter_code:
+        print('Буквы одинаковые')
+    else:
+        print('Букв между введенным буквами : %d ' %(abs(first_letter_code - second_letter_code)-1))
+except:
+    print(ER_STRING)
