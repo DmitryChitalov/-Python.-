8a5932523fd59e9c8d4793cc7c5164111fcf413c
Vladislav Vovk <vladyslavvovk@yandex.ru>
2020-03-21 02:03:32 +0300

    task_9_1 Данное задание сделал только циклом.Рекурсию докрутить не успел.

--- Урок 2. Практическое задание/task_9/task_9_1.py
+++ Урок 2. Практическое задание/task_9/task_9_1.py
@@ -10,3 +10,28 @@
 
 ЗДЕСЬ ДОЛЖНА БЫТЬ РЕАЛИЗАЦИЯ ЧЕРЕЗ ЦИКЛ
 """
+try:
+    NUMBER = int(input('Введите количество чисел:'))
+    if NUMBER > 0:
+        MAX_SUMM = 0
+        MAX_NUMBER = 0
+        COUNT = 0
+        while NUMBER != COUNT:
+            NEXT_NUMBER = int(input('Введите очередное число:'))
+            if NEXT_NUMBER >= 0:
+                MAX_NEXT_NUMBER = NEXT_NUMBER
+                SUMM_NUMBER = 0
+                COUNT += 1
+                while NEXT_NUMBER > 0:
+                    SUMM_NUMBER += NEXT_NUMBER %10
+                    NEXT_NUMBER //= 10
+                if SUMM_NUMBER > MAX_SUMM:
+                    MAX_SUMM = SUMM_NUMBER
+                    MAX_NUMBER = MAX_NEXT_NUMBER
+            else:
+                raise ValueError
+        print(f'Наибольшее число по сумме цифр : {MAX_NUMBER}, сумма цифр {MAX_SUMM}')
+    else:
+        raise ValueError
+except ValueError:
+    print('Некорректное значение.Введите натуральное число')
