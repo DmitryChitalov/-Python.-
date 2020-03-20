292ee9310002a5c079728ed6dff88e1abd4fbc7b
Vladislav Vovk <vladyslavvovk@yandex.ru>
2020-03-21 01:56:13 +0300

    task_2_1

--- Урок 2. Практическое задание/task_2/task_2_1.py
+++ Урок 2. Практическое задание/task_2/task_2_1.py
@@ -12,3 +12,24 @@
 
 ЗДЕСЬ ДОЛЖНА БЫТЬ РЕАЛИЗАЦИЯ ЧЕРЕЗ ЦИКЛ
 """
+try:
+    NUMBERS = int(input('Введите число : '))
+    if NUMBERS > 0:
+        EVEN_SCORE = 0
+        NOT_EVEN_SCORE = 0
+        COUNT_OF_NUMBERS = 0
+        for el in str(NUMBERS):
+            COUNT_OF_NUMBERS += 1
+            if int(el) % 2 == 0:
+                EVEN_SCORE += 1
+            else:
+                NOT_EVEN_SCORE += 1
+        print(f'В числе {NUMBERS} всего {COUNT_OF_NUMBERS} цифр ,'
+              f'из которых четных цифр : {EVEN_SCORE} и нечетных цифр : {NOT_EVEN_SCORE}')
+    else:
+        raise IOError
+except ValueError:
+    print('Некорректное значение.Введите натуральное число.')
+except IOError:
+    print('Натуральное числа начинаются с 1 ')
+
