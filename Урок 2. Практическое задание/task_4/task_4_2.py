fe22a083a71cd8973d3526e5f08754d15ea027e5
Vladislav Vovk <vladyslavvovk@yandex.ru>
2020-03-21 01:57:02 +0300

    task_4_2

--- Урок 2. Практическое задание/task_4/task_4_2.py
+++ Урок 2. Практическое задание/task_4/task_4_2.py
@@ -8,3 +8,19 @@
 
 ЗДЕСЬ ДОЛЖНА БЫТЬ РЕАЛИЗАЦИЯ ЧЕРЕЗ РЕКУРСИЮ
 """
+def find_numbers(NUMBER, count):
+    try:
+        if int(NUMBER) > 0:
+            if count != int(NUMBER):
+                k, summ = 1., 0.
+                for el in range(int(NUMBER)):
+                    summ += k
+                    k /= -2
+                return summ
+            return find_numbers(int(NUMBER), count +1)
+        raise ValueError
+    except ValueError:
+        return 'Некорректное значение. Введите натуральное число.'
+
+NUMBER = input('Введите натуральное число : ')
+print(find_numbers(NUMBER, count=0))
