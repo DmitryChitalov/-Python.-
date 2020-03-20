465a06d6bf3663a585e6b2a3cd79661c3097857a
Vladislav Vovk <vladyslavvovk@yandex.ru>
2020-03-21 02:00:01 +0300

    task_6_2

--- Урок 2. Практическое задание/task_6/task_6_2.py
+++ Урок 2. Практическое задание/task_6/task_6_2.py
@@ -7,3 +7,27 @@
 
 ЗДЕСЬ ДОЛЖНА БЫТЬ РЕАЛИЗАЦИЯ ЧЕРЕЗ РЕКУРСИЮ
 """
+from random import random
+
+def random_number(NUMBER, TRY_COUNT):
+    if TRY_COUNT <= 10:
+        try:
+            USER_NUMBER = int(input('Угадайте число от 0 до 100 : '))
+            if USER_NUMBER >= 0 and USER_NUMBER <= 100:
+                if USER_NUMBER < NUMBER:
+                    print(f'Вы ввели слишком маленькое число.У вас осталось {10 - TRY_COUNT} попыток')
+                    return random_number(NUMBER, TRY_COUNT +1)
+                if USER_NUMBER > NUMBER:
+                    print(f'Вы ввели слишком большое число.У вас осталось {10 - TRY_COUNT} попыток')
+                    return random_number(NUMBER, TRY_COUNT +1)
+                print(f'Поздравляю. Вы угадали за {TRY_COUNT} попыток.')
+            else:
+                raise ValueError
+        except ValueError:
+            print('Некорректное значение.Введите целое число от 0 до 100.')
+            return random_number(NUMBER, TRY_COUNT)
+    else:
+        print(f'Увы, количество попыток Вы исчерпали.Правильный ответ : {NUMBER}')
+
+
+random_number(NUMBER=round(random() * 100), TRY_COUNT=1)
