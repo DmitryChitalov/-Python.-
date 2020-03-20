0796614656f1afb70bc186b398670e3d9fc45b9e
dmitriikudrin <39126785+dmitriikudrin@users.noreply.github.com>
2020-03-20 20:08:14 +0700

    task 3_2 - 8_2

--- Урок 2. Практическое задание/task_4/task_4_2.py
+++ Урок 2. Практическое задание/task_4/task_4_2.py
@@ -8,3 +8,31 @@
 
 ЗДЕСЬ ДОЛЖНА БЫТЬ РЕАЛИЗАЦИЯ ЧЕРЕЗ РЕКУРСИЮ
 """
+
+
+def number_of_elements():
+    number = input("Enter the number of items: ")
+    if number.isdigit():
+        return int(number)
+    else:
+        print("Invalid number. Repeat entry.")
+        return number_of_elements()
+
+
+def sum_of_elements(number, count=0, sum_el=0):
+    if count == number:
+        return sum_el
+    if count % 2 == 0:
+        sum_el += 1 / pow(2, count)
+        count += 1
+        return sum_of_elements(number, count, sum_el)
+    else:
+        sum_el -= 1 / pow(2, count)
+        count += 1
+        return sum_of_elements(number, count, sum_el)
+
+
+NUMBER = number_of_elements()
+
+print(f"Number of elements - {NUMBER}, their sum - {sum_of_elements(NUMBER)}")
+
