b4a775f24a931d3e4a726274dcc6f3039a5ccde9
Sin93 <zinovev.maxim93@mail.ru>
2020-03-21 20:33:12 +0300

    Задания к уроку 3

--- Урок 3.Практическое задание/task_6.py
+++ Урок 3.Практическое задание/task_6.py
@@ -13,3 +13,14 @@
 Массив: [88, 58, 50, 77, 49, 6, 42, 67, 14, 79]
 Сумма элементов между минимальным (6)  и максимальным (88) элементами: 234
 """
+from random import randint
+
+COUNT = int(input('Введите количество элементов списка: '))
+LIST = [randint(0, 100) for i in range(COUNT)]
+print(LIST)
+MIN_VAL_IND = LIST.index(min(LIST))
+MAX_VAL_IND = LIST.index(max(LIST))
+if MIN_VAL_IND > MAX_VAL_IND:
+    MIN_VAL_IND, MAX_VAL_IND = MAX_VAL_IND, MIN_VAL_IND
+
+print(sum(LIST[MIN_VAL_IND+1:MAX_VAL_IND]))
