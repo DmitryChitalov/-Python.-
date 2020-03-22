9977df340751dd033eded9099d11befe22a959be
Artem Potashov <artem.potashov@bk.ru>
2020-03-22 19:03:56 +0300

    Реализация рекурсией

--- Lesson2.Practice/task_9/task_9_2.py
+++ Lesson2.Practice/task_9/task_9_2.py
@@ -11,3 +11,42 @@
 ЗДЕСЬ ДОЛЖНА БЫТЬ РЕАЛИЗАЦИЯ ЧЕРЕЗ РЕКУРСИЮ
 """
 
+
+def get_sum(num, max_sum):
+    """
+    Вычисление суммы цифр числа
+    """
+    if num == 0:
+        return max_sum
+    max_sum = max_sum + num % 10
+    return get_sum(num // 10, max_sum)
+
+
+def get_max(count_nums, max_sum=0, max_num=0):
+    """
+    Получение числа с большей суммой цифр
+    """
+    if count_nums <= 0:
+        return max_num, max_sum
+    try:
+        user_num = abs(int(input("Введите число: ")))
+        digit = get_sum(user_num, 0)
+        if digit > max_sum:
+            max_sum = digit
+            max_num = user_num
+        return get_max(count_nums - 1, max_sum, max_num)
+    except ValueError:
+        print("Ошибка ввода значения")
+
+
+if __name__ == "__main__":
+    try:
+        COUNT_NUMS = int(input("Введите количество чисел: "))
+        if COUNT_NUMS < 0:
+            print("Количество чисел должно быть неотрицательным")
+        else:
+            MAX_NUM, MAX_SUM = get_max(COUNT_NUMS)
+            print(
+                f'Наибольшее число по сумме цифр: {MAX_NUM}, сумма его цифр: {MAX_SUM}')
+    except ValueError:
+        print("Ошибка ввода значения")
