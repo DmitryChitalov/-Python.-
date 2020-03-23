dc46d0a5e434665e8df83fe97fa726412ed6df95
Iuliia-Fedkovych <Julia_Fedkovich@mail.ru>
2020-03-23 17:14:32 +0300

    task_9

--- Урок 3.Практическое задание/task_9.py
+++ Урок 3.Практическое задание/task_9.py
@@ -1,3 +1,5 @@
+import random
+
 """
 Задание_9.Найти максимальный элемент среди минимальных элементов столбцов матрицы.
 
@@ -11,3 +13,19 @@
 [13, 12, 7, 15] минимальные значения по столбцам
 Максимальное среди них = 15
 """
+n = int(input('Введите колличество строк в масиве: '))
+m = int(input('Введите колличество столбцов в масиве: '))
+a = []
+for i in range(0, n):
+    a.append([random.randint(0, 100) for j in range(0, m)])
+    print(a[i])
+min_col = []
+for j in range(0, m):
+    min_c = a[0][j]
+    for i in range(1, n):
+        if a[i][j] < min_c:
+            min_c = a[i][j]
+    min_col.append(min_c)
+max_min_col = max(min_col)
+min_col.append(max_min_col)
+print(min_col)
