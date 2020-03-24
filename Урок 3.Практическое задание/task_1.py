acb2feef774ca1568ff2735877385f9337971615
megokahn <kah2008@mail.ru>
2020-03-24 21:34:06 +0300

    сделано

--- Урок 3.Практическое задание/task_1.py
+++ Урок 3.Практическое задание/task_1.py
@@ -13,4 +13,39 @@
 В диапазоне 2-99: 14 чисел кратны 7
 В диапазоне 2-99: 12 чисел кратны 8
 В диапазоне 2-99: 11 чисел кратны 9
+
+
+
+
+
+def devuser(n):
+    usernum = n
+    counter = 0
+    for i in range(2,100):
+        if i % usernum == 0:
+            counter += 1
+        if i >= 99:
+            print(f'В диапазоне 2-99: {counter} чисел кратны {n}')
+
+
+devuser(2)
+devuser(3)
+devuser(4)
+devuser(5)
+devuser(6)
+devuser(7)
+devuser(8)
+devuser(9)
+
 """
+
+
+usernum1 = 2
+counter1 = 0
+for i in range(usernum1, 10):
+    for u in range(2,100):
+        if u % i == 0:
+            counter1 += 1
+        if u >= 99:
+            print(f'В диапазоне 2-99: {counter1} чисел кратны {i}')
+            counter1 = 0
