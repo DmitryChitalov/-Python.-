83535df591d730bf2161a757382c95b993a278ee
megokahn <kah2008@mail.ru>
2020-03-24 21:34:32 +0300

    сделано

--- Урок 3.Практическое задание/task_3.py
+++ Урок 3.Практическое задание/task_3.py
@@ -11,3 +11,24 @@
 6 позиции, а минимальное число  -49 стоит на    0 позиции
 [-49, 26, 41, 75, 23, 52, 88, 60, 69, -18]
 """
+
+
+from random import random
+N = 10
+arr = [0]*N
+even = []
+amin = 0
+amax = 0
+for i in range(N):
+    arr[i] = int(random() * 35)
+print('Исходный массив: ', arr)
+
+amin = min(arr)
+amax = max(arr)
+aminindex = arr.index(amin)
+amaxindex = arr.index(amax)
+arr.insert(amaxindex, amin)
+arr.pop(amaxindex + 1)
+arr.insert(aminindex, amax)
+arr.pop(aminindex + 1)
+print('Получился:       ', arr)
