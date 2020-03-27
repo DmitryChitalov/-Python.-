04a67cb2574353192b1db5b40e4bfa8882ab60e6
TerryNight <lizkakryuchkova@gmail.com>
2020-03-27 15:15:40 +0300

    сделано

--- Урок 2. Практическое задание/task_9/task_9_2.py
+++ Урок 2. Практическое задание/task_9/task_9_2.py
@@ -10,3 +10,29 @@
 
 ЗДЕСЬ ДОЛЖНА БЫТЬ РЕАЛИЗАЦИЯ ЧЕРЕЗ РЕКУРСИЮ
 """
+def fun(a, steps, high, max):
+    sum = 0
+    try:
+        number = input("Введите число: ")
+        for i in number:
+            sum += int(i)
+        if sum > max:
+            max = sum
+            high = number
+        steps += 1
+        if steps == a:
+            return f"Наибольшее число по сумме цифр: {high}, сумма его цифр: {max}"
+        else:
+            return fun(a, steps, high, max)
+    except ValueError:
+        print("Вы ввели некорректные данные")
+
+STEPS = 0
+HIGH = 0
+MAX = 0
+
+try:
+    A = int(input("Введите количество чисел: "))
+    print(fun(A, STEPS, HIGH, MAX))
+except ValueError:
+    print("Вы ввели некорректные данные")
