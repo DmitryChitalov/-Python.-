3813dd78ba50ac2e2c6427ae4869da44dd208769
dmitriikudrin <dskudr@gmail.com>
2020-03-28 12:04:53 +0700

    complete lesson 3

--- Урок 3.Практическое задание/task_9.py
+++ Урок 3.Практическое задание/task_9.py
@@ -11,3 +11,37 @@
 [13, 12, 7, 15] минимальные значения по столбцам
 Максимальное среди них = 15
 """
+
+from random import randint
+
+
+def num_enter(message=""):
+    while True:
+        num = input(message)
+        if num.isdigit():
+            return int(num)
+        else:
+            print("Invalid number. Repeat entry.")
+
+
+row_count = num_enter("Задайте количество строк в матрице: ")
+column_count = num_enter("Задайте количество столбцов в матрице: ")
+
+matrix = [[randint(0, 100) for j in range(0, column_count)] for i in range(0, row_count)]
+
+min_el = []
+
+for j in range(0, column_count):
+    temp_list = []
+    for i in range(0, row_count):
+        temp_list.append(matrix[i][j])
+    min_el.append(min(temp_list))
+
+print("Исходная матрица:")
+for row in matrix:
+    print(row)
+
+print("Минимальные значения по столбцам:")
+print(min_el)
+
+print(f"Максимальное среди них - {max(min_el)}")
