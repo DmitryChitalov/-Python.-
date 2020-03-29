28b3b895755dc4a78b51c1ff0c3462b647f82ce2
megokahn <kah2008@mail.ru>
2020-03-29 14:04:01 +0300

    готово

--- Урок 3.Практическое задание/task_8.py
+++ Урок 3.Практическое задание/task_8.py
@@ -36,3 +36,49 @@
 [3, 3, 3, 3, 12]
 [3, 3, 3, 3, 12]
 """
+sumnum1 = 0
+sumnum2 = 0
+sumnum3 = 0
+sumnum4 = 0
+sumnum5 = 0
+b1 = []
+b2 = []
+b3 = []
+b4 = []
+b5 = []
+a1 = input("введите числа 1 строки через пробел ").split()
+a2 = input("введите числа 2 строки через пробел ").split()
+a3 = input("введите числа 3 строки через пробел ").split()
+a4 = input("введите числа 4 строки через пробел ").split()
+a5 = input("введите числа 5 строки через пробел ").split()
+
+for i in a1:
+    sumnum1 += int(i)
+    b1.append(int(i))
+b1.append(sumnum1)
+
+for e in a2:
+    sumnum2 += int(e)
+    b2.append(int(e))
+b2.append(sumnum2)
+
+for k in a3:
+    sumnum3 += int(k)
+    b3.append(int(k))
+b3.append(sumnum3)
+
+for o in a4:
+    sumnum4 += int(o)
+    b4.append(int(o))
+b4.append(sumnum4)
+
+for u in a5:
+    sumnum5 += int(u)
+    b5.append(int(u))
+b5.append(sumnum5)
+
+print(b1)
+print(b2)
+print(b3)
+print(b4)
+print(b5)
