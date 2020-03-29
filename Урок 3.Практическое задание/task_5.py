3fc1ae868cc1ffff849e7ca5f0955a228537bf45
megokahn <kah2008@mail.ru>
2020-03-29 14:03:48 +0300

    готово

--- Урок 3.Практическое задание/task_5.py
+++ Урок 3.Практическое задание/task_5.py
@@ -8,3 +8,27 @@
 Базовый список: [-55, -69, -5, 72, -41, -58, -79, 58, 74, 1]
 Максимальный отрицательный элемент в данном массиве = -5, его индекс 2
 """
+
+
+from random import random
+N = 10
+arrplus = [0] * N
+arrminus = [0] * N
+arrsourse = []
+even = []
+amin = 0
+amax = 0
+for i in range(N):
+    arrplus[i] = int(random() * 35)
+for k in range(N):
+    arrminus[k] = int(random() * - 35)
+arr = arrminus + arrplus
+
+for h in range(len(arr)):
+    if arr[h] < 0:
+        arrsourse.append(arr[h])
+amax = max(arrsourse)
+amaxindex = arr.index(amax)
+print('Исходный массив: ', arr)
+print(f'максимальное число из отрицательный {amax} находиться на позиции {amaxindex}')
+
