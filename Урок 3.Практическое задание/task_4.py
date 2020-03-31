70f1a4f90a75b07288e822797240831617da4de7
TerryNight <lizkakryuchkova@gmail.com>
2020-03-31 17:46:50 +0300

    Тоже без помощи не вышло, не ясно было как использовать функцию max

--- Урок 3.Практическое задание/task_4.py
+++ Урок 3.Практическое задание/task_4.py
@@ -3,3 +3,15 @@
 
 Подсказка: можно применить ф-цию max с параметром key
 """
+import random
+
+def fun(mass):
+    print(f"{mass}")
+    # key параметром функции max() является функция, которая вычисляет ключ,
+    # который используется для определения того, как ранжировать элементы.
+    numb = max(mass, key=mass.count)
+    print(f"Число {numb} встречается {mass.count(numb)} раза")
+
+
+MASS = [random.randint(-100, 100) for i in range(50)]
+fun(MASS)
