97e8555aeb5cb4703074cb3c5bb7f052dc6b90e7
KurskiySergey <59256173+KurskiySergey@users.noreply.github.com>
2020-03-31 21:01:07 +0300

    Четвертое задание

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -12,3 +12,140 @@
 ВНИМАНИЕ: ЗАДАНИЯ, В КОТОРЫХ БУДУТ ГОЛЫЕ ЦИФРЫ ЗАМЕРОВ (БЕЗ АНАЛИТИКИ)
 БУДУТ ПРИНИМАТЬСЯ С ОЦЕНКОЙ УДОВЛЕТВОРИТЕЛЬНО
 """
+
+from timeit import Timer
+from random import randint
+
+"""
+Первая функция
+"""
+
+
+def task3_1(n):
+    # result = {i: 0 for i in range(2, 10)}
+    result = [0 for i in range(2, 10)]
+    for i in range(2, n):
+        for j in range(2, 10):
+            if i % j == 0:
+                # value = result.get(j) + 1
+                # result.update({i: value})
+                result[j - 2] += 1
+
+# task3_1 T(N) = const + N*const -> O(N)
+
+
+"""
+Производим замеры для исходной функции
+на различных масштабах
+сравниваем полученную О-нотацию
+с реальными данными
+"""
+N = 100
+with open("task3_1_opt.txt", "w", encoding="utf8") as file:
+    while N < 100:
+        t1 = Timer(f"task3_1({N})", "from __main__ import task3_1")
+        time = t1.timeit(number=1000)
+        file.write(f"{N}\t{time}\n")
+        del t1
+        N += 100
+
+# результаты, записанные в файле совпадают с исходной линейной оценкой
+# графическую интерпритацию можно увидеть в exel файле
+# в данной задаче можно оптимизировать способ генерации данных
+# Используя вместо словаря генератор списков так как в данном случае
+# Тратиться дополнительное время на поиск и замену данных
+# N = 4900	time_opt = 4,1084308  time_not_opt = 5.116492899999997
+
+
+"""
+Вторая функция.
+Здесь приведены несколько вариантов реализации
+для оценки наилучших результатов
++ две второстепенные функции генерации
+массивов которые не влияют на оценку времени
+исходной функции только на тип данных( список или словарь )
+"""
+
+
+def matrix_min(matrix):
+
+    # min_values = [el for el in matrix[0]]
+    # min_values = list(matrix[0])
+    min_values = list(matrix.get(0))
+    # min_values = matrix[0]
+    for string in matrix:
+        for i in range(len(string)):
+            if string[i] < min_values[i]:
+                min_values[i] = string[i]
+    return min_values
+
+
+def matrix_min_dict(matrix):
+
+    min_values = list(matrix[0])
+    for item in matrix.items():
+        for i in range(len(item)-1):
+            if item[i] < min_values[i]:
+                min_values[i] = item[i]
+    return min_values
+# matrix_min T(N) = N + K*N -> O(N^2) if K == N
+
+
+"""
+Наихудший вариант достигается при равенстве K и N 
+Тогда затраченное время будет расти пропорционально N^2
+Проверим результат на входных данных
+"""
+#  Генерация рандомной квадратной матрицы
+
+# Как словарь
+def new_matrix_dict(n):
+    # matrix = []
+    matrix = {i: [] for i in range(n)}
+    for key in matrix:
+        matrix[key] = [randint(0, 100) for i in range(n)]
+    return matrix
+
+# Как список списков
+def new_matrix(n):
+    matrix = []
+    for i in range(n):
+        matrix.append([])
+        for j in range(n):
+            matrix[i].append(randint(0, 100))
+    return matrix
+
+
+N = 1
+file_name = "matrix_min_dict.txt"
+with open(file_name, "w", encoding='utf8') as file:
+    while N < 100:
+        # tmp_matrix = new_matrix(N)
+        tmp_matrix = new_matrix_dict(N)
+        t1 = Timer(f"matrix_min_dict({tmp_matrix})", "from __main__ import matrix_min_dict")
+        # t1 = Timer(f"matrix_min({tmp_matrix})", "from __main__ import matrix_min")
+        time = t1.timeit(number=100)
+        file.write(f"{N}\t{time}\n")
+        N += 1
+
+"""
+Результаты совпадают с исхродным предположением квадратичной зависимости
+отклонения от общей тенденции связаны с разницой обработки условия сравнения
+Возможная оптимизация:
+1)вместо генератора использовать лист
+2)Использовать словарь для хранения матрицы
+Ухудшение оптимизации:
+1)Присвоение списка без генератора
+Все результаты см в файле exel или текстовые файлы
+У всех вариантов наблюдается квадратичная зависимость 
+Однако тенденция роста у всех разная 
+Это связано с более оптимизировнными способами генерации данных
+"""
+
+
+# Учитывая полученные данные, можно сделать вывод, что наиболее оптимизированный способ
+# Использование листа вместо генератора тогда как простое присвоение
+# Ухудшает производимость так же лучше использовать в качестве хранения словарь
+# так как он использует хеш-функции, что намного быстрее
+# N = 99	time_gen = 0,0531535	time_list = 0,0524669	time_eq = 0,1103767     time_dict_list = 0,0095262
+
--- Урок 4. Практическое задание/task_2.py
+++ Урок 4. Практическое задание/task_2.py
@@ -12,3 +12,90 @@
 ВНИМАНИЕ: ЗАДАНИЯ, В КОТОРЫХ БУДУТ ГОЛЫЕ ЦИФРЫ ЗАМЕРОВ (БЕЗ АНАЛИТИКИ)
 БУДУТ ПРИНИМАТЬСЯ С ОЦЕНКОЙ УДОВЛЕТВОРИТЕЛЬНО
 """
+
+import math
+from timeit import Timer
+
+"""
+в данной функции поскольку на каждое простое число p
+призодитя n/p итераций то =>
+в итоге оценку можно записать как 
+сумму по всем p из исходного множества n/p
+оценка скорости сводится к оценке суммы 1/p
+учитывая оценку p как n/ln(n) то ряд можно заменьть интергалом
+в итоге получаем оценку O(N*ln(lnN)) 
+при малых N можно аппроксимировать до линейной функции
+"""
+def eratosfen(n, p):
+
+    if p <= (n/math.log(n)) + 1:
+        index = 1
+        values = {i: True for i in range(2, n+1)}
+        for key in values:
+            if values[key]:
+                if index == p:
+                    return key
+                index += 1
+                k = 2
+                while key*k <= n:
+                    values[key*k] = False
+                    k += 1
+    return f"В данном диапазоне может быть примерно {n// math.log(n) + 1} простых чисел\n" \
+           f"Задайте больший диапазон"
+
+
+"""
+в данной функции, поскольку в худшем случае для
+простого числа идет обход по всему списку,
+то для двойного цикла общую сложность 
+можно оценить как O(N^2)
+"""
+def simple_check(n, p):
+
+    if p <= (n/math.log(n)) + 1:
+        index = -1
+        values = list(range(2, n))
+        simple = []
+        for el in values:
+            if index == p-1:
+                return simple[index]
+            check = True
+            for number in values:
+                if el % number == 0 and el != number:
+                    check = False
+                    break
+            if check:
+                simple.append(el)
+                index += 1
+    return f"В данном диапазоне может быть примерно {n // math.log(n) + 1} простых чисел\n" \
+           f"Задайте больший диапазон"
+
+
+"""
+Функция для проведения расчетов и оценки времени
+"""
+def time_check(start_n=10, stop_n=1000, step=10):
+    n = start_n
+    with open(f"simple.txt", "w", encoding='utf8') as file:
+        file.write("N\tP_Index\tEratosfen\tSimple_Check\n")
+        while n <= stop_n:
+            p = n // math.log(n) + 1
+            t1 = Timer(f"eratosfen({n}, {p})", f"from __main__ import eratosfen")
+            t2 = Timer(f"simple_check({n}, {p})", f"from __main__ import simple_check")
+            time1 = t1.timeit(number=100)
+            time2 = t2.timeit(number=100)
+            file.write(f"{n}\t{p}\t{time1}\t{time2}\n")
+            n += step
+
+
+time_check()
+# Результаты показывают, что на больших масштабах
+# Эффективнее пользоваться решетом Эратосфена
+# Поскольку работает быстрее обычного поиска
+# Но на малых масштабах это не существенно и
+# И можно пользоваться любым из меодов
+# Данные взяты изфайла simple.txt
+# Общие тенденции представлены в exel файле
+#   N = 10	P = 5	time_erat = 0,0003229	time_simple = 0,000412
+#   N = 100	P = 22	time_erat = 0,0026565	time_simple = 0,0123347
+#   N = 1000 P = 145 time_erat = 0,0403658	time_simple = 0,6435195
