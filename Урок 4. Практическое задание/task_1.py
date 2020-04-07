ce94d2cf9f9cd94bd58b2254550a8d092f5768e2
Mixiz <lavrikov.denis@gmail.com>
2020-04-07 14:34:29 +0500

    Задания к четвертому практическому занятию Дополнены вычисления для первого задания

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -20,6 +20,7 @@
 где n - любое натуральное число.
 ЗДЕСЬ ДОЛЖНА БЫТЬ РЕАЛИЗАЦИЯ ЧЕРЕЗ ЦИКЛ
 """
+import timeit
 
 
 # Ожидает на ввод число. Повторяет попытку ввода, пока не введено целое
@@ -37,13 +38,46 @@ def input_number():
     return num
 
 
-n = input_number()
-res_formula = n * (n + 1) // 2
-res = 0
-for i in range(1, n + 1):
-    res += i
-print(f'Результат суммирования {res}, результат формулы {res_formula}')
+def calc_formula(n):
+    return n * (n + 1) // 2
+
+
+def calc_cycle(n):
+    res = 0
+    for i in range(1, n + 1):
+        res += i
 
 
 # Первая часть вычисляет значение через формулу, сложность O(1)
 # Вторая часть вычисляет через линейный цикл, сложность O(n)
+print('calc_formula')
+print(timeit.timeit(setup="from __main__ import calc_formula", stmt="calc_formula(10)", number=1000))
+print(timeit.timeit(setup="from __main__ import calc_formula", stmt="calc_formula(100)", number=1000))
+print(timeit.timeit(setup="from __main__ import calc_formula", stmt="calc_formula(1000)", number=1000))
+print(timeit.timeit(setup="from __main__ import calc_formula", stmt="calc_formula(10000)", number=1000))
+print(timeit.timeit(setup="from __main__ import calc_formula", stmt="calc_formula(100000)", number=1000))
+print(timeit.timeit(setup="from __main__ import calc_formula", stmt="calc_formula(1000000)", number=1000))
+
+print('calc_cycle')
+print(timeit.timeit(setup="from __main__ import calc_cycle", stmt="calc_cycle(10)", number=1000))
+print(timeit.timeit(setup="from __main__ import calc_cycle", stmt="calc_cycle(100)", number=1000))
+print(timeit.timeit(setup="from __main__ import calc_cycle", stmt="calc_cycle(1000)", number=1000))
+print(timeit.timeit(setup="from __main__ import calc_cycle", stmt="calc_cycle(10000)", number=1000))
+print(timeit.timeit(setup="from __main__ import calc_cycle", stmt="calc_cycle(100000)", number=1000))
+print(timeit.timeit(setup="from __main__ import calc_cycle", stmt="calc_cycle(1000000)", number=1000))
+
+# Как видно из результата, по формуле высчитывается за одно и то же время
+# В то время как через цикл, время увеличивается пропорционально увеличению аргумента
+# calc_formula
+# 0.00010600000000000193
+# 0.00012859999999999955
+# 0.00013979999999999895
+# 0.00013979999999999895
+# 0.00017780000000000226
+# 0.0001708000000000022
+# calc_cycle
+# 0.0005381999999999991
+# 0.0034561
+# 0.04054939999999999
+# 0.5269569
+# 4.8001537
