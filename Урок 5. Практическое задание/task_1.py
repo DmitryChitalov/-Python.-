456cb0cf003f30062928633d834c3455bd844ea4
rashevskii <rashevskii@mail.ru>
2020-04-07 22:26:35 +0400

    Tasks for 5 lesson

--- Урок 5. Практическое задание/task_1.py
+++ Урок 5. Практическое задание/task_1.py
@@ -25,3 +25,29 @@
 
 Предприятия, с прибылью ниже среднего значения: Копыта
 """
+from collections import defaultdict
+COUNT_ORG = int(input('Введите количество предприятий для расчета прибыли: '))
+COUNTER = COUNT_ORG
+ORGANISATION = defaultdict(list)
+
+while COUNTER > 0:
+    NAME_ORG = input('Введите название предприятия: ')
+    PROFIT_ORG = input('через пробел введите прибыль данного '
+                       'предприятия за каждый квартал(Всего 4 квартала): ').split(' ')
+
+    for el in PROFIT_ORG:
+        ORGANISATION[NAME_ORG].append(int(el))
+    COUNTER -= 1
+
+NAMES_ORG = list(ORGANISATION.keys())
+SUM_OF_ORG = 0
+for el in NAMES_ORG:
+    ORGANISATION[el] = sum(ORGANISATION[el])
+    SUM_OF_ORG += ORGANISATION[el]
+AVG_PROFIT = SUM_OF_ORG / COUNT_ORG
+
+MAX_PROFIT = [el[0] for el in ORGANISATION.items() if AVG_PROFIT < el[1]]
+MIN_PROFIT = [el[0] for el in ORGANISATION.items() if AVG_PROFIT > el[1]]
+print(f'Средняя годовая прибыль всех предприятий: {AVG_PROFIT}')
+print(f'Предприятия, с прибылью выше среднего значения: {", ".join(MAX_PROFIT)}')
+print(f'Предприятия, с прибылью выше среднего значения: {", ".join(MIN_PROFIT)}')
--- Урок 5. Практическое задание/task_2.py
+++ Урок 5. Практическое задание/task_2.py
@@ -11,3 +11,47 @@
 Также попробуйте решить задачу вообще без collections и применить только ваши знания по ООП
 (в частности по перегрузке методов)
 """
+from collections import deque
+from itertools import islice
+
+FIRST_NUMBER = deque(list(input('Введите первое число в шестнадцатеричной системе счисления: ')))
+SECOND_NUMBER = deque(list(input('Введите второе число в шестнадцатеричной системе счисления: ')))
+
+LIST_OF_NUMBERS = [str(i) for i in range(10)] + ['A', 'B', 'C', 'D', 'E', 'F']
+
+if len(FIRST_NUMBER) > len(SECOND_NUMBER):
+    FIRST_NUMBER, SECOND_NUMBER = SECOND_NUMBER, FIRST_NUMBER
+
+SECOND_NUMBER.reverse()
+THIRD_NUMBER = deque(list())
+
+J = -1
+K = 0
+for i in SECOND_NUMBER:
+    one = LIST_OF_NUMBERS.index(i)
+    two = LIST_OF_NUMBERS.index(FIRST_NUMBER[J])
+    THIRD_NUMBER.append(LIST_OF_NUMBERS[(one + two + K) % 16])
+    if(one + two) >= 15:
+        K = 1
+    else:
+        K = 0
+    J -= 1
+    if J == -len(FIRST_NUMBER)-1:
+        break
+
+DIFFERENT = len(SECOND_NUMBER) - len(FIRST_NUMBER)
+SECOND_NUMBER.reverse()
+SLICE_DIFF = list(islice(SECOND_NUMBER, 0, DIFFERENT))
+if DIFFERENT:
+    for i in SLICE_DIFF:
+        THIRD_NUMBER.append(LIST_OF_NUMBERS[(LIST_OF_NUMBERS.index(i) + K) % 16])
+        if LIST_OF_NUMBERS.index(i) + 1 >= 15:
+            K = 1
+        else:
+            K = 0
+
+if K == 1:
+    THIRD_NUMBER.append('1')
+
+THIRD_NUMBER.reverse()
+print(list(THIRD_NUMBER))
