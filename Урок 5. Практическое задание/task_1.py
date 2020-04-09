2e2db1b8ebc00b5071663ca04f9cb83ef477eb10
pavelstef <p.stefanenko@gmail.com>
2020-04-09 23:14:36 +0300

    Практическое задание 5. Коллекции

--- Урок 5. Практическое задание/task_1.py
+++ Урок 5. Практическое задание/task_1.py
@@ -25,3 +25,63 @@
 
 Предприятия, с прибылью ниже среднего значения: Копыта
 """
+
+
+import sys
+from collections import defaultdict, namedtuple
+
+
+def avg(iterable) -> float:
+    """ Вычисление среднего значения """
+    return sum(iterable) / len(iterable)
+
+
+def print_result(result) -> None:
+    """ Печать результатов высислений """
+    print('Средняя годовая прибыль всех предприятий: ', result.general_avg)
+    print('Предприятия, с прибылью выше среднего значения: ',
+          ', '.join(result.companies_positive))
+    print('Предприятия, с прибылью ниже среднего значения: ',
+          ', '.join(result.companies_negative))
+
+
+def solution(companies_number: int):
+    """ Решение, используюшее defaultdict  и namedtuple одновременно """
+    companies_dict = defaultdict(float)
+    for _ in range(companies_number):
+        company_name = input('Введите название предприятия: ').strip()
+        profit = input('Введите прибыль данного предприятия за каждый'
+                       ' квартал через пробел (Всего 4 квартала): ').strip()
+        profit = profit.split(' ')
+        try:
+            profit = [float(x) for x in profit]
+        except ValueError:
+            print('Неверное зачение прибыли, выходим.')
+            sys.exit()
+
+        company_avg = avg(profit)
+        companies_dict[company_name] = company_avg
+
+    Result = namedtuple('Result', ['general_avg', 'companies_positive', 'companies_negative'])
+    result = Result(general_avg=avg(companies_dict.values()),
+                    companies_positive=[], companies_negative=[])
+
+    for key, value in companies_dict.items():
+        if value >= result.general_avg:
+            result.companies_positive.append(key)
+        else:
+            result.companies_negative.append(key)
+
+    return result
+
+
+if __name__ == "__main__":
+
+    try:
+        CORP_NUMBER = int(
+            input('Введите количество предприятий для расчета прибыли: ').strip())
+    except ValueError:
+        print('Неверное количество, выходим.')
+        sys.exit()
+
+    print_result(solution(CORP_NUMBER))
--- Урок 5. Практическое задание/task_2.py
+++ Урок 5. Практическое задание/task_2.py
@@ -11,3 +11,59 @@
 Также попробуйте решить задачу вообще без collections и применить только ваши знания по ООП
 (в частности по перегрузке методов)
 """
+
+
+from collections import deque, namedtuple
+
+
+def oop_solution(value_1, value_2) -> None:
+    """ ООП + стандартные преобразования, без коллекций """
+    class HexValue:
+        """ Класс hex чисел """
+        def __init__(self, val):
+            self.val = int(''.join(val), 16)
+
+        def __add__(self, other):
+            return hex(self.val + other.val)[2:]
+
+        def __mul__(self, other):
+            return hex(self.val * other.val)[2:]
+
+    val_1 = HexValue(value_1)
+    val_2 = HexValue(value_2)
+    print(f'ООП результат сложения: {(val_1 + val_2).upper()}')
+    print(f'ООП результат умножения: {(val_1 * val_2).upper()}')
+
+
+def collections_solution(data) -> None:
+    """ Стандартные преобразования + работа с коллекциями """
+
+    def to_string(item):
+        result = ''
+        for _ in range(len(item)):
+            result += item.popleft()
+        return result
+
+    val_1 = data.val_1
+    val_2 = data.val_2
+    val_1 = to_string(val_1)
+    val_2 = to_string(val_2)
+
+    result_sum = hex(int(val_1, 16) + int(val_2, 16))[2:]
+    result_mult = hex(int(val_1, 16) * int(val_2, 16))[2:]
+    print(f'Коллекции результат сложения: {result_sum.upper()}')
+    print(f'Коллекции результат умножения: {result_mult.upper()}')
+
+
+if __name__ == "__main__":
+    VAL_1 = (input('Ведите первое шестнадцатеричное число: ').strip())
+    VAL_2 = (input('Ведите второе шестнадцатеричное число: ').strip())
+    VAL_1 = deque(VAL_1)
+    VAL_2 = deque(VAL_2)
+    oop_solution(VAL_1, VAL_2)
+
+    # Упаковка в коллекцию
+    InputedDdata = namedtuple('InData', ['val_1', 'val_2'])
+    INPUTED_DATA = InputedDdata(val_1=VAL_1, val_2=VAL_2)
+
+    collections_solution(INPUTED_DATA)
