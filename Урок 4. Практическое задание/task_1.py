697cf6a2d95b4676ee1aaef724fdb3853bff8009
Kirill Ivanov <kirusha1991@gmail.com>
2020-04-09 20:53:08 +0500

    Решение 4-ого домашнего задания

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -12,3 +12,95 @@
 ВНИМАНИЕ: ЗАДАНИЯ, В КОТОРЫХ БУДУТ ГОЛЫЕ ЦИФРЫ ЗАМЕРОВ (БЕЗ АНАЛИТИКИ)
 БУДУТ ПРИНИМАТЬСЯ С ОЦЕНКОЙ УДОВЛЕТВОРИТЕЛЬНО
 """
+
+from timeit import Timer
+
+
+USER_RANGE = 10
+MY_ARRAY = [i for i in range(2, USER_RANGE)]
+
+def multiplicity():
+    for i in range(2, 10):
+        MULTIPLE = 0
+        for j in MY_ARRAY:
+            if j % i == 0:
+                MULTIPLE += 1
+
+"""
+Сложность текущего алгоритма - линейная, т.к. при увеличении входных данных, 
+увеличивается только вложенный цикл j. Цикл i всегда выполняется 9 раз
+"""
+
+t1 = Timer("multiplicity()", "from __main__ import multiplicity")
+print(f"При входных данных {USER_RANGE}, функция 1 выполнилась за {t1.timeit(number=10000)} секунд")
+USER_RANGE = 20
+MY_ARRAY = [i for i in range(2, USER_RANGE)]
+print(f"При входных данных {USER_RANGE}, функция 1 выполнилась за {t1.timeit(number=10000)} секунд")
+USER_RANGE = 30
+MY_ARRAY = [i for i in range(2, USER_RANGE)]
+print(f"При входных данных {USER_RANGE}, функция 1 выполнилась за {t1.timeit(number=10000)} секунд")
+USER_RANGE = 40
+MY_ARRAY = [i for i in range(2, USER_RANGE)]
+print(f"При входных данных {USER_RANGE}, функция 1 выполнилась за {t1.timeit(number=10000)} секунд")
+
+
+"""
+Сложность алгоритма - квадратичная, так как теперь увеличивается и цикл i, и цикл j.
+И в итоге при увеличении входных данных, время выполнения увеличивается нелинейно.
+"""
+
+USER_RANGE = 10
+
+
+def multiplicity_2():
+    for i in range(2, USER_RANGE):
+        MULTIPLE = 0
+        for j in MY_ARRAY:
+            if j % i == 0:
+                MULTIPLE += 1
+
+
+t2 = Timer("multiplicity_2()", "from __main__ import multiplicity_2")
+print(f"При входных данных {USER_RANGE}, функция 2 выполнилась за {t2.timeit(number=10000)} секунд")
+USER_RANGE = 20
+MY_ARRAY = [i for i in range(2, USER_RANGE)]
+print(f"При входных данных {USER_RANGE}, функция 2 выполнилась за {t2.timeit(number=10000)} секунд")
+USER_RANGE = 30
+MY_ARRAY = [i for i in range(2, USER_RANGE)]
+print(f"При входных данных {USER_RANGE}, функция 2 выполнилась за {t2.timeit(number=10000)} секунд")
+USER_RANGE = 40
+MY_ARRAY = [i for i in range(2, USER_RANGE)]
+print(f"При входных данных {USER_RANGE}, функция 2 выполнилась за {t2.timeit(number=10000)} секунд")
+
+
+"""
+Попробуем оптимизировать задачу на переворот числа
+"""
+USER_NUMBER = 8237
+
+
+def number_converter(number):
+    converted_number = ""
+    while number != 0:
+        converted_number = converted_number + str(number % 10)
+        number = number // 10
+    return converted_number
+
+
+t3 = Timer("number_converter(USER_NUMBER)", "from __main__ import number_converter, USER_NUMBER")
+print(f"{t3.timeit()} секунд выполнялась функция при конкатинации")
+
+
+def number_converter_2(number):
+    converted_number = str(number)[::-1]
+    return converted_number
+
+
+t4 = Timer("number_converter_2(USER_NUMBER)", "from __main__ import number_converter_2, USER_NUMBER")
+print(f"{t4.timeit()} секунд выполнялась функция при использовании слайса")
+
+"""
+При использовании среза, операция занимает намного меньше времени, так как
+конкатинация получается намного тяжелее (не нашел информации о сложности операции конкатинации)
+При использовании среза, сложность становится линейной O(n), где n - длина среза
+"""
--- Урок 4. Практическое задание/task_2.py
+++ Урок 4. Практическое задание/task_2.py
@@ -12,3 +12,63 @@
 ВНИМАНИЕ: ЗАДАНИЯ, В КОТОРЫХ БУДУТ ГОЛЫЕ ЦИФРЫ ЗАМЕРОВ (БЕЗ АНАЛИТИКИ)
 БУДУТ ПРИНИМАТЬСЯ С ОЦЕНКОЙ УДОВЛЕТВОРИТЕЛЬНО
 """
+import math
+from timeit import Timer
+
+
+def non_erat(num):
+    numbers = [i for i in range(2,10000)]
+    counter = 0
+    primal_counter = 0
+    for i in numbers:
+        if primal_counter == num:
+            return i-1
+        counter += 1
+        is_primal = True
+        for j in range(2, i):
+            if i % j == 0:
+                is_primal = False
+                break
+        if is_primal:
+            primal_counter += 1
+
+
+
+def erat(num):
+    *primes, = [i for i in range(2,10000)]
+    primes[4::2] = [0] * len(primes[4::2])
+    for i in range(3, math.ceil(num), 2):
+        if primes[i]:
+            primes[i * i::2*i] * len(primes[i * i::2*i])
+    return sorted(set(primes))[2:]
+
+
+t1 = Timer("non_erat(USER_NUMBER)", "from __main__ import non_erat, USER_NUMBER")
+t2 = Timer("erat(USER_NUMBER)", "from __main__ import erat, USER_NUMBER")
+
+USER_NUMBER = 10
+print(f"При поиске {USER_NUMBER} простого числа с помощью алгоритма "
+      f"без решета понадобилось {t1.timeit(number=10)} сек на 10 итераций")
+print(f"При поиске {USER_NUMBER} простого числа с помощью алгоритма "
+      f"с решетом понадобилось {t2.timeit(number=10)} сек на 10 итераций")
+USER_NUMBER = 200
+print(f"При поиске {USER_NUMBER} простого числа с помощью алгоритма "
+      f"без решета понадобилось {t1.timeit(number=10)} сек на 10 итераций")
+print(f"При поиске {USER_NUMBER} простого числа с помощью алгоритма "
+      f"с решетом понадобилось {t2.timeit(number=10)} сек на 10 итераций")
+USER_NUMBER = 800
+print(f"При поиске {USER_NUMBER} простого числа с помощью алгоритма "
+      f"без решета понадобилось {t1.timeit(number=10)} сек на 10 итераций")
+print(f"При поиске {USER_NUMBER} простого числа с помощью алгоритма "
+      f"с решетом понадобилось {t2.timeit(number=10)} сек на 10 итераций")
+
+
+"""
+Алгоритм без решета, который последовательно проверяет числа, имеет сложность O(n^2)
+работает эффективнее при малых входных значениях.
+При этом решето считается на малых входных данных выходит более трудозатратным.
+Но при увеличении входных данных, решето выигрывает, т.к. сложность алгоритма
+логорифмическая O(n log n)
+"""
+
+
