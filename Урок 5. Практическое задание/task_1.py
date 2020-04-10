a6d3f9e1decfcb533cbb9cec200e66521e355d9f
Vladislav Vovk <vladyslavvovk@yandex.ru>
2020-04-10 10:04:43 +0300

    Практическое задание к 5 уроку.

--- Урок 5. Практическое задание/task_1.py
+++ Урок 5. Практическое задание/task_1.py
@@ -25,3 +25,91 @@
 
 Предприятия, с прибылью ниже среднего значения: Копыта
 """
+
+# первый вариант с использованием ChainMap
+from collections import ChainMap
+from functools import reduce
+
+N = int(input('Введите количество предприятий для расчета прибыли: '))
+COUNT = 0
+COMPANY_DATA = ChainMap()
+
+while COUNT != N:
+    COUNT += 1
+    NAME = input('Введите название предприятия: ')
+    COMPANY_PROFIT = input('через пробел введите прибыль данного'
+                             'предприятия за каждый квартал(Всего 4 квартала):').split()
+    # считаем среднию прибыль предприятия
+    AVG = reduce(lambda x, y: int(x) + int(y), COMPANY_PROFIT)
+    # заполняем цепочку словарей
+    COMPANY_DATA.update({NAME: AVG})
+# считаем среднию прибыль всех предприятий
+AVG_PROFIT_ALL = sum(COMPANY_DATA.values())/N
+
+HIGH_AVG = []
+LOW_AVG = []
+
+for key in COMPANY_DATA.keys():
+    if COMPANY_DATA.get(key) > AVG_PROFIT_ALL:
+        HIGH_AVG.append(key)
+    elif COMPANY_DATA.get(key) < AVG_PROFIT_ALL:
+        LOW_AVG.append(key)
+
+
+print(f'Средняя годовая прибыль всех предприятий: {AVG_PROFIT_ALL}')
+print(f'Предприятия, с прибылью выше среднего значения: {" ".join(HIGH_AVG)}')
+print(f'Предприятия, с прибылью меньше среднего значения: {" ".join(LOW_AVG)}')
+
+
+
+
+# второй вариант реализации с использованием namedtuple
+
+from collections import namedtuple
+
+N = int(input('Введите количество предприятий для расчета прибыли: '))
+COUNT = 0
+
+# объявляем кортеж
+CompanyData = namedtuple('COMAPINIES_DATA', 'first second third firth')
+
+# создаем словарь для хранения кортежей
+COMPANY_ALL = {}
+
+while COUNT != N:
+    COUNT += 1
+    NAME = input('Введите название предприятия: ')
+    COMPANY_PROFIT = input('через пробел введите прибыль данного'
+                             'предприятия за каждый квартал(Всего 4 квартала):').split()
+
+    # заполняем кортеж данными за каждый квартал
+    # на случай если понадобиться вытащить данные за каждый
+    # квартал по конкретному предприятию
+
+    COMPANY_ALL[NAME] = CompanyData(
+        first=int(COMPANY_PROFIT[0]),
+        second=int(COMPANY_PROFIT[1]),
+        third=int(COMPANY_PROFIT[2]),
+        firth=int(COMPANY_PROFIT[3])
+    )
+# считаем общию прибыль
+TOTAL_PROFIT = 0
+
+for name, profit in COMPANY_ALL.items():
+    TOTAL_PROFIT += sum(profit)
+
+# считаем среднию прибыль
+AVG_ALL = TOTAL_PROFIT / N
+
+HIGH_AVG = []
+LOW_AVG = []
+
+for name, profit in COMPANY_ALL.items():
+    if sum(profit) > AVG_ALL:
+        HIGH_AVG.append(name)
+    elif sum(profit) < AVG_ALL:
+        LOW_AVG.append(name)
+
+print(f'Средняя годовая прибыль всех предприятий: {AVG_ALL}')
+print(f'Предприятия, с прибылью выше среднего значения: {" ".join(HIGH_AVG)}')
+print(f'Предприятия, с прибылью меньше среднего значения: {" ".join(LOW_AVG)}')
--- Урок 5. Практическое задание/task_2.py
+++ Урок 5. Практическое задание/task_2.py
@@ -11,3 +11,81 @@
 Также попробуйте решить задачу вообще без collections и применить только ваши знания по ООП
 (в частности по перегрузке методов)
 """
+# первый вариант с использованием defaultdict
+
+from collections import defaultdict
+from functools import reduce
+
+D = defaultdict(list)
+# можно ипользовать ChainMap вместо defaultdict
+# заменив на строку D = ChainMap() или обычный словарь
+D['FIRST_NUM'] = [i for i in input('Введите первое число :')]
+D['SECOND_NUM'] = [i for i in input('Введите второе число :')]
+
+print(f'Первое число : {D.get("FIRST_NUM")}\n'
+      f'Второе число : {D.get("SECOND_NUM")}')
+
+# сумма чисел
+AMOUNT = hex(int(reduce(lambda x, y: x+y, D.get('FIRST_NUM')), 16) +
+             int(reduce(lambda x, y: x+y, D.get('SECOND_NUM')), 16))
+
+# произведение чисел
+PRODUCT = hex(int(reduce(lambda x, y: x+y, D.get('FIRST_NUM')), 16) *
+              int(reduce(lambda x, y: x+y, D.get('SECOND_NUM')), 16))
+
+print(f'Сумма чисел : {[i for i in AMOUNT.upper()[2:]]}\n'
+      f'Произведение чисел : {[i for i in PRODUCT.upper()[2:]]}')
+
+
+# вариант с использованием именованного кортежа
+
+from collections import namedtuple
+from functools import reduce
+
+NUMBERS = namedtuple('NUMBERS', 'first second')
+NUMBERS_TO_COUNT = NUMBERS(
+      first=[i for i in input('Введите первое число :')],
+      second=[i for i in input('Введите второе число :')]
+)
+
+print(f'Первое число : {NUMBERS_TO_COUNT.first}\n'
+      f'Второе число : {NUMBERS_TO_COUNT.second}')
+
+# сумма чисел
+AMOUNT = hex(int(reduce(lambda x, y: x+y, NUMBERS_TO_COUNT.first), 16) +
+             int(reduce(lambda x, y: x+y, NUMBERS_TO_COUNT.second), 16))
+
+# произведение чисел
+PRODUCT = hex(int(reduce(lambda x, y: x+y, NUMBERS_TO_COUNT.first), 16) *
+              int(reduce(lambda x, y: x+y, NUMBERS_TO_COUNT.second), 16))
+
+print(f'Сумма чисел : {[i for i in AMOUNT.upper()[2:]]}\n'
+      f'Произведение чисел : {[i for i in PRODUCT.upper()[2:]]}')
+
+
+
+# вариант без collection используя класс
+
+class Numbers():
+      def __init__(self, num):
+            self.num = num
+
+      def __add__(self, other):
+            return Numbers(hex(int(reduce(lambda x, y: x + y, self.num), 16) +
+                               int(reduce(lambda x, y: x + y, other.num), 16)))
+
+      def __mul__(self, other):
+            return Numbers(hex(int(reduce(lambda x, y: x + y, self.num), 16) *
+                               int(reduce(lambda x, y: x + y, other.num), 16)))
+
+      def __str__(self):
+            return f'{[i for i in self.num[2:].upper()]}'
+
+
+NUMBER_1 = [i for i in input('Введите первое число :')]
+NUMBER_2 = [i for i in input('Введите второе число :')]
+
+N1 = Numbers(NUMBER_1)
+N2 = Numbers(NUMBER_2)
+print(f'Сумма чисел : {N1 + N2}\n'
+      f'Произведение чисел : {N1 * N2}')
