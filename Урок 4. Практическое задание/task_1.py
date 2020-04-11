64bcbc442dd5e5183434c4418c8f94c6fde9b1df
dmitriikudrin <39126785+dmitriikudrin@users.noreply.github.com>
2020-04-11 16:16:59 +0700

    lesson5

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -12,3 +12,73 @@
 ВНИМАНИЕ: ЗАДАНИЯ, В КОТОРЫХ БУДУТ ГОЛЫЕ ЦИФРЫ ЗАМЕРОВ (БЕЗ АНАЛИТИКИ)
 БУДУТ ПРИНИМАТЬСЯ С ОЦЕНКОЙ УДОВЛЕТВОРИТЕЛЬНО
 """
+
+import timeit
+import copy
+from random import randint
+
+
+# Вариант 1
+def sort_by_selection1():
+    arr = copy.deepcopy(input_list)
+    N = len(arr)
+    i = 0
+    while i < N - 1:
+        m = i
+        j = i + 1
+        while j < N:
+            if arr[j] < arr[m]:
+                m = j
+            j += 1
+        arr[i], arr[m] = arr[m], arr[i]
+        i += 1
+    return arr
+
+
+# Вариант 2
+def find_smallest(arr):
+    smalest = arr[0]
+    smalest_idx = 0
+    for i in range(1, len(arr)):
+        if arr[i] < smalest:
+            smalest = arr[i]
+            smalest_idx = i
+    return smalest_idx
+
+
+def sort_by_selection2():
+    arr = copy.deepcopy(input_list)
+    new_arr = []
+    for i in range(0, len(arr)):
+        idx = find_smallest(arr)
+        new_arr.append(arr.pop(idx))
+    return new_arr
+
+
+items = 100
+input_list = [randint(0, 100) for i in range(items)]
+
+
+print(sort_by_selection1())
+print(sort_by_selection2())
+
+number = 100
+t1 = timeit.Timer("sort_by_selection1()", setup="from __main__ import sort_by_selection1")
+print("sort_by_selection1() - ", t1.timeit(number=number), "milliseconds")
+t2 = timeit.Timer("sort_by_selection2()", setup="from __main__ import sort_by_selection2")
+print("sort_by_selection2() - ", t2.timeit(number=number), "milliseconds")
+
+"""
+Вариант 1
+У нас есть один внешний цикл - его сложность О(n).
+У нас есть вложенный цикл - его сложность О(n).
+В итоге общая сложность алгоритма - О(n^2)
+
+Вариант 2
+В основной функции у нас есть цикл с максимальной сложностью О(n).
+В вызываемой функции у нас есть цикл - у него сложность меняется от О(1) до О(n).
+Соотвественно средняя сложность цикла - О(n/2).
+В итоге общая сложность алгоритма - О(n*(n/2))
+
+Как видно по результатам второй алгоритм действительно выполняется быстрее.
+"""
--- Урок 5. Практическое задание/task_2.py
+++ Урок 5. Практическое задание/task_2.py
@@ -11,3 +11,37 @@
 Также попробуйте решить задачу вообще без collections и применить только ваши знания по ООП
 (в частности по перегрузке методов)
 """
+
+
+# Вариант 1 - ООП
+class HexNum:
+    def __init__(self, hex_str):
+        self.hex_list = list(hex_str)
+
+    def __add__(self, obj):
+        add_int = int("".join(self.hex_list), 16) + int("".join(obj.hex_list), 16)
+        return list(hex(add_int).upper()[2:])
+
+    def __mul__(self, obj):
+        mul_int = int("".join(self.hex_list), 16) * int("".join(obj.hex_list), 16)
+        return list(hex(mul_int).upper()[2:])
+
+
+num1 = HexNum(input("Введите первое число: "))
+num2 = HexNum(input("Введите второе число: "))
+
+print(f"Сумма чисел - {num1 + num2}")
+print(f"Произведение чисел - {num1 * num2}")
+
+
+# Вариант 2 - Collections
+import collections
+
+nums = collections.defaultdict(list)
+nums['num1'] = list(input("Введите первое число: "))
+nums['num2'] = list(input("Введите первое число: "))
+nums['sum'] = list(hex(int("".join(nums['num1']), 16) + int("".join(nums['num2']), 16)).upper()[2:])
+nums['mul'] = list(hex(int("".join(nums['num1']), 16) * int("".join(nums['num2']), 16)).upper()[2:])
+
+print(f"Сумма чисел - {nums['sum']}")
+print(f"Произведение чисел - {nums['mul']}")
