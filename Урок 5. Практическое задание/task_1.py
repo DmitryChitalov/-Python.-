36a8693fceb9f89cc00658333356033946b64fca
anna <anna@rcbd.org>
2020-04-10 23:43:43 -0400

    task_1-2

--- Урок 5. Практическое задание/task_1.py
+++ Урок 5. Практическое задание/task_1.py
@@ -25,3 +25,78 @@
 
 Предприятия, с прибылью ниже среднего значения: Копыта
 """
+import collections
+
+ENTERPRISE = collections.namedtuple('Enterprise', ['name', 'q1', 'q2', 'q3', 'q4'])
+
+
+def input_enterprise():
+    """Ввод данных с клавиатуры"""
+    iname = input('Введите название предприятия: ')
+    iq1 = int(input('Введите прибыль за 1 квартал: '))
+    iq2 = int(input('Введите прибыль за 2 квартал: '))
+    iq3 = int(input('Введите прибыль за 3 квартал: '))
+    iq4 = int(input('Введите прибыль за 4 квартал: '))
+
+    exn = ENTERPRISE(name=iname, q1=iq1, q2=iq2, q3=iq3, q4=iq4)
+    return exn
+
+def annual_revenue(exn: ENTERPRISE):
+    """Total"""
+    return exn.q1 + exn.q2 + exn.q3 + exn.q4
+
+def calculate(es_new: list):
+    """Расчет выше стреднего и ниже среднего занчения"""
+    sum_revenue = 0
+    for e_new in es_new:
+        sum_revenue += annual_revenue(e_new)
+    average_revenue = sum_revenue / len(es_new)
+
+    below_avarage = list()
+    above_avarage = list()
+
+    for e_new in es_new:
+        if annual_revenue(e_new) > average_revenue:
+            above_avarage.append(e_new)
+        elif annual_revenue(e_new) < average_revenue:
+            below_avarage.append(e_new)
+
+    return below_avarage, above_avarage
+
+def solution():
+    """Решение задачи"""
+    count = int(input('Введите количество предприятий для расчета: '))
+    es_new = list()
+
+    for _ in range(count):
+        e_new = input_enterprise()
+        es_new.append(e_new)
+
+    above_average, below_average = calculate(es_new)
+
+    print("Выше среднего")
+    for e_new in above_average:
+        print(e_new.name)
+
+    print("Ниже среднего")
+    for e_new in below_average:
+        print(e_new.name)
+
+def test():
+    """test"""
+    e1_new = ENTERPRISE(name="one", q1=1, q2=1, q3=1, q4=1)
+    e2_new = ENTERPRISE(name="two", q1=2, q2=2, q3=2, q4=2)
+    e3_new = ENTERPRISE(name="three", q1=3, q2=3, q3=3, q4=3)
+
+    above, below = calculate(list([e1_new, e2_new, e3_new]))
+
+    assert len(above) == 1
+    assert len(below) == 1
+
+    print("Выше среднего", above)
+
+    print("Ниже среднего", below)
+
+# start
+#solution()
+test()
--- Урок 5. Практическое задание/task_2.py
+++ Урок 5. Практическое задание/task_2.py
@@ -11,3 +11,48 @@
 Также попробуйте решить задачу вообще без collections и применить только ваши знания по ООП
 (в частности по перегрузке методов)
 """
+from collections import defaultdict
+from collections import deque
+
+
+def my_dex(list):
+    """Преабразует строку в число"""
+    dex = 0
+    num = deque(list)
+    num.reverse()
+    for i in range(len(num)):
+        dex += TABLE[num[i]] * 16 ** i
+    return dex
+
+def my_str_to_list(string):
+    """Преобразуем ввод пользователя в лист"""
+    result = list()
+    for cnx in string:
+        result.append(cnx)
+    return result
+
+def my_hex(numb):
+    """Преабразует число в 16ый формат"""
+    num = deque()
+    while numb > 0:
+        dec = numb % 16
+        for i in TABLE:
+            if TABLE[i] == dec:
+                num.append(i)
+        numb //= 16
+    num.reverse()
+    return list(num)
+
+
+SIGNS = '0123456789ABCDEF'
+TABLE = defaultdict(int)
+COUNTER = 0
+for key in SIGNS:
+    TABLE[key] += COUNTER
+    COUNTER += 1
+
+NUM_1 = my_dex(my_str_to_list(input('Введите первое число в 16ом формате:\n ').upper()))
+NUM_2 = my_dex(my_str_to_list(input('Введите второе число в 16ом формате:\n ').upper()))
+
+print(f'Сумма чисел: {my_hex(NUM_1 + NUM_2)}')
+print(f'Произведение чисел: {my_hex(NUM_1 * NUM_2)}')
