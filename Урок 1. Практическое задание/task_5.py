cd46d20badde32ee18f3cbfaf12bc31465fb9b99
Nikita Shataev <shataev.nikita5@gmail.com>
2020-04-22 10:12:41 +0700

    task 1.5 done

--- Урок 1. Практическое задание/task_5.py
+++ Урок 1. Практическое задание/task_5.py
@@ -10,3 +10,18 @@
 В обоих случаях программа должна вывести корректный результат.
 В обоих случаях он 24, но никак не -24
 """
+
+LETTER_1 = input('input a 1st letter ')
+LETTER_NUMB_1 = (ord(LETTER_1))
+LETTER_2 = input('input a second letter ')
+LETTER_NUMB_2 = (ord(LETTER_2))
+NUMB_DIFF = LETTER_NUMB_2 - LETTER_NUMB_1-1
+if NUMB_DIFF < 0:
+    NUMB_DIFF = LETTER_NUMB_2 - LETTER_NUMB_1+1
+    NUMB_DIFF *= -1
+    print('Между ',LETTER_1, ' и ',LETTER_2,' находится', NUMB_DIFF,' букв')
+
+else:
+    print('Между ',LETTER_1, ' и ',LETTER_2,' находится', NUMB_DIFF,' букв')
+
+
