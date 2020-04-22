9b82b4fd1beb0fd7f2957221c667cc42df371b7f
Iuliia-Fedkovych <Julia_Fedkovich@mail.ru>
2020-04-22 16:46:06 +0300

    task_2

--- Урок 7. Практическое задание/task_2.py
+++ Урок 7. Практическое задание/task_2.py
@@ -1,3 +1,5 @@
+import random
+
 """
 2. Отсортируйте по возрастанию методом слияния одномерный вещественный массив,
 заданный случайными числами на промежутке [0; 50). Выведите на экран исходный
@@ -8,3 +10,40 @@
 Исходный - [46.11436617832828, 41.62921998361278, 18.45859540989644, 12.128870723745806, 8.025098788570562]
 Отсортированный - [8.025098788570562, 12.128870723745806, 18.45859540989644, 41.62921998361278, 46.11436617832828]
 """
+
+
+def merge_sort(orig_list):
+    if len(orig_list) > 1:
+        center = len(orig_list) // 2
+        left = orig_list[:center]
+        right = orig_list[center:]
+
+        merge_sort(left)
+        merge_sort(right)
+
+        i, j, k = 0, 0, 0
+
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                orig_list[k] = left[i]
+                i += 1
+            else:
+                orig_list[k] = right[j]
+                j += 1
+            k += 1
+
+        while i < len(left):
+            orig_list[k] = left[i]
+            i += 1
+            k += 1
+
+        while j < len(right):
+            orig_list[k] = right[j]
+            j += 1
+            k += 1
+        return orig_list
+
+
+new_list = [random.uniform(0, 50) for i in range(0, 10)]
+print(new_list)
+print(merge_sort(new_list))
