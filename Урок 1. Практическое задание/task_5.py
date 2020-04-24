3239320bbae4581bbcb0dfbb1687c6b877230b06
Puff <puffvl@gmail.com>
2020-04-24 20:24:13 +1000

    Решения к заданиям урока №1

--- Урок 1. Практическое задание/task_5.py
+++ Урок 1. Практическое задание/task_5.py
@@ -10,3 +10,11 @@
 В обоих случаях программа должна вывести корректный результат.
 В обоих случаях он 24, но никак не -24
 """
+LETTER_A = ord(input('Введите первую букву:'))
+LETTER_B = ord(input('Введите вторую букву:'))
+if LETTER_A > LETTER_B:
+    LETTER_A, LETTER_B = LETTER_B, LETTER_A
+DISTANCE = LETTER_B - LETTER_A - 1
+print(f'Буква "{chr(LETTER_A)}" в алфавите на позиции {LETTER_A - 96}')
+print(f'Буква "{chr(LETTER_B)}" в алфавите на позиции {LETTER_B - 96}')
+print(f'Между ними {DISTANCE} букв')
