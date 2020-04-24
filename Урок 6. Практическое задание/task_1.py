01e229d4da4eab4efa958f6c38f473699cca7318
stmexx <stmexx@gmail.com>
2020-04-24 12:34:45 +0300

    less 6 done

--- Урок 6. Практическое задание/task_1.py
+++ Урок 6. Практическое задание/task_1.py
@@ -12,3 +12,44 @@
 ВНИМАНИЕ: ЗАДАНИЯ, В КОТОРЫХ БУДУТ ГОЛЫЕ ЦИФРЫ ЗАМЕРОВ (БЕЗ АНАЛИТИКИ)
 БУДУТ ПРИНИМАТЬСЯ С ОЦЕНКОЙ УДОВЛЕТВОРИТЕЛЬНО
 """
+import random
+import sys
+from functools import reduce
+
+from memory_profiler import profile
+
+
+@profile
+def find_idx():
+    numbers = [random.randint(0, 200) for i in range(60000)]
+    multiples_idx = [idx for idx, num in enumerate(numbers) if num % 2 == 0]
+
+    print(numbers)
+    print('Индексы четных элементов,', multiples_idx)
+    print(sys.getrefcount(multiples_idx))
+
+
+find_idx()
+
+
+@profile
+def replace_digits():
+    numbers = [random.randint(0, 200) for i in range(60000)]
+
+    num_min = min(numbers)
+    num_max = max(numbers)
+
+    idx_min = numbers.index(num_min)
+    idx_max = numbers.index(num_max)
+
+    replace_numbers = numbers[:]
+    replace_numbers[idx_min], replace_numbers[idx_max] = replace_numbers[idx_max], replace_numbers[idx_min]
+    print(numbers)
+    print(replace_numbers)
+    print(sys.getrefcount(replace_numbers))
+
+
+replace_digits()
+
+
+#обе программы работают без увеличения колва памяти
