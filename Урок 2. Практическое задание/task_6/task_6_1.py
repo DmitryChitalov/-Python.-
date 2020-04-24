eee004943d84b896395db4ed91cde6a0804560fb
Begemotina <Begemotina@nightmail.ru>
2020-04-24 23:45:02 +0300

    6.1 - готово

--- Урок 2. Практическое задание/task_6/task_6_1.py
+++ Урок 2. Практическое задание/task_6/task_6_1.py
@@ -7,3 +7,35 @@
 
 ЗДЕСЬ ДОЛЖНА БЫТЬ РЕАЛИЗАЦИЯ ЧЕРЕЗ ЦИКЛ
 """
+from random import random
+
+# загдываем число
+R_NUMBER = int(random()*100)
+print(R_NUMBER)
+END = 10
+i = 1
+
+# отгадываем число
+while True:
+    if i <= END:
+        print(f'попытка {i}')
+        while True:
+            try:
+                USER_NUMBER = int(input('Введите число от 0 до 100\n'))
+                break
+            except ValueError:
+                print('Вы вводите что-то не то попробуйте еще раз\n')
+                i = i + 1
+                print(f'попытка {i}')
+        if R_NUMBER == USER_NUMBER:
+            print("Вы угадали!!!")
+            break
+        elif R_NUMBER < USER_NUMBER:
+            print("Ваше число больше загаднного\n")
+            i = i + 1
+        elif R_NUMBER > USER_NUMBER:
+            print("Ваше меньше загаднного\n")
+            i = i + 1
+    else:
+        print(f"Вы не угадали, число было {R_NUMBER}")
+        break
