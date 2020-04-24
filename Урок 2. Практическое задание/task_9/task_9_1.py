662d8f2c66fa96f7ca9f20472d1ae4ea0cbb544e
Begemotina <Begemotina@nightmail.ru>
2020-04-25 01:29:58 +0300

    9.1 готово

--- Урок 2. Практическое задание/task_9/task_9_1.py
+++ Урок 2. Практическое задание/task_9/task_9_1.py
@@ -10,3 +10,34 @@
 
 ЗДЕСЬ ДОЛЖНА БЫТЬ РЕАЛИЗАЦИЯ ЧЕРЕЗ ЦИКЛ
 """
+# чего считаем
+TOTAL = int(input('Введите сколько будет чисел: \n'))
+max_summ = 0
+summ = 0
+max_row = 0
+# считаем
+i = 0
+ROW_NUMBER = 1
+while i < TOTAL:
+
+
+    ROW = user_row = int(input(f'Введите число {ROW_NUMBER}: '))
+    ROW_NUMBER = ROW_NUMBER + 1
+    ROW_LENGTH = len(str(ROW))
+
+    k = 1
+    summ = 0
+    while k <= ROW_LENGTH:
+        NUMBER = ROW % 10
+        summ += NUMBER
+        ROW = ROW // 10
+        k = k + 1
+
+    if max_summ < summ:
+        max_summ = summ
+        max_row = user_row
+
+    i = i + 1
+
+# выводим ответ
+print(f'Наибольшее число по сумме цифр: {max_row}, сумма его цифр: {max_summ}')
