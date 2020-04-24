b7b6aa1bf35bba76c10ab220c73b0452aed1a5da
Igor Kuzmin <igorkuz2018@yandex.ru>
2020-04-24 03:20:21 +0300

    Задание к 7 уроку

--- Урок 7. Практическое задание/task_1.py
+++ Урок 7. Практическое задание/task_1.py
@@ -9,3 +9,87 @@
 Подсказка: обратите внимание, сортируем не по возрастанию, как в примере,
 а по убыванию
 """
+import timeit
+import random
+import copy
+
+
+def bubble_sort(orig_list):
+    n = 1
+    flag = False
+    start_list = copy.deepcopy(orig_list)
+    while n < len(orig_list):
+        for i in range(len(orig_list)-n):
+            if orig_list[i] < orig_list[i+1]:
+                orig_list[i], orig_list[i+1] = orig_list[i+1], orig_list[i]
+                flag = True
+        if not flag:
+            break
+        n += 1
+    return f"начальный список{start_list} конечный{orig_list}"
+
+
+def bubble_sort2(orig_list):
+    n = 1
+    start_list = copy.deepcopy(orig_list)
+    while n < len(orig_list):
+        t = 0
+        for i in range(len(orig_list)-n):
+            if orig_list[i] < orig_list[i+1]:
+                orig_list[i], orig_list[i+1] = orig_list[i+1], orig_list[i]
+        n += 1
+    return f"начальный список{start_list} конечный{orig_list}"
+
+
+
+
+
+
+orig_list = [random.randint(-100, 100) for _ in range(10)]
+
+# замеры 10
+
+print("замеры 10 с выходом:")
+print(timeit.timeit("bubble_sort(orig_list)", \
+    setup="from __main__ import bubble_sort, orig_list", number=1000))
+print("Без выхода")
+print(timeit.timeit("bubble_sort2(orig_list)", \
+    setup="from __main__ import bubble_sort2, orig_list", number=1000))
+
+
+orig_list = [random.randint(-100, 100) for _ in range(100)]
+
+# замеры 100
+print("замеры 100 с выходом")
+print(timeit.timeit("bubble_sort(orig_list)", \
+    setup="from __main__ import bubble_sort, orig_list", number=1000))
+print("Без выхода")
+print(timeit.timeit("bubble_sort2(orig_list)", \
+    setup="from __main__ import bubble_sort2, orig_list", number=1000))
+
+orig_list = [random.randint(-10000, 10000) for _ in range(1000)]
+
+# замеры 1000
+
+print("замеры 1000 с выходом")
+print(timeit.timeit("bubble_sort(orig_list)", \
+    setup="from __main__ import bubble_sort, orig_list", number=1000))
+
+print("Без выхода")
+print(timeit.timeit("bubble_sort2(orig_list)", \
+    setup="from __main__ import bubble_sort2, orig_list", number=1000))
+"""
+замеры 10 с выходом:
+0.013286840000000001
+Без выхода
+0.019024961
+замеры 100 с выходом
+0.1004286
+Без выхода
+0.7062443230000001
+замеры 1000 с выходом
+1.199337974
+Без выхода
+66.694136468
+функция с выходом оказалась быстрей
+"""
--- Урок 7. Практическое задание/task_3.py
+++ Урок 7. Практическое задание/task_3.py
@@ -6,3 +6,104 @@
 массива. Но если это слишком сложно, то используйте метод сортировки,
 который не рассматривался на уроках
 """
+import statistics
+import random
+import copy
+import timeit
+import copy
+
+def func(a):
+    a.sort()
+    return a[len(a)//2]
+
+def my_mid(a):
+    b = copy.deepcopy(a)
+    mid_l = random.choice(b)
+    indx_mid = b.index(mid_l)
+    b.pop(indx_mid)
+    R = []
+    L = []
+
+    for i in b:  # делим массив на правую и левую часть
+        if i > mid_l:
+            R.append(i)
+        else:
+            L.append(i)
+    if len(R) == len(L):
+        return mid_l
+
+    elif len(R) < len(L):
+        R.append(mid_l)
+        mid_l = max(L)
+        L.pop(L.index(mid_l))
+        while len(R) != len(L): # уравниваем 2 массива
+            R.append(mid_l)
+            mid_l = max(L)
+            L.pop(L.index(mid_l))
+        return mid_l
+
+    elif len(R) > len(L):
+        L.append(mid_l)
+        mid_l = min(R)
+        R.pop(R.index(mid_l))
+        while len(R) != len(L): # уравниваем 2 массива
+            L.append(mid_l)
+            mid_l = min(R)
+            R.pop(R.index(mid_l))
+        return mid_l
+
+
+
+# замеры 101
+a = [random.randint(-10000, 10000) for _ in range(11)]
+
+print("1__________________")
+print(timeit.timeit("func(a)", \
+    setup="from __main__ import func, a", number=1000))
+
+print("2_________________")
+print(timeit.timeit("my_mid(a)", \
+    setup="from __main__ import my_mid, a", number=1000))
+
+
+a = [random.randint(-100000, 100000) for _ in range(101)]
+# замеры 10001
+
+print("1__________________")
+print(timeit.timeit("func(a)", \
+    setup="from __main__ import func, a", number=1000))
+
+print("2_________________")
+print(timeit.timeit("my_mid(a)", \
+     setup="from __main__ import my_mid, a", number=1000))
+print("3_________________")
+print(timeit.timeit("med_all(a)", \
+    setup="from __main__ import med_all, a", number=1000))
+
+a = [random.randint(-100000, 100000) for _ in range(1001)]
+
+
+# замеры 100001
+
+print("1__________________")
+print(timeit.timeit("func(a)", \
+    setup="from __main__ import func, a", number=1000))
+print("2_________________")
+print(timeit.timeit("my_mid(a)", \
+    setup="from __main__ import my_mid, a", number=1000))
+"""
+1__________________
+1.2399000000003213e-05
+2_________________
+6.673499999999832e-05
+1__________________
+0.0024469520000000022
+2_________________
+0.146323355
+1__________________
+0.031191161999999995
+2_________________
+33.163421750999994
+
+моя функция оказалась на много дольше, и с увеличением массива становится дольше
+"""
