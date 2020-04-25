1b8b2f9be389d4c07ebbe4cdce9587f8e90e9f52
Begemotina <Begemotina@nightmail.ru>
2020-04-25 16:12:35 +0300

    6.2 готово

--- Урок 2. Практическое задание/task_6/task_6_2.py
+++ Урок 2. Практическое задание/task_6/task_6_2.py
@@ -7,3 +7,30 @@
 
 ЗДЕСЬ ДОЛЖНА БЫТЬ РЕАЛИЗАЦИЯ ЧЕРЕЗ РЕКУРСИЮ
 """
+
+from random import random
+
+# загдываем число
+R_NUMBER = int(random() * 100)
+MAX_TRIES = 3
+
+
+# отгадываем число
+def gessing(r_number, max_tries, user_try=1):
+    """игра в угадайку"""
+    if user_try != max_tries + 1:
+        user_number = int(input(f'попытка {user_try}. Введите число от 0 до 100\n'))
+        if user_number == r_number:
+            return print("Вы угадали!!!")
+        elif user_number > r_number:
+            print("Ваше число больше загаднного\n")
+            return gessing(r_number, max_tries, user_try + 1)
+        else:
+            print("Ваше число меньше загаднного\n")
+            return gessing(r_number, max_tries, user_try + 1)
+    else:
+        return print(f"Вы не угадали, число было {r_number}")
+
+
+# поехали
+gessing(R_NUMBER, MAX_TRIES)
