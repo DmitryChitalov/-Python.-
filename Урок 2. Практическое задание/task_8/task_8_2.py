6b780180de4b42e1625dc898df5f9d0346268c1a
Begemotina <Begemotina@nightmail.ru>
2020-04-25 20:50:02 +0300

    8.2

--- Урок 2. Практическое задание/task_8/task_8_2.py
+++ Урок 2. Практическое задание/task_8/task_8_2.py
@@ -12,3 +12,40 @@
 
 ЗДЕСЬ ДОЛЖНА БЫТЬ РЕАЛИЗАЦИЯ ЧЕРЕЗ РЕКУРСИЮ
 """
+
+TOTAL = 3  # int(input('Введите сколько будет чисел \n'))
+NUMBER = 3  # int(input('Введите цифру, которо будем считать \n'))
+ROW = int("3")
+
+
+# def counter(row, number, coinsedence):
+#     """ситает количество цифер в ряд"""
+#     if len(str(row)) == 1:
+#         if int(row) == number:
+#             coinsedence += 1
+#             return coinsedence
+#         else:
+#             return coinsedence
+#     else:
+#         check = int(row % 10)
+#         if check == number:
+#             coinsedence += 1
+#             coinsedence = coinsedence + counter(row // 10, number)
+#             return coinsedence
+#         else:
+#             coinsedence = coinsedence + counter(row // 10, number)
+#             return coinsedence
+
+
+def row_maker(total, r_number=1):
+    """делает ряды цифры"""
+    while r_number <= total:
+        row = int(input(f"Введите число {r_number}: "))
+        print(row)
+        r_number += 1
+    return r_numbersssssssssssssssssssssssssssssssssssssssssssssss
+
+R_NUMBER = row_maker(TOTAL)
+print(f'{R_NUMBER-}')
+
+# print(f'Было введено {COINSEDENCE} цифры "{NUMBER}"')
