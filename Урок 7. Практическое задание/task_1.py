53a35e1c5428dc5693af6cd2f959fee8520f6503
anna <anna@rcbd.org>
2020-04-24 23:09:12 -0400

    task_1-3

--- Урок 7. Практическое задание/task_1.py
+++ Урок 7. Практическое задание/task_1.py
@@ -9,3 +9,49 @@
 Подсказка: обратите внимание, сортируем не по возрастанию, как в примере,
 а по убыванию
 """
+from random import randint
+from timeit import timeit
+
+MAX_SIZE = 100
+NUMBER_EXECUTIONS = 10_000
+
+
+def bubble_smart(array):
+    """Bubble sort with optimization"""
+    for i in range(len(array) - 1, 0, -1):
+        swapped = False
+        for n in range(i):
+            if array[n] < array[n+1]:
+                array[n], array[n+1] = array[n+1], array[n]
+                swapped = True
+
+        if not swapped:
+            break
+    return array
+
+
+def bubble_simple(array):
+    """Bubble sort without optimization"""
+    for i in range(len(array) - 1, 0, -1):
+        for n in range(i):
+            if array[n] < array[n+1]:
+                array[n], array[n+1] = array[n+1], array[n]
+
+    return array
+
+
+NUMS = [randint(-100, 100) for _ in range(MAX_SIZE)]
+print(NUMS)
+print(bubble_smart(NUMS))
+
+
+TIME1 = timeit(f'bubble_simple({NUMS})',
+               setup='from __main__ import bubble_simple',
+               number=NUMBER_EXECUTIONS)
+
+TIME2 = timeit(f'bubble_smart({NUMS})',
+               setup='from __main__ import bubble_smart',
+               number=NUMBER_EXECUTIONS)
+
+print("Simple", TIME1)
+print("Smart", TIME2)
--- Урок 7. Практическое задание/task_3.py
+++ Урок 7. Практическое задание/task_3.py
@@ -6,3 +6,52 @@
 массива. Но если это слишком сложно, то используйте метод сортировки,
 который не рассматривался на уроках
 """
+
+from random import randint
+
+SIZE = 3
+
+
+def partition(array, start, end):
+    """Partition"""
+    pivot = array[start]
+    low = start + 1
+    high = end
+
+    while True:
+        while low <= high and array[high] >= pivot:
+            high = high - 1
+
+        while low <= high and array[low] <= pivot:
+            low = low + 1
+        if low <= high:
+            array[low], array[high] = array[high], array[low]
+        else:
+            break
+
+    array[start], array[high] = array[high], array[start]
+
+    return high
+
+
+def quick_sort(array, start, end):
+    """Quick sort"""
+    if start >= end:
+        return
+
+    part = partition(array, start, end)
+    quick_sort(array, start, part-1)
+    quick_sort(array, part+1, end)
+
+
+def mediana(numbers):
+    """Finding mediana"""
+    middle = len(numbers)//2
+    return numbers[middle]
+
+
+NUMS = [randint(-100, 100) for _ in range(SIZE * 2 + 1)]
+print(NUMS)
+quick_sort(NUMS, 0, len(NUMS) - 1)
+print(NUMS)
+print("Mediana", mediana(NUMS))
