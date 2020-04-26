3be911121b871b06cfb3e00bd7178c57c108904d
Begemotina <Begemotina@nightmail.ru>
2020-04-26 18:55:23 +0300

    9.2 - готово без ввода рекурсией

--- Урок 2. Практическое задание/task_9/task_9_2.py
+++ Урок 2. Практическое задание/task_9/task_9_2.py
@@ -10,3 +10,38 @@
 
 ЗДЕСЬ ДОЛЖНА БЫТЬ РЕАЛИЗАЦИЯ ЧЕРЕЗ РЕКУРСИЮ
 """
+
+# данные
+TOTAL = int(input('Введите сколько будет чисел \n'))
+
+
+# тут цикл
+def comparior(total, max_sum=0, i=1):
+    """сравнивает сумму"""
+    while total > 0:
+        row = int(input(f'введите число {i} : '))
+        row_sum = sum_row(row)
+        total -= 1
+        i += 1
+
+        if max_sum <= row_sum:
+            max_sum = row_sum
+            max_row = row
+
+    return max_sum, max_row
+
+# тут рекурсия
+def sum_row(row):
+    """  сумма чисел"""
+    if len(str(row)) == 1:
+        summ = int(row)
+        return summ
+    else:
+        summ = row % 10 + sum_row(row // 10)
+        return summ
+
+
+# поехали
+MAX_SUM, ROW = comparior(TOTAL)
+
+print(f'самое большая сумма : {MAX_SUM}, в ряду "{ROW}"')
