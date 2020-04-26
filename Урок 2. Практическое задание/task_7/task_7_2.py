6fec0fe96fc0dac5cdaa08ac8273718667a1b266
a-morev <ion05pk@gmail.com>
2020-04-26 20:41:05 +0300

    Задания 2 урока.

--- Урок 2. Практическое задание/task_7/task_7_2.py
+++ Урок 2. Практическое задание/task_7/task_7_2.py
@@ -2,6 +2,19 @@
 7.	Напишите программу, доказывающую или проверяющую, что для множества
 натуральных чисел выполняется равенство: 1+2+...+n = n(n+1)/2,
  где n - любое натуральное число.
-
 ЗДЕСЬ ДОЛЖНА БЫТЬ РЕАЛИЗАЦИЯ ЧЕРЕЗ РЕКУРСИЮ
 """
+
+
+def equality(n: int, right_part=0, left_part=1):
+    if right_part == left_part:
+        return 'Равенство для выполняется!'
+    elif right_part < left_part:
+        return equality(n, right_part + 1, n * (n + 1) // 2)
+
+
+try:
+    number_n = int(input('Введите любое натуральное число для n: '))
+    equality(number_n)
+except ValueError:
+    print('Введены некорректные данные!')
--- Урок 2. Практическое задание/task_9/task_9_1.py
+++ Урок 2. Практическое задание/task_9/task_9_1.py
@@ -1,12 +1,24 @@
 """
 9. Среди натуральных чисел, которые были введены, найти
 наибольшее по сумме цифр. Вывести на экран это число и сумму его цифр.
-
 Пример:
 Введите количество чисел: 2
 Введите очередное число: 23
 Введите очередное число: 2
 Наибольшее число по сумме цифр: 23, сумма его цифр: 5
-
 ЗДЕСЬ ДОЛЖНА БЫТЬ РЕАЛИЗАЦИЯ ЧЕРЕЗ ЦИКЛ
 """
+quantity_numbers = int(input('Введите количество чисел: '))
+max_sum_number = 0
+max_number = 0
+for i in range(1, quantity_numbers + 1):
+    number = int(input(f'Введите очередное число: '))
+    count = 0
+    user_num = number
+    while number > 0:
+        count += number % 10
+        number //= 10
+    if count > max_sum_number:
+        max_sum_number = count
+        max_number = user_num
+print(f'Наибольшее число по сумме цифр: {max_number}, сумма его цифр: {max_sum_number}')
