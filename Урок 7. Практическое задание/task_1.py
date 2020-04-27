9a68c7daba3617d1815c86e5c869e86a2e45abc8
TerryNight <lizkakryuchkova@gmail.com>
2020-04-27 14:48:32 +0300

    7.1

--- Урок 7. Практическое задание/task_1.py
+++ Урок 7. Практическое задание/task_1.py
@@ -9,3 +9,47 @@
 Подсказка: обратите внимание, сортируем не по возрастанию, как в примере,
 а по убыванию
 """
+from random import randint
+from timeit import timeit
+
+MAX_SIZE = 100
+
+def bubble_sort_1(array):
+    for i in range(len(array) - 1, 0, -1):
+        flag = True
+        for n in range(i):
+            if array[n] > array[n+1]:
+                array[n], array[n+1] = array[n+1], array[n]
+                flag = False
+
+        if flag == True:
+            break
+    return array
+
+
+def bubble_sort_2(array):
+    for i in range(len(array) - 1, 0, -1):
+        for n in range(i):
+            if array[n] > array[n+1]:
+                array[n], array[n+1] = array[n+1], array[n]
+
+    return array
+
+
+numbers = [randint(-100, 100) for _ in range(MAX_SIZE)]
+print(numbers)
+print(bubble_sort_1(numbers))
+numbers = [randint(-100, 100) for _ in range(MAX_SIZE)]
+print(numbers)
+print(bubble_sort_2(numbers))
+
+time1 = timeit(f'bubble_sort_1({numbers})',
+              setup='from __main__ import bubble_sort_1',
+              number=50)
+time2 = timeit(f'bubble_sort_2({numbers})',
+              setup='from __main__ import bubble_sort_2',
+              number=50)
+print(time1)
+print(time2)
+"""Время первой реализации - 0.0005471999999999977"""
+"""Время второй реализации - 0.0260121"""
