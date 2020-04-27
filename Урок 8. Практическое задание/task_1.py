20f36d3eaaa412973a3df28475bdbfb43d1ddb77
pavelstef <p.stefanenko@gmail.com>
2020-04-27 19:46:58 +0300

    Практическое задание 8 сделано.

--- Урок 8. Практическое задание/task_1.py
+++ Урок 8. Практическое задание/task_1.py
@@ -16,3 +16,33 @@
 
 Итог: 6 подстрок
 """
+
+
+import hashlib
+
+
+def unique_substring(user_str: str) -> int:
+    """
+    Подсчёт количества уникальных подстрок.
+    Алгоритм имеет сложность O(n**2)
+    """
+    ss_set = set()
+    len_str = len(user_str)
+    for cursor in range(len_str):
+        for shift in range(1, len_str - cursor + 1):
+            substring = user_str[cursor: cursor + shift]
+            if substring == user_str:
+                pass
+            else:
+                ss_set.add(hashlib.sha1(substring.encode()).hexdigest())
+    del len_str
+    return len(ss_set)
+
+
+if __name__ == "__main__":
+
+    USER_STRING = input('Введите строку: ')
+    print(
+        'Количество уникальных подстрок в данной строке =',
+        unique_substring(USER_STRING)
+    )
--- Урок 8. Практическое задание/task_2.py
+++ Урок 8. Практическое задание/task_2.py
@@ -8,3 +8,76 @@ s = "beep boop beer!"
 Результат:
 00 11 11 101 010 00 011 011 101 010 00 11 11 1000 1001
 """
+
+
+from collections import Counter, deque
+
+
+class HNode:
+    """ Класс узла двоичного дерева """
+
+    def __init__(self, left=None, right=None):
+        self.left = left
+        self.right = right
+
+    def children(self):
+        """ Возврат левой и правой частей """
+        return self.left, self.right
+
+
+def frequency_matrix(user_str: str) -> deque:
+    """ Создание частотной матрицы """
+    f_matrix = Counter(user_str).items()
+    f_matrix = sorted(f_matrix, key=lambda x: x[1])
+    return deque(f_matrix)
+
+
+def huffman_tree(f_matrix: deque) -> deque:
+    """ Создание бинарного дерева """
+    while len(f_matrix) > 1:
+
+        weight = f_matrix[0][1] + f_matrix[1][1]
+        node = HNode(left=f_matrix.popleft()[0], right=f_matrix.popleft()[0])
+
+        for i, item in enumerate(f_matrix):
+            if weight > item[1]:
+                continue
+            else:
+                f_matrix.insert(i, (node, weight))
+                break
+        else:
+            f_matrix.append((node, weight))
+
+    return f_matrix[0][0]
+
+
+def codetable(node: HNode, code='') -> dict:
+    """ Построение кодовой таблицы """
+    if isinstance(node, str):
+        return {
+            node: code
+        }
+
+    result_dict = {}
+    left, right = node.children()
+    result_dict.update(codetable(left, code + "0"))
+    result_dict.update(codetable(right, code + "1"))
+    return result_dict
+
+
+def decode(user_str: str, codetable_d: dict) -> str:
+    """ Кодирование строки в соответствии с кодовой таблицей"""
+    result_str = ''
+    for letter in user_str:
+        result_str += codetable_d.get(letter) + ' '
+    return result_str[:-1]
+
+
+if __name__ == "__main__":
+
+    TEST_STRING = "beep boop beer!"
+    TEST_RESULT = '00 11 11 101 010 00 011 011 101 010 00 11 11 1000 1001'
+    CODETABLE_DICT = codetable(huffman_tree(frequency_matrix(TEST_STRING)))
+    DECODED_STRING = decode(TEST_STRING, CODETABLE_DICT)
+    print(DECODED_STRING)
+    print('Результат корректен?: ', TEST_RESULT == DECODED_STRING)
