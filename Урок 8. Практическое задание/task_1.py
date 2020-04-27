f25c7165f9392d04b7d3f1464e03619abac21838
Iuliia-Fedkovych <Julia_Fedkovich@mail.ru>
2020-04-27 21:39:35 +0300

    task_1

--- Урок 8. Практическое задание/task_1.py
+++ Урок 8. Практическое задание/task_1.py
@@ -16,3 +16,23 @@
 
 Итог: 6 подстрок
 """
+import hashlib
+
+
+def find_num_string(s):
+    list_string = []
+    len_s = len(s)
+    for i in range(len_s):
+        list_string.append(hashlib.sha1(s[i].encode('utf-8')).hexdigest())
+        for j in range(i + 1, len_s):
+            if j == len_s:
+                list_string.append(hashlib.sha1(s[i:].encode('utf-8')).hexdigest())
+            list_string.append(hashlib.sha1(s[i:j + 1].encode('utf-8')).hexdigest())
+    list_string.remove(hashlib.sha1(s.encode('utf-8')).hexdigest())
+
+    return len(set(list_string))
+
+
+my_string = input('Enter string: ')
+
+print(find_num_string(my_string))
