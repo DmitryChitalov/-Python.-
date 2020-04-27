ab839c50e8c32e7adf6d07b6efdb559bdf5ad70a
Sin93 <zinovev.maxim93@mail.ru>
2020-04-27 06:11:08 +0300

    задания к уроку 7

--- Урок 7. Практическое задание/task_1.py
+++ Урок 7. Практическое задание/task_1.py
@@ -9,3 +9,25 @@
 Подсказка: обратите внимание, сортируем не по возрастанию, как в примере,
 а по убыванию
 """
+
+import random
+
+
+def bubble_sort(orig_list):
+    num = 0
+    len_list = len(orig_list)
+    while num < len_list:
+        flag = 0
+        for i in list(range(len_list))[:num:-1]:
+            if orig_list[i] > orig_list[i-1]:
+                orig_list[i], orig_list[i - 1] = orig_list[i - 1], orig_list[i]
+                flag = 1
+        if not flag:
+            break
+        num += 1
+    return orig_list
+
+
+ORIG_LIST = [random.randint(-100, 100) for _ in range(30)]
+print(ORIG_LIST)
+print(bubble_sort(ORIG_LIST))
--- Урок 7. Практическое задание/task_3.py
+++ Урок 7. Практическое задание/task_3.py
@@ -6,3 +6,49 @@
 массива. Но если это слишком сложно, то используйте метод сортировки,
 который не рассматривался на уроках
 """
+
+from random import randint, choice
+
+
+def median(orig_list, mid, pivot_list=choice):
+    """
+    Поиск медианы в списке целых чисел. Для списков длиною 2m + 1
+    :param orig_list: входной список чисел
+    :param mid: индекс, равный половине количества элементов во входном списке
+    :param pivot_list: опорный элемент - выбирается случайно из входного списка
+    :return: медиана - элемент с индексом mid, в отсортированном входном списке
+    """
+    if len(orig_list) == 1:
+        # Если входной список содержит только 1 элемент,
+        # то этот элемент - и есть медиана
+        return orig_list[0]
+    # выбор опорного элемента
+    pivot = pivot_list(orig_list)
+    # заполнение списков значениями - меньше, больше и равных опорному элементу
+    left = [itm for itm in orig_list if itm < pivot]
+    right = [itm for itm in orig_list if itm > pivot]
+    pivots = [itm for itm in orig_list if itm == pivot]
+
+    if mid < len(left):
+        # если длина списка left - больше индекса медианы,
+        # то рекурсивно вызвать функцию, подать на вход список left
+        return median(left, mid)
+    elif mid < len(left) + len(pivots):
+        # если выпала медиана, то вернуть её
+        return pivots[0]
+    else:
+        # если медиана осталась в правом списке, то необходимо скорректировать индекс,
+        # с учетом того, что часть элементов остались в левом списке и списке
+        # опорных элементов, соответственно вычесть из индекса лишние элементы
+        return median(right, mid - len(left) - len(pivots))
+
+
+NUMBER_OF_ITM = int(input('Введите число элементов: '))
+if NUMBER_OF_ITM % 2 == 1:
+    ORIG_LIST = [randint(0, 100) for _ in range(NUMBER_OF_ITM)]
+    print(ORIG_LIST)
+    print(f'Медиана данного списка: {median(ORIG_LIST, (len(ORIG_LIST) // 2))}')
+    print('Для наглядности отсортированный список:')
+    print(sorted(ORIG_LIST))
+else:
+    print('Необходимо вводить количество элементов равное 2m + 1')
