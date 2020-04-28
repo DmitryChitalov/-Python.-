1e7dd53cd39af90916bdde1563c9b0901449befe
stmexx <stmexx@gmail.com>
2020-04-28 14:15:10 +0300

    Less 7 done

--- Урок 7. Практическое задание/task_1.py
+++ Урок 7. Практическое задание/task_1.py
@@ -9,3 +9,49 @@
 Подсказка: обратите внимание, сортируем не по возрастанию, как в примере,
 а по убыванию
 """
+
+from random import randint
+from timeit import timeit
+
+MAX_SIZE = 100
+NUMBER_EXECUTIONS = 10_000
+
+
+def bubble_sort(array):
+    for i in range(len(array) - 1, 0, -1):
+        for n in range(i):
+            if array[n] > array[n+1]:
+                array[n], array[n+1] = array[n+1], array[n]
+
+    return array
+
+def bubble_sort_new(array):
+    for i in range(len(array) - 1, 0, -1):
+        flag = True
+        for n in range(i):
+            if array[n] > array[n+1]:
+                array[n], array[n+1] = array[n+1], array[n]
+                flag = False
+
+        if flag == True:
+            break
+    return array
+
+
+
+
+
+numbers = [randint(-100, 100) for _ in range(MAX_SIZE)]
+print(numbers)
+print(bubble_sort_new(numbers))
+
+time1 = timeit(f'bubble_sort({numbers})',
+              setup='from __main__ import bubble_sort',
+              number=NUMBER_EXECUTIONS)
+
+time2 = timeit(f'bubble_sort_new({numbers})',
+              setup='from __main__ import bubble_sort_new',
+              number=NUMBER_EXECUTIONS)
+
+print(f'Пузырьковая сортировка: {time1}')
+print(f'Пузырьковая сортировка улучшенная : {time2}')
--- Урок 7. Практическое задание/task_3.py
+++ Урок 7. Практическое задание/task_3.py
@@ -6,3 +6,23 @@
 массива. Но если это слишком сложно, то используйте метод сортировки,
 который не рассматривался на уроках
 """
+import random
+import statistics
+
+m = int(input('Введите число : '))
+numbers = [random.randint(-100, 100) for _ in range(2 * m + 1)]
+
+
+
+
+def median(nums):
+    half = len(nums) // 2
+    nums.sort()
+    if not len(nums) % 2:
+        return (nums[half - 1] + nums[half]) / 2
+    return nums[half]
+
+
+print('Исходный массив:', numbers)
+print('Медиана:', median(numbers))
+print('Медиана statistics:', statistics.median(numbers))
