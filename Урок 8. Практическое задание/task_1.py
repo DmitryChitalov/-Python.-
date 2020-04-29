0d543c095781a58aa22c85f0aef0d080e3419ad6
Artem <shalyapinartyom5@gmail.com>
2020-04-30 01:11:46 +0500

    lesson-8 homework

--- Урок 8. Практическое задание/task_1.py
+++ Урок 8. Практическое задание/task_1.py
@@ -16,3 +16,38 @@
 
 Итог: 6 подстрок
 """
+import hashlib
+from itertools import combinations
+
+
+def num_of_substring(s):
+    """
+    Функция принимает на вход строку и определяет кол-во подстрок
+    """
+    # Определяем хэш строки
+    s_hash = hashlib.sha1(s.encode('utf-8')).hexdigest()
+    # Множества для подстрок и их хешей
+    sub_str = set()
+    sub_hash = set()
+    # Определение с помощью функции combinations всех подстрок в строке
+    for x, y in combinations(range(len(s) + 1), 2):
+        if s[x:y] != " ":
+            sub_str.add(s[x:y])
+        else:
+            continue
+    # Определение хешей подстрок
+    for item in sub_str:
+        h = hashlib.sha1(item.encode('utf-8')).hexdigest()
+        sub_hash.add(h)
+    # Удаление из множества хеша строки
+    sub_hash.remove(s_hash)
+    # Удаление из множества исходной строки
+    sub_str.remove(s)
+    # Вывод подстрок
+    for i in sub_str:
+        print(i, end="\n")
+    return f"Итог: {len(sub_hash)} подстрок"
+
+
+S = input("Строка: ")
+print(num_of_substring(S))
--- Урок 8. Практическое задание/task_2.py
+++ Урок 8. Практическое задание/task_2.py
@@ -8,3 +8,60 @@ s = "beep boop beer!"
 Результат:
 00 11 11 101 010 00 011 011 101 010 00 11 11 1000 1001
 """
+from collections import Counter, deque
+
+
+class MyNode:
+
+    def __init__(self, left=None, right=None):
+        self.left = left
+        self.right = right
+
+
+def huffman_tree(s):
+    """'
+    Построение дерева для кодирования по алгоритму Хаффмана
+    """
+    # Подсчет частоты значений
+    count_s = Counter(s)
+    # Сортировка частот в порядке неубывания
+    sorted_s = deque(sorted(count_s.items(), key=lambda item: item[1]))
+    # Создание двоичного дерева
+    while len(sorted_s) > 1:
+        weight = sorted_s[0][1] + sorted_s[1][1]
+        node = MyNode(left=sorted_s.popleft()[0], right=sorted_s.popleft()[0])
+        # Перемещение узлов и веса на нужное место в список
+        for i, item in enumerate(sorted_s):
+            if weight > item[1]:
+                continue
+            else:
+                sorted_s.insert(i, (node, weight))
+                break
+        else:
+            sorted_s.append((node, weight))
+    return sorted_s[0][0]
+
+
+code_table = dict()
+
+
+def huffman_code(tree, path=''):
+    """
+    Составление таблицы кодирования по дереву
+    """
+    if not isinstance(tree, MyNode):
+        code_table[tree] = path
+    else:
+        huffman_code(tree.left, path=f'{path}0')
+        huffman_code(tree.right, path=f'{path}1')
+
+
+S = input('Введите строку для кодировки: ')
+
+# Формирование таблицы кодирования
+huffman_code(huffman_tree(S))
+# Вывод кода
+for i in S:
+    print(code_table[i], end=' ')
+
+print()
