2f032f2af6b275e0f9d0607003ba0752ad22b570
Andrey Kuzminykh <56219243+AKZMH@users.noreply.github.com>
2020-04-29 17:57:40 +0300

    Add files via upload

--- Урок 7. Практическое задание/task_1.py
+++ Урок 7. Практическое задание/task_1.py
@@ -10,6 +10,11 @@
 а по убыванию
 """
 import random
+import timeit
+
+""" Судя по проведенным замерам скорость алгоритма с "отсечкой" в случае его сортировки значительно выше, соответсвенно
+алгоритм эффективней и быстрее.
+"""
 
 
 def bubble_sort(orig_list):
@@ -28,6 +33,48 @@ def bubble_sort(orig_list):
     return orig_list
 
 
-or_list = [random.randint(-100, 100) for _ in range(10)]
-print(f'Изначальный список - {or_list}')
-print(f'Отсортированныый список - {bubble_sort(or_list)}')
+user_list = [random.randint(-100, 100) for _ in range(10)]
+# print(f'Изначальный список - {or_list}')
+# print(f'Отсортированныый список - {bubble_sort(or_list)}')
+
+# замеры 10
+print(timeit.timeit("bubble_sort(user_list)", number=1000, globals=globals()))  # 0.002342854999999991
+
+user_list_1 = [random.randint(-100, 100) for _ in range(100)]
+
+# замеры 100
+print(timeit.timeit("bubble_sort(user_list_1)", number=1000, globals=globals()))  # 0.030227458999999998
+
+user_list_2 = [random.randint(-100, 100) for _ in range(1000)]
+
+# замеры 1000
+print(timeit.timeit("bubble_sort(user_list_2)", number=1000, globals=globals()))  # 0.301444718
+
+
+def bubble_sort_1(orig_list):
+    n = 1
+    while n < len(orig_list):
+        for i in range(len(orig_list)-n):
+            if orig_list[i] > orig_list[i+1]:
+                orig_list[i], orig_list[i+1] = orig_list[i+1], orig_list[i]
+        n += 1
+    return orig_list
+
+
+my_list = [random.randint(-100, 100) for _ in range(10)]
+
+# замеры 10
+print(timeit.timeit("bubble_sort_1(my_list)", number=1000, globals=globals()))  # 0.02234721399999999
+
+my_list_1 = [random.randint(-100, 100) for _ in range(100)]
+
+# замеры 100
+print(timeit.timeit("bubble_sort_1(my_list_1)", number=1000, globals=globals()))  # 0.559923608
+
+my_list_2 = [random.randint(-100, 100) for _ in range(1000)]
+
+# замеры 1000
+print(timeit.timeit("bubble_sort_1(my_list_2)", number=1000, globals=globals()))  # 53.539715445999995
+
+
+
--- Урок 7. Практическое задание/task_3.py
+++ Урок 7. Практическое задание/task_3.py
@@ -6,3 +6,42 @@
 массива. Но если это слишком сложно, то используйте метод сортировки,
 который не рассматривался на уроках
 """
+import random
+import timeit
+import statistics
+
+
+def find_median(arr):
+    for i in range(len(arr)):
+        small = equal = big = 0
+        for j in range(len(arr)):
+            if arr[i] < arr[j]:
+                small += 1
+            elif arr[i] > arr[j]:
+                big += 1
+            else:
+                equal += 1
+        equal -= 1
+
+        if small == big or small == equal + big or big == small + equal or \
+                (equal > 1 and abs(big - small) < equal):
+            return arr[i]
+
+
+SIZE = 5
+LIMIT = 100
+my_arr = [random.randrange(0, LIMIT) for _ in range(2 * SIZE + 1)]
+print(my_arr)
+print(f'медиана: {find_median(my_arr)}')
+print(timeit.timeit("find_median(my_arr)", number=1000, globals=globals()))  # 0.009351991999999996
+
+"""
+ниже решение с помощью инструмента модуля statistics, работает в 10 раз быстрее
+"""
+print(f'\n\n{my_arr}')
+print(f'Медиана = {statistics.median(my_arr)}')
+print(timeit.timeit('statistics.median(my_arr)', number=1000, globals=globals()))  # 0.0009106200000000009
+
+
+
+
