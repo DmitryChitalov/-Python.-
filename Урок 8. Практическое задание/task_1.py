23133901b70cafcd361723a9dc336f74954e8dbf
em <moy_garb@bk.ru>
2020-04-29 15:54:05 +0300

    lesson 8

--- Урок 8. Практическое задание/task_1.py
+++ Урок 8. Практическое задание/task_1.py
@@ -16,3 +16,20 @@
 
 Итог: 6 подстрок
 """
+
+import hashlib
+
+
+STR = 'papa'
+# сразу добавляем в список хэшей исходную строку
+HASHS = [hashlib.sha1(bytes(STR, encoding='ascii')).hexdigest()]
+LEN = len(STR)
+UNIQUE = []
+for i in range(LEN):
+    for j in range(i, LEN):
+        string = STR[i:j + 1]
+        h = hashlib.sha1(bytes(string, encoding='ascii'))
+        if h.hexdigest() not in HASHS:
+            HASHS.append(h.hexdigest())
+            UNIQUE.append(string)
+print(f'В строке "{STR}" имеется {len(UNIQUE)} уникальных подстрок: {UNIQUE}')
