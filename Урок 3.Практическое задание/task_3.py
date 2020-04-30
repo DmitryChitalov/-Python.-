11bf44e608b50cb9178bd86c62f1f5b98fd3e185
dakostyleva <dashatkacheva@gmail.com>
2020-04-30 14:11:03 +0300

    Lesson 3

--- Урок 3.Практическое задание/task_3.py
+++ Урок 3.Практическое задание/task_3.py
@@ -11,3 +11,24 @@
 6 позиции, а минимальное число  -49 стоит на    0 позиции
 [-49, 26, 41, 75, 23, 52, 88, 60, 69, -18]
 """
+import random
+
+array = [random.randint(-100, 100) for i in range(10)]
+print(array)
+
+max = max(array)
+min = min(array)
+
+print(f'Максимальное число в массиве: {max}')
+print(f'Минимальное число в массиве: {min}')
+
+a = array.index(min)
+b = array.index(max)
+
+array.remove(min)
+array.remove(max)
+
+array.insert(a, max)
+array.insert(b, min)
+
+print(f"Новый массив: {array}")
