567f5db05c3fc2038d62a93019bdcfaf9908efb0
dakostyleva <dashatkacheva@gmail.com>
2020-04-30 14:11:17 +0300

    Lesson 3

--- Урок 3.Практическое задание/task_6.py
+++ Урок 3.Практическое задание/task_6.py
@@ -13,3 +13,21 @@
 Массив: [88, 58, 50, 77, 49, 6, 42, 67, 14, 79]
 Сумма элементов между минимальным (6)  и максимальным (88) элементами: 234
 """
+import random
+
+array = [random.randint(0, 100) for i in range(10)]
+print(array)
+
+a = min(array)
+b = max(array)
+a_ind = array.index(a) #7
+b_ind = array.index(b) #3
+
+if a_ind < b_ind:
+    new_array = [el for el in array if (array.index(el) > a_ind and array.index(el) < b_ind)]
+else:
+    new_array = [el for el in array if (array.index(el) < a_ind and array.index(el) > b_ind)]
+
+print(new_array)
+sum_arr = sum(new_array)
+print(f"Минимальное число массива {a}, максимальное число - {b}, сумма элементов между ними: {sum_arr}")
