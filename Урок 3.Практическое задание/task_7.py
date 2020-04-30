e8e1020fa727fcb0a0a0affff8977e1657575ac1
dakostyleva <dashatkacheva@gmail.com>
2020-04-30 14:11:20 +0300

    Lesson 3

--- Урок 3.Практическое задание/task_7.py
+++ Урок 3.Практическое задание/task_7.py
@@ -7,3 +7,15 @@
 Наименьший элемент: -86, встречается в этом массиве 1 раз
 Второй наименьший элемент: -73
 """
+import random
+
+array = [random.randint(0, 100) for i in range(20)]
+print(array)
+
+a = min(array)
+
+array.remove(a)
+
+b = min(array)
+
+print(f"Наименьшие числа массива: {a} и {b}")
