28653841f4854d9a29e395bc7447b63a39adb080
Mixiz <lavrikov.denis@gmail.com>
2020-05-01 22:29:01 +0500

    Поправлено кодирование/декодирование

--- Урок 8. Практическое задание/task_2.py
+++ Урок 8. Практическое задание/task_2.py
@@ -19,7 +19,7 @@ class TreeNode:
 
 class Haffman:
     _codes = dict()
-    _tree = None
+    _tree = TreeNode(None, None)
 
     # Разбор входной строки и назначение кода каждому символу
     def parse(self, string):
@@ -44,11 +44,6 @@ class Haffman:
         # Разбираем коды
         self._tree = elements[0][0]
         self.haffman_code(self._tree)
-        # Кодируем исходную строку
-        result = ''
-        for i in string:
-            result += self._codes[i] + ' '
-        return result
 
     @classmethod
     # Рекурсивно обходим дерево и присваиваем коды каждому символу
@@ -59,30 +54,43 @@ class Haffman:
             cls.haffman_code(tree.left, path=f'{path}0')
             cls.haffman_code(tree.right, path=f'{path}1')
 
-    @classmethod
     # Декодирует на основе текущего словаря
-    def decode(cls, string):
+    def decode(self, string):
         result = ''
-        cur_elem = cls._tree
+        cur_elem = self._tree
         for i in string:
+            # Выбираем, по какой ветке пойти
+            if i == '0':
+                cur_elem = cur_elem.left
+            else:
+                cur_elem = cur_elem.right
+            # Проверяем, что достигли листа
             if not isinstance(cur_elem, TreeNode):
                 result += cur_elem
-                cur_elem = cls._tree
-            else:
-                # Выбираем, по какой ветке пойти
-                if i == '0':
-                    cur_elem = cur_elem.left
-                else:
-                    cur_elem = cur_elem.right
+                cur_elem = self._tree
+        return result
+
+    # Кодирует строку
+    def encode(self, string):
+        result = ''
+        try:
+            for i in string:
+                result += self._codes[i] + ' '
+        except KeyError:
+            print(f'Не удалось закодировать строку, отсутствует код для "{i}"')
+            result = ''
         return result
 
 
 if __name__ == '__main__':
     haffman = Haffman()
-    res = haffman.parse('beep boop beer!')
+    haffman.parse('beep boop beer!')
+    print(haffman.encode('beer bro porez'))
+    print(haffman.encode('beer bro pore'))
+    res = haffman.encode('beep boop beer!')
     print(res)
     # Также можно декодировать зашифрованную строку
     code = res.replace(' ', '')
-    res = Haffman.decode(code)
+    res = haffman.decode(code)
     print(res)
 
