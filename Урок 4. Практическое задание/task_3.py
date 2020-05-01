30af478912e2c024f131d7e1ca13e1fea030a78f
minkika <lubov@golubeva.su>
2020-05-01 20:53:56 +0300

    timer fixed

--- Урок 4. Практическое задание/task_3.py
+++ Урок 4. Практическое задание/task_3.py
@@ -3,24 +3,30 @@ import time
 def legacy_timeit(func):
     def tmp(n):
         t = time.time()
-        res = func(n)
-        print(f"Execution time: {time.time()-t}")
+        for i in range(1000):
+            res = func(n)
+        print(f"Average time: {(time.time()-t)/1000}")
         return res
 
     return tmp
 
 @legacy_timeit
-def prime(num):
-    count = 1
-    next_num = 2
+def sieve(n):
+    p_func = {4: 10, 25: 10 ** 2, 168: 10 ** 3, 1229: 10 ** 4, 9592: 10 ** 5, 78498: 10 ** 6, 664579: 10 ** 7,
+              5761455: 10 ** 8, 50847534: 10 ** 9, 455052511: 10 ** 10}
+    for key in p_func.keys():
+        if n <= key:
+            length = p_func[key]
+            break
+    s = [x for x in range(2, length + 1) if
+         x not in [i for sub in [list(range(2 * j, length + 1, j)) for j in range(2, length // 2)] for i in sub]]
+    return s[n - 1]
 
-    while count < num:
-        next_num += 1
-        for i in range(2, int(next_num ** 0.5) + 1):
-            if next_num % i == 0:
-                break
-        else:
-            count += 1
-    return next_num
+sieve(20)
 
-prime(30)
+# Average time: 0.003087822437286377
+# Average time: 0.0030162153244018557
+# Average time: 0.0030391948223114015
+
+# task_2.sieve(20)"
+# 1000 loops, best of 5: 3.17 msec per loop
