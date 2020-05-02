f1b854597692f5018a01b153a778b31ae3e3e6df
Kirill Ivanov <kirusha1991@gmail.com>
2020-05-02 19:54:15 +0500

    Добавил решение 7 урока

--- Урок 7. Практическое задание/task_1.py
+++ Урок 7. Практическое задание/task_1.py
@@ -9,3 +9,37 @@
 Подсказка: обратите внимание, сортируем не по возрастанию, как в примере,
 а по убыванию
 """
+from random import randint
+from timeit import Timer
+RND_LIST_1 = [randint(-100, 100) for _ in range(1000)]
+RND_LIST_2 = [randint(-100, 100) for _ in range(1000)]
+
+
+def bubble_1(my_list):
+    """Простая сортировка пузырьком"""
+    for _ in range(len(my_list)):
+        for i in range(len(my_list)-1):
+            if my_list[i] < my_list[i + 1]:
+                my_list[i], my_list[i + 1] = my_list[i + 1], my_list[i]
+    return my_list
+
+
+def bubble_2(my_list):
+    """Улучшеная сортировка пузырьком"""
+    for _ in range(len(my_list)):
+        complete = True
+        for i in range(len(my_list)-1):
+            if my_list[i] < my_list[i + 1]:
+                complete = False
+                my_list[i], my_list[i + 1] = my_list[i + 1], my_list[i]
+        if complete:
+            break
+    return my_list
+
+
+T1 = Timer("bubble_1(RND_LIST_1)", "from __main__ import bubble_1, RND_LIST_1")
+T2 = Timer("bubble_2(RND_LIST_2)", "from __main__ import bubble_2, RND_LIST_2")
+print(f"Список случайных чисел после обычного варианта сортировки:"
+      f"\n{bubble_1(RND_LIST_1)}\nСортировка заняла: {T1.timeit(number=10)}")
+print(f"Список случайных чисел после улучшенного варианта сортировки:"
+      f"\n{bubble_2(RND_LIST_2)}\nСортировка заняла: {T2.timeit(number=10)}")
