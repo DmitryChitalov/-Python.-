1778d4db45a9923ff74174c946f62acd9775fa74
TerryNight <lizkakryuchkova@gmail.com>
2020-05-03 14:45:44 +0300

    8.1

--- Урок 8. Практическое задание/task_1.py
+++ Урок 8. Практическое задание/task_1.py
@@ -16,3 +16,13 @@
 
 Итог: 6 подстрок
 """
+import hashlib
+
+MY_STRING = input('Введите строку - ').lower()
+NEW_STRING = set()
+
+for i in range(len(MY_STRING)):
+	for j in range(i,len(MY_STRING)):
+		NEW_STRING.add(hashlib.sha1(MY_STRING[i:j].encode('utf-8')).hexdigest())
+
+print(f'Итого {len(list(NEW_STRING))} подстрок')
