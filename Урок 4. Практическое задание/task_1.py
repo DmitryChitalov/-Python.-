b8b42fe0562d7843ac343ae0e3d95032eb0357bd
alex vydrin <tapat@mail.ru>
2020-05-04 00:59:26 +0300

    py_algos_lesson_4

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -12,3 +12,142 @@
 ВНИМАНИЕ: ЗАДАНИЯ, В КОТОРЫХ БУДУТ ГОЛЫЕ ЦИФРЫ ЗАМЕРОВ (БЕЗ АНАЛИТИКИ)
 БУДУТ ПРИНИМАТЬСЯ С ОЦЕНКОЙ УДОВЛЕТВОРИТЕЛЬНО
 """
+
+# Посчитать четные и нечетные цифры введенного натурального числа.
+# Например, если введено число 34560, то у него 3 четные цифры
+# (4, 6 и 0) и 2 нечетные (3 и 5).
+
+
+import timeit
+
+
+# Вариант 1 - цикл
+def get_even_odd_1_cycle(number):
+    number_even, number_odd = 0, 0  # Счетчики четных и нечетных цифр
+    number_i = number  # Число для использования в итерациях (будет изменяться)
+    while True:
+        if number_i % 10 % 2:
+            number_odd += 1
+        else:
+            number_even += 1
+        number_i = number_i // 10
+        if not number_i:
+            break
+    return number_even, number_odd
+
+
+# Вариант 2 - рекурсия
+def get_even_odd_2_recursion(number):
+    # условие завершения рекурсии
+    if not number:
+        return 0, 0
+    # условие рекурсивного вызова
+    number_even, number_odd = get_even_odd_2_recursion(number // 10)
+    if number % 10 % 2:
+        number_odd += 1
+    else:
+        number_even += 1
+    return number_even, number_odd
+
+
+# Вариант 3 - цикл - оптимизировано
+def get_even_odd_3_cycle(number):
+    number_even, number_odd = 0, 0  # Счетчики четных и нечетных цифр
+    for c in str(number):
+        if c in ('1', '3', '5', '7', '9'):
+            number_odd += 1
+        else:
+            number_even += 1
+    return number_even, number_odd
+
+
+# Вариант 4 - рекурсия - оптимизировано
+def get_even_odd_4_recursion(number):
+    # условие завершения рекурсии
+    if not number:
+        return 0, 0
+    # условие рекурсивного вызова
+    number_even, number_odd = get_even_odd_4_recursion(str(number)[:-1])
+    if str(number)[-1] in ('1', '3', '5', '7', '9'):
+        number_odd += 1
+    else:
+        number_even += 1
+    return number_even, number_odd
+
+
+# Вариант 5 - рекурсия - оптимизировано еще раз
+def get_even_odd_5_recursion(number):
+    # условие завершения рекурсии
+    if not number:
+        return 0, 0
+    # условие рекурсивного вызова
+    if isinstance(number, str):
+        str_number = number
+    else:
+        str_number = str(number)
+    number_even, number_odd = get_even_odd_4_recursion(str_number[:-1])
+    if str_number[-1] in ('1', '3', '5', '7', '9'):
+        number_odd += 1
+    else:
+        number_even += 1
+    return number_even, number_odd
+
+
+number_for_test = 987654321987654321987654321987654321
+print(get_even_odd_1_cycle(number_for_test))
+print(get_even_odd_2_recursion(number_for_test))
+print(get_even_odd_3_cycle(number_for_test))
+print(get_even_odd_4_recursion(number_for_test))
+print(get_even_odd_5_recursion(number_for_test))
+
+print("get_even_odd_1_cycle()    =",
+      timeit.timeit("get_even_odd_1_cycle(987654321987654321987654321987654321)",
+                    setup="from __main__ import get_even_odd_1_cycle",
+                    number=1000))
+print("get_even_odd_2_recursion()=",
+      timeit.timeit("get_even_odd_2_recursion(987654321987654321987654321987654321)",
+                    setup="from __main__ import get_even_odd_2_recursion",
+                    number=1000))
+print("get_even_odd_3_cycle()    =",
+      timeit.timeit("get_even_odd_3_cycle(987654321987654321987654321987654321)",
+                    setup="from __main__ import get_even_odd_3_cycle",
+                    number=1000))
+print("get_even_odd_4_recursion()=",
+      timeit.timeit("get_even_odd_4_recursion(987654321987654321987654321987654321)",
+                    setup="from __main__ import get_even_odd_4_recursion",
+                    number=1000))
+print("get_even_odd_5_recursion()=",
+      timeit.timeit("get_even_odd_5_recursion(987654321987654321987654321987654321)",
+                    setup="from __main__ import get_even_odd_5_recursion",
+                    number=1000))
+
+
+# Решил задачу с использованием цикла и рекурсии
+# Затем каждую функцию попытался оптимизировать, преобразовав число в строку и работая с ней как с массивом
+# Делаю анализ:
+# get_even_odd_1_cycle()    = 0.015200099999999998
+# get_even_odd_2_recursion()= 0.0255743
+# get_even_odd_3_cycle()    = 0.008668200000000001
+# get_even_odd_4_recursion()= 0.041118299999999997
+
+# Циклы работают быстрее, чем рекурсии, это ожидаемо,
+# так как в общем случае сложность цикла меньше чем сложность рекурсии из-за использования рекурсией стека вызовов
+# При оптимизации цикла есть значительный эффект,
+# так как уменьшаем количество математических операций с большими числами, преобразовав число в строку лишь однажды
+# При оптимизации рекурсии эффект обратный,
+# так как преобразование числа в строку происходит при каждом рекурсивном вызове
+# На основании анализа решил еще раз оптимизировать рекурсию, исключив повторные преобрахования типов
+# Делаю анализ:
+
+# get_even_odd_1_cycle()    = 0.018241599999999997
+# get_even_odd_2_recursion()= 0.026952899999999995
+# get_even_odd_3_cycle()    = 0.00855460000000001
+# get_even_odd_4_recursion()= 0.0411112
+# get_even_odd_5_recursion()= 0.04128209999999999
+
+# эффекта от дополнительной оптимизации не получил
+# видимо функция isinstance(), вызваемая при каждом рекурсивном методе тоже достаточно затратная
+
+# Вывод:
+# из пяти проанализированных алгоритмов самым оптимальным в данном случае является
+# get_even_odd_3_cycle - цикл с минимизацией математических операций с большими числами
--- Урок 4. Практическое задание/task_2.py
+++ Урок 4. Практическое задание/task_2.py
@@ -12,3 +12,98 @@
 ВНИМАНИЕ: ЗАДАНИЯ, В КОТОРЫХ БУДУТ ГОЛЫЕ ЦИФРЫ ЗАМЕРОВ (БЕЗ АНАЛИТИКИ)
 БУДУТ ПРИНИМАТЬСЯ С ОЦЕНКОЙ УДОВЛЕТВОРИТЕЛЬНО
 """
+
+
+import timeit
+
+
+def get_prime_number_eratosthenes(number):
+    if number > 1229:
+        # Функция разработана только для параметра <= 1229
+        # так как решето эратосфена для целей задачи рассчитываем до 10000
+        # при необходимости этот параметр можем увеличить
+        return None  # Признак ошибочного завершения
+
+    lst = [i for i in range(2, 10000)]
+
+    for n in range(len(lst)):
+        p = lst[n]
+        if not p:
+            continue
+        k = n
+        while True:
+            if k + p >= len(lst):
+                break
+            lst[k + p] = 0
+            k += p
+
+    result = [i for i in lst if i]
+
+    return result[number - 1]
+
+
+def get_prime_number_simple(number):
+    i = 1
+    prime_number = 2
+    while i < number:
+        prime_number += 1
+        for k in range(2, prime_number):
+            if prime_number % k == 0:
+                break
+        else:
+            i += 1
+    return prime_number
+
+
+print(get_prime_number_eratosthenes(100))
+print(get_prime_number_simple(100))
+
+# 10
+print("get_prime_number_eratosthenes(10)=",
+      timeit.timeit("get_prime_number_eratosthenes(10)",
+                    setup="from __main__ import get_prime_number_eratosthenes",
+                    number=50))
+print("get_prime_number_simple(10)=",
+      timeit.timeit("get_prime_number_simple(10)",
+                    setup="from __main__ import get_prime_number_simple",
+                    number=50))
+
+# 100
+print("get_prime_number_eratosthenes(100)=",
+      timeit.timeit("get_prime_number_eratosthenes(100)",
+                    setup="from __main__ import get_prime_number_eratosthenes",
+                    number=50))
+print("get_prime_number_simple(100)=",
+      timeit.timeit("get_prime_number_simple(100)",
+                    setup="from __main__ import get_prime_number_simple",
+                    number=50))
+
+# 1000
+print("get_prime_number_eratosthenes(1000)=",
+      timeit.timeit("get_prime_number_eratosthenes(1000)",
+                    setup="from __main__ import get_prime_number_eratosthenes",
+                    number=50))
+print("get_prime_number_simple(1000)=",
+      timeit.timeit("get_prime_number_simple(1000)",
+                    setup="from __main__ import get_prime_number_simple",
+                    number=50))
+
+# Анализируем:
+
+# get_prime_number_eratosthenes(10)= 0.6521514
+# get_prime_number_simple(10)= 0.0017209000000000252
+# get_prime_number_eratosthenes(100)= 0.6730517
+# get_prime_number_simple(100)= 0.17253659999999993
+# get_prime_number_eratosthenes(1000)= 0.6638286999999998
+# get_prime_number_simple(1000)= 22.085386
+
+# Вывод:
+# При использовании решета Эратосфена время постоянно
+# так как сложность алгоритма O(nlog(log n))
+# Важно! в этой функции n-константа и равна 10000, а не входному параметру
+# кажется на вебинаре это не обсуждалось и мне кажется это не всем было очевидно - например мне)
+# таким образом, сложность алгоритма решета Эратосфена в данной реализации НЕ завитит от входящего параметра!
+# Сложность простого алгорится равняется О(n^2) - квадратичная
+# таким образом, сложность простого алгоритма СИЛЬНО завитит от входящего параметра!
+# Вывод:
+# Использование алгоритма решета Эратосфена оправдано, если предполагается его использование с большими числами
