c137f5b65b23993037a2f7c68ecbed6ef29156a5
Ilya Gradoboev <grilyage@ya.ru>
2020-05-04 17:05:32 +0300

    Lesson_4
    Домашнее задание

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -12,3 +12,47 @@
 ВНИМАНИЕ: ЗАДАНИЯ, В КОТОРЫХ БУДУТ ГОЛЫЕ ЦИФРЫ ЗАМЕРОВ (БЕЗ АНАЛИТИКИ)
 БУДУТ ПРИНИМАТЬСЯ С ОЦЕНКОЙ УДОВЛЕТВОРИТЕЛЬНО
 """
+
+import timeit
+
+NUMBER = "123884906456435435345345345656456546564564562421313133445645766456346345242344667678658765774575"
+LEN = len(NUMBER)
+NUM = int(NUMBER)
+def rev(i, num, newi, newnum):
+    if i == 0:
+        return #print(f'Перевернутое число: {newnum}')
+    else:
+        i -= 1
+        digit = num // int('1' + i * '0')
+        num -= int(str(digit) + i * '0')
+        newnum += int(str(digit) + newi * '0')
+        rev(i, num, newi+1, newnum)
+"""Использую свой алгоритм с рекурсией, не самое быстрое решение
+Результат 0.590"""
+
+def cycle_rev(num):
+    I = len(num)
+    NUM = int(num)
+    NewNUM = 0
+    NewI = 0
+    while I > 0:
+        I -= 1
+        digit = NUM // int('1' + I * '0')
+        NUM -= int(str(digit) + I * '0')
+        NewNUM += int(str(digit) + NewI * '0')
+        NewI += 1
+"""Использую свой алгоритм с циклом
+Чуть быстрее 0.497"""
+
+def flip(numb, fliped = 0):
+    if numb == 0:
+        return #print(fliped)
+    else:
+        fliped = (fliped*10) + (numb % 10)
+        numb = numb // 10
+        flip(numb, fliped)
+"""Эталонный алгоритм, результат 0.103"""
+
+print(timeit.timeit('rev(LEN, NUM, 0, 0)', setup='from __main__ import rev, NUMBER, LEN, NUM', number=1000))
+print(timeit.timeit('cycle_rev(NUMBER)', setup='from __main__ import cycle_rev, NUMBER, LEN, NUM', number=1000))
+print(timeit.timeit('flip(NUM)', setup='from __main__ import flip, NUMBER, LEN, NUM', number=1000))
--- Урок 4. Практическое задание/task_2.py
+++ Урок 4. Практическое задание/task_2.py
@@ -12,3 +12,124 @@
 ВНИМАНИЕ: ЗАДАНИЯ, В КОТОРЫХ БУДУТ ГОЛЫЕ ЦИФРЫ ЗАМЕРОВ (БЕЗ АНАЛИТИКИ)
 БУДУТ ПРИНИМАТЬСЯ С ОЦЕНКОЙ УДОВЛЕТВОРИТЕЛЬНО
 """
+import cProfile
+import timeit
+
+target = int(input('Введите номер простого числа: '))
+
+def simple_n(target, simple=[]):
+    for i in range(2, 1000000000):
+        k = 0
+        for x in range(2, i):
+            if i % x == 0:
+                k += 1
+        if k == 0:
+            simple.append(i)
+        if len(simple) == target:
+            break
+    return #print(f'{target}-ое простое число: {simple[-1]}')
+"""
+Результат для 100-го простого числа  0.019 seconds
+Результат для 500-го простого числа  1.053 seconds
+Результат для 1000-го простого числа 5.615 seconds
+Результат для 2000-го простого числа 29.064 seconds
+Самый простой и медленный алгоритм. 
+Ищем количество делителей у числа из диапазона от 2 до самого числа невключительно,
+если таковых нет - объявляем простым числом
+Предположительная сложность 2n2"""
+
+
+def simple_n1(target, simple=[]):
+    for i in range(2, 1000000000):
+        for x in range(2, i):
+            if i % x == 0:
+                break
+        else:
+            simple.append(i)
+        if len(simple) == target:
+            break
+    return #print(f'{target}-ое простое число: {simple[-1]}')
+"""
+Результат для 100-го простого числа  0.004 seconds
+Результат для 500-го простого числа  0.170 seconds
+Результат для 1000-го простого числа 0.671 seconds
+Результат для 2000-го простого числа 3.169 seconds
+Перестаем искать количество делителей, если находим хотя бы один - движемся дальше.
+Почти на порядок быстрее предыдущего
+Предположительная сложность n2"""
+
+def simple_n2(target, simple=[]):
+    for i in range(2, 1000000000):
+        for x in simple:
+            if i % x == 0:
+                break
+        else:
+            simple.append(i)
+        if len(simple) == target:
+            break
+    return #print(f'{target}-ое простое число: {simple[-1]}')
+"""
+Результат для 100-го простого числа 0.0024 seconds
+Результат для 500-го простого числа 0.0353 seconds
+Результат для 1000-го простого числа 0.081 seconds
+Результат для 2000-го простого числа 0.331 seconds
+Используем только простые числа в качестве делителя 
+Почти на порядок быстрее предыдущего
+Предположительная сложность n2"""
+
+def simple_n3(target, simple=[]):
+    for i in range(2, 1000000000):
+        if (i > 10):
+            if (i % 2 == 0) or (i % 10 == 5):
+                continue
+        for x in simple:
+            if i % x == 0:
+                break
+        else:
+            simple.append(i)
+        if len(simple) == target:
+            break
+    return #print(f'{target}-ое простое число: {simple[-1]}')
+"""
+Результат для 100-го простого числа 0.0024 seconds
+Результат для 500-го простого числа 0.0337 seconds
+Результат для 1000-го простого числа 0.079 seconds
+Результат для 2000-го простого числа 0.323 seconds
+Данная оптимизация становится актуальна при поиске простых чисел с индексами больше 100,
+но стабильного прироста не показывает
+Предположительная сложность n2"""
+
+def simples():
+    a = 2
+    while True:
+        for i in range(2, a):
+            if a % i == 0:
+                break
+        else:
+            yield a
+        a += 1
+
+
+def simples_n(target, simple=0):
+    i = 0
+    num = simples()
+    while i != target:
+        simple = next(num)
+        i += 1
+    #print((f'{target}-ое простое число: {simple}'))
+
+"""
+Результат для 2000-го простого числа 3.008 seconds
+Данный алгоритм не обладает преимуществами, медленный, сложный"""
+
+
+cProfile.run('simple_n(target)')
+cProfile.run('simple_n1(target)')
+cProfile.run('simple_n2(target)')
+cProfile.run('simple_n3(target)')
+cProfile.run('simples_n(target)')
+
+
+#print(timeit.timeit('simple_n2(target)', setup='from __main__ import simple_n2, target', number=10000))
+#print(timeit.timeit('simple_n3(target)', setup='from __main__ import simple_n3, target', number=10000))
+#print(timeit.timeit('simples_n(target)', setup='from __main__ import simples_n, simples, target', number=1000))
