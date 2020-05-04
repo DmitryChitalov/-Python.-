99618f93303261acb560646ae4fb3ed09cdec51a
Ershgun <ershgun@mail.ru>
2020-05-04 09:47:44 +0300

    Lesson_04
    Домашнее задание к уроку 4

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -12,3 +12,168 @@
 ВНИМАНИЕ: ЗАДАНИЯ, В КОТОРЫХ БУДУТ ГОЛЫЕ ЦИФРЫ ЗАМЕРОВ (БЕЗ АНАЛИТИКИ)
 БУДУТ ПРИНИМАТЬСЯ С ОЦЕНКОЙ УДОВЛЕТВОРИТЕЛЬНО
 """
+
+import cProfile
+import timeit
+import sys
+
+sys.setrecursionlimit(150000)
+
+# Первый пример
+# Урок 2. Задание 3.
+# Сформировать из введенного числа обратное по порядку входящих в него
+# цифр и вывести на экран. Например, если введено число 3486,
+# то надо вывести число 6843.
+
+# Рекурсия
+def revers(enter_num, revers_num=0):
+    if enter_num == 0:
+        return
+    else:
+        num = enter_num % 10
+        revers_num = (revers_num + num / 10) * 10
+        enter_num //= 10
+        revers(enter_num, revers_num)
+
+# Цикл
+def revers_2(enter_num, revers_num=0):
+    while enter_num != 0:
+        num = enter_num % 10
+        revers_num = (revers_num + num / 10) * 10
+        enter_num //= 10
+    return revers_num
+
+# Срез
+def revers_3(enter_num):
+    enter_num = str(enter_num)
+    revers_num = enter_num[::-1]
+    return revers_num
+
+
+print('Пример №1')
+enter_num = int(input('Введите целое число: '))
+revers(enter_num, revers_num=0)
+revers_2(enter_num, revers_num=0)
+revers_3(enter_num)
+
+
+print('Число на оборот на рекурсиях: ', timeit.timeit(f'revers({enter_num})', setup='from __main__ import revers', number=10000))
+print('Число на оборот на циклах: ', timeit.timeit(f'revers_2({enter_num})', setup='from __main__ import revers_2', number=10000))
+print('Число на оборот на срезах: ', timeit.timeit(f'revers_3({enter_num})', setup='from __main__ import revers_3', number=10000))
+
+cProfile.run('revers(10000000000)')
+cProfile.run('revers_2(10000000000)')
+cProfile.run('revers_3(10000000000)')
+
+# Вывод: Я применил 3 вида реализации задачи ( рекурсия, цикл, срез). Согласно полученным результатам видно что срез наиболее быстрый в плане выполнения
+# задачи. Рекурсия и цикл имеют арифмитические действия поэтому они проигрывают в скорости по сравнению со срезом в котором отсутствуют арифмитические действия.
+# Следовательно в подобных задачах оптимально использовать срез.
+
+print('--------------------------------------------------')
+print('--------------------------------------------------')
+
+# Второй пример
+# Урок 2. Задание 4.
+# Найти сумму n элементов следующего ряда чисел: 1 -0.5 0.25 -0.125 ...
+
+def cycle(n=10, val=1, sum=0):
+
+    for i in range(n):
+        sum += val
+        val = -(val / 2)
+
+def recursion(n=10, val=1, sum=0):
+    if n == 0:
+        return
+    else:
+        sum = sum + val
+        val /= -2
+        n -= 1
+        recursion(n, val, sum)
+
+print('\nПримет №2')
+print('Длина ряда = 10')
+print(f'Время затраченное на цикл: ', timeit.timeit(cycle, number=10000))
+print(f'Время затраченное на рекурсию:', timeit.timeit(recursion, number=10000))
+
+
+cProfile.run('cycle(10000)')
+cProfile.run('recursion(10000)')
+
+# Вывод: Во втором задании я применил два решение ( цикл и рекурсию). Цикл оказался быстрее рекурсии
+# так как рекурсия тратит дополнительное время на вызов саму себя тем самым замедляя выполнения операции.
+# По сложности цикл так же опережает рекурсию.
+# Следовательно в подобных задачах оптимально всего использоваь цикл.
+
+
+
+
+ # Мои результаты:
+ # Пример №1
+# Введите целое число: 567
+# Число на оборот на рекурсиях:  0.026144434999991972
+# Число на оборот на циклах:  0.009701919999997699
+# Число на оборот на срезах:  0.0038655270000163
+#          15 function calls (4 primitive calls) in 0.000 seconds
+#
+#    Ordered by: standard name
+#
+#    ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+#         1    0.000    0.000    0.000    0.000 <string>:1(<module>)
+#      12/1    0.000    0.000    0.000    0.000 task_1.py:14(revers)
+#         1    0.000    0.000    0.000    0.000 {built-in method builtins.exec}
+#         1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
+#
+#
+#          4 function calls in 0.000 seconds
+#
+#    Ordered by: standard name
+#
+#    ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+#         1    0.000    0.000    0.000    0.000 <string>:1(<module>)
+#         1    0.000    0.000    0.000    0.000 task_1.py:24(revers_2)
+#         1    0.000    0.000    0.000    0.000 {built-in method builtins.exec}
+#         1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
+#
+#
+#          4 function calls in 0.000 seconds
+#
+#    Ordered by: standard name
+#
+#    ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+#         1    0.000    0.000    0.000    0.000 <string>:1(<module>)
+#         1    0.000    0.000    0.000    0.000 task_1.py:32(revers_3)
+#         1    0.000    0.000    0.000    0.000 {built-in method builtins.exec}
+#         1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
+#
+#
+# --------------------------------------------------
+# --------------------------------------------------
+#
+# Примет №2
+# Длина ряда = 10
+# Время затраченное на цикл:  0.010612643000058597
+# Время затраченное на рекурсию: 0.018344318999993448
+#          4 function calls in 0.001 seconds
+#
+#    Ordered by: standard name
+#
+#    ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+#         1    0.000    0.000    0.001    0.001 <string>:1(<module>)
+#         1    0.001    0.001    0.001    0.001 task_1.py:65(cycle)
+#         1    0.000    0.000    0.001    0.001 {built-in method builtins.exec}
+#         1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
+#
+#
+#          10004 function calls (4 primitive calls) in 0.009 seconds
+#
+#    Ordered by: standard name
+#
+#    ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+#         1    0.000    0.000    0.009    0.009 <string>:1(<module>)
+#   10001/1    0.009    0.000    0.009    0.009 task_1.py:71(recursion)
+#         1    0.000    0.000    0.009    0.009 {built-in method builtins.exec}
+#         1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
+#
+#
+#
--- Урок 4. Практическое задание/task_2.py
+++ Урок 4. Практическое задание/task_2.py
@@ -12,3 +12,54 @@
 ВНИМАНИЕ: ЗАДАНИЯ, В КОТОРЫХ БУДУТ ГОЛЫЕ ЦИФРЫ ЗАМЕРОВ (БЕЗ АНАЛИТИКИ)
 БУДУТ ПРИНИМАТЬСЯ С ОЦЕНКОЙ УДОВЛЕТВОРИТЕЛЬНО
 """
+
+
+import timeit
+
+
+def eratosthenes(n):
+    sieve = list(range(n + 1))
+    sieve[1] = 0
+    for i in sieve:
+        if i > 1:
+            for j in range(i + i, len(sieve), i):
+                sieve[j] = 0
+    return sieve
+
+def func(n):
+    b = []
+    for i in range(2, n):
+        result = True
+        for j in range(2, i):
+            if i % j == 0:
+                result = False
+        if result:
+            b.append(i)
+    return b
+
+
+print('\nВремя на решете Эратосфена(10): ', timeit.timeit(f'eratosthenes(10)', setup='from __main__ import eratosthenes', number=100))
+print('Время на обычной функции(10): ', timeit.timeit(f'func(10)', setup='from __main__ import func', number=100), '\n')
+
+print('\nВремя на решете Эратосфена(100): ', timeit.timeit(f'eratosthenes(100)', setup='from __main__ import eratosthenes', number=100))
+print('Время на обычной функции(100): ', timeit.timeit(f'func(100)', setup='from __main__ import func', number=100), '\n')
+
+print('\nВремя на решете Эратосфена(1000): ', timeit.timeit(f'eratosthenes(1000)', setup='from __main__ import eratosthenes', number=100))
+print('Время на обычной функции(1000): ', timeit.timeit(f'func(1000)', setup='from __main__ import func', number=100), '\n')
+
+
+
+# Мои данные:
+# Время на решете Эратосфена(10):  0.00022344199987855973
+# Время на обычной функции(10):  0.0003820070000983833
+#
+#
+# Время на решете Эратосфена(100):  0.0016190349999760656
+# Время на обычной функции(100):  0.02820273899988024
+#
+#
+# Время на решете Эратосфена(1000):  0.021256588000142074
+# Время на обычной функции(1000):  2.4750579080000534
+#
+# Вывод: При увеличении входных данных значительно увеличивается время выполнения функции в то время как решето Эратосфена увеличивается не столь быстро.
+#
