a65c2c9a89a448c997de9fb9e73fc16807f4e010
maxxymuch <maxxymuch@yandex.ru>
2020-05-04 14:38:00 +0300

    HW4 done

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -12,3 +12,127 @@
 ВНИМАНИЕ: ЗАДАНИЯ, В КОТОРЫХ БУДУТ ГОЛЫЕ ЦИФРЫ ЗАМЕРОВ (БЕЗ АНАЛИТИКИ)
 БУДУТ ПРИНИМАТЬСЯ С ОЦЕНКОЙ УДОВЛЕТВОРИТЕЛЬНО
 """
+
+import random
+
+# Урок 3 Задание_5.	В массиве найти максимальный отрицательный элемент.
+# Вывести на экран его значение и позицию (индекс) в массиве.
+
+
+def task3_5(num_list):
+    min_el = float('-inf')
+    for i, item in enumerate(num_list):
+        if min_el < item < 0:
+            min_el = item
+            min_idx = i
+
+# python -m timeit -n 100 -s "import random" "x = [random.randint(-100, 0) for _ in range(10)]" "import task_1" "task_1.task3_5(x)"
+# 100 loops, best of 5: 18 usec per loop
+# 100 loops, best of 5: 99.1 usec per loop [random.randint(-100, 0) for _ in range(100)]
+# 100 loops, best of 5: 1.1 msec per loop [random.randint(-100, 0) for _ in range(1000)]
+# 100 loops, best of 5: 10.8 msec per loop [random.randint(-100, 0) for _ in range(10000)]
+# 100 loops, best of 5: 104 msec per loop [random.randint(-100, 0) for _ in range(100000)]
+
+def task3_5_v2(num_list):
+    new_list = [i for i in num_list if i < 0]
+    maximum = max(new_list)
+    idx = new_list.index(maximum)
+
+# 100 loops, best of 5: 10.9 usec per loop
+# 100 loops, best of 5: 103 usec per loop
+# 100 loops, best of 5: 1.01 msec per loop
+# 100 loops, best of 5: 10.9 msec per loop
+# 100 loops, best of 5: 108 msec per loop
+
+# task3_5 и task3_5_v2 при увеличении значений in range(значение) быстродествие в пользу функции task3_5
+
+# ==================================================================================================================
+
+#  Урок 3 Задание_7.	В одномерном массиве целых чисел определить два наименьших элемента.
+# Они могут быть как равны между собой (оба являться минимальными), так и различаться.
+
+def task3_7(random_list):
+    min_first = min(random_list)
+    list_copy = random_list.copy()
+    list_copy.remove(min_first)
+    min_second = min(list_copy)
+
+# 100 loops, best of 5: 11.5 usec per loop random.randint(-100, 0) for _ in range(10)
+# 100 loops, best of 5: 100 usec per loop random.randint(-100, 0) for _ in range(100)
+# 100 loops, best of 5: 999 usec per loop random.randint(-100, 0) for _ in range(1000)
+# 100 loops, best of 5: 10.1 msec per loop random.randint(-100, 0) for _ in range(10000)
+# 100 loops, best of 5: 103 msec per loop random.randint(-100, 0) for _ in range(100000)
+# 100 loops, best of 5: 208 msec per loop random.randint(-100, 0) for _ in range(200000)
+
+def task3_7_v2(random_list):
+    min_first = 100
+    min_second = 100
+    for el in random_list:
+        if el < min_first:
+            min_second = min_first
+            min_first = el
+        elif el == min_first:
+            min_second = el
+        elif el < min_second:
+            min_second = el
+
+# 100 loops, best of 5: 10.8 usec per loop
+# 100 loops, best of 5: 99.1 usec per loop
+# 100 loops, best of 5: 999 usec per loop
+# 100 loops, best of 5: 10.1 msec per loop
+# 100 loops, best of 5: 106 msec per loop
+# 100 loops, best of 5: 210 msec per loop random.randint(-100, 0) for _ in range(200000)
+
+# функции min/max работают быстрее
+
+# ==================================================================================================================
+
+# Урок 3 Задание_9.Найти максимальный элемент среди минимальных элементов столбцов матрицы.
+
+def task3_9(matrix):
+    for i, line in enumerate(matrix):
+        if i == 0:
+            min_line = line.copy()
+            continue
+        for idx, item in enumerate(line):
+            if item < min_line[idx]:
+                min_line[idx] = item
+    max_min = min_line[0]
+    for item in min_line:
+        if item > max_min:
+            max_min = item
+
+
+# python -m timeit -n 100 -s "import random" "x = [[random.randint(1, 10) for _ in range(3)] for _ in range(3)]" "import task_1" "task_1.task3_9(x)"
+# 100 loops, best of 5: 11.6 usec per loop 3x3
+# 100 loops, best of 5: 28.4 usec per loop 5x5
+# 100 loops, best of 5: 105 usec per loop 10x10
+# 100 loops, best of 5: 56.6 usec per loop 5x10
+# 100 loops, best of 5: 54.1 usec per loop 10x5
+# 100 loops, best of 5: 2.55 msec per loop 50x50
+# 100 loops, best of 5: 10.2 msec per loop 100x100
+# 100 loops, best of 5: 41.4 msec per loop 200x200
+# 100 loops, best of 5: 94.7 msec per loop 300x300c
+# 100 loops, best of 5: 263 msec per loop 500x500
+
+def task3_9_v2(matrix):
+    max_ = float('-inf')
+    for j in range(len(matrix[0])):
+        min_ = matrix[0][j]
+        for i in range(len(matrix)):
+            if matrix[i][j] < min_:
+                min_ = matrix[i][j]
+        if min_ > max_:
+            max_ = min_
+
+# 100 loops, best of 5: 13.3 usec per loop 3x3
+# 100 loops, best of 5: 29.6 usec per loop 5x5
+# 100 loops, best of 5: 109 usec per loop 10x10
+# 100 loops, best of 5: 2.59 msec per loop 50x50
+# 100 loops, best of 5: 10.3 msec per loop 100x100
+# 100 loops, best of 5: 41.4 msec per loop 200x200
+# 100 loops, best of 5: 94.4 msec per loop 300x300
+# 100 loops, best of 5: 267 msec per loop 500x500
+
+# task3_9_v2 менее быстродейственна чем task3_9
+
--- Урок 4. Практическое задание/task_2.py
+++ Урок 4. Практическое задание/task_2.py
@@ -12,3 +12,138 @@
 ВНИМАНИЕ: ЗАДАНИЯ, В КОТОРЫХ БУДУТ ГОЛЫЕ ЦИФРЫ ЗАМЕРОВ (БЕЗ АНАЛИТИКИ)
 БУДУТ ПРИНИМАТЬСЯ С ОЦЕНКОЙ УДОВЛЕТВОРИТЕЛЬНО
 """
+import cProfile
+
+
+def eratosphens_meth(n):
+    size = int(n ** 1.5 + 2)
+    sieve = [i for i in range(size)]
+    sieve[1] = 0
+
+    for i in range(2, size):
+        if sieve[i] != 0:
+            j = i + i
+
+            while j < size:
+                sieve[j] = 0
+                j += i
+
+    res = [i for i in sieve if i != 0]
+    return res[n - 1]
+
+
+# ====================================================================================================================
+
+def non_eratosphens(n):
+    if n == 1:
+        return 2
+    res = [2]
+    numb_list = [2]
+    number = 3
+
+    while len(res) != n:
+        flag = True
+
+        for itm in numb_list:
+            if number % itm == 0:
+                flag = False
+                break
+
+        if flag:
+            res.append(number)
+
+        numb_list.append(number)
+        number += 1
+
+    return res[n - 1]
+
+# cProfile.run('eratosphens_meth(100)')
+# 6 function calls in 0.008 seconds
+#
+#    Ordered by: standard name
+#
+#    ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+#         1    0.000    0.000    0.008    0.008 <string>:1(<module>)
+#         1    0.008    0.008    0.008    0.008 task_2.py:18(eratosphens_meth)
+#         1    0.000    0.000    0.000    0.000 task_2.py:20(<listcomp>)
+#         1    0.000    0.000    0.000    0.000 task_2.py:31(<listcomp>)
+#         1    0.000    0.000    0.008    0.008 {built-in method builtins.exec}
+#         1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
+#
+# cProfile.run('non_eratosphens(100)')
+#          1182 function calls in 0.003 seconds
+#
+#    Ordered by: standard name
+#
+#    ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+#         1    0.000    0.000    0.003    0.003 <string>:1(<module>)
+#         1    0.003    0.003    0.003    0.003 task_2.py:37(non_eratosphens)
+#         1    0.000    0.000    0.003    0.003 {built-in method builtins.exec}
+#       540    0.000    0.000    0.000    0.000 {built-in method builtins.len}
+#       638    0.000    0.000    0.000    0.000 {method 'append' of 'list' objects}
+#         1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
+
+
+# cProfile.run('eratosphens_meth(1000)')
+# 6 function calls in 0.028 seconds
+#
+#    Ordered by: standard name
+#
+#    ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+#         1    0.001    0.001    0.028    0.028 <string>:1(<module>)
+#         1    0.024    0.024    0.027    0.027 task_2.py:18(eratosphens_meth)
+#         1    0.002    0.002    0.002    0.002 task_2.py:20(<listcomp>)
+#         1    0.002    0.002    0.002    0.002 task_2.py:31(<listcomp>)
+#         1    0.000    0.000    0.028    0.028 {built-in method builtins.exec}
+#         1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
+#
+# cProfile.run('non_eratosphens(1000)')
+#          16838 function calls in 0.363 seconds
+#
+#    Ordered by: standard name
+#
+#    ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+#         1    0.000    0.000    0.363    0.363 <string>:1(<module>)
+#         1    0.361    0.361    0.363    0.363 task_2.py:37(non_eratosphens)
+#         1    0.000    0.000    0.363    0.363 {built-in method builtins.exec}
+#      7918    0.001    0.000    0.001    0.000 {built-in method builtins.len}
+#      8916    0.001    0.000    0.001    0.000 {method 'append' of 'list' objects}
+#         1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
+
+
+# cProfile.run('eratosphens_meth(2000)')
+# 6 function calls in 0.061 seconds
+#
+#    Ordered by: standard name
+#
+#    ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+#         1    0.001    0.001    0.061    0.061 <string>:1(<module>)
+#         1    0.053    0.053    0.060    0.060 task_2.py:18(eratosphens_meth)
+#         1    0.003    0.003    0.003    0.003 task_2.py:20(<listcomp>)
+#         1    0.004    0.004    0.004    0.004 task_2.py:31(<listcomp>)
+#         1    0.000    0.000    0.061    0.061 {built-in method builtins.exec}
+#         1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
+
+# python -m timeit -n 100 -s "import task_2" "task_2.eratosphens_meth(2000)"
+# 100 loops, best of 5: 28.5 msec per loop
+
+#
+# cProfile.run('non_eratosphens(2000)')
+#          36778 function calls in 1.338 seconds
+#
+#    Ordered by: standard name
+#
+#    ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+#         1    0.000    0.000    1.338    1.338 <string>:1(<module>)
+#         1    1.333    1.333    1.338    1.338 task_2.py:37(non_eratosphens)
+#         1    0.000    0.000    1.338    1.338 {built-in method builtins.exec}
+#     17388    0.002    0.000    0.002    0.000 {built-in method builtins.len}
+#     19386    0.002    0.000    0.002    0.000 {method 'append' of 'list' objects}
+#         1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
+
+# python -m timeit -n 100 -s "import task_2" "task_2.non_eratosphens(2000)"
+# 100 loops, best of 5: 738 msec per loop
+
+# Поиск натурального числа c использованием решета Эратосфена работает существенно быстрее, и имеет сложность близкую к
+# линейной O(n) по сравнению с алгоритмом, основанном на переборе всех элементов, со сложнсотью O(n^2)
+
