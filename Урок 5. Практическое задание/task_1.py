c6463daad78fc3546ef75acd491f82568011231c
alex vydrin <tapat@mail.ru>
2020-05-06 22:59:05 +0300

    py_algos_lesson_5

--- Урок 5. Практическое задание/task_1.py
+++ Урок 5. Практическое задание/task_1.py
@@ -25,3 +25,73 @@
 
 Предприятия, с прибылью ниже среднего значения: Копыта
 """
+
+from collections import namedtuple
+from random import randint
+
+# Режим отладки
+# DEBUG_MODE == True - Данные вводятся автоматически
+# DEBUG_MODE == False - Данные вводятся пользователем
+# (проверку на правильность ввода пользователем делать не стал, посчитал для задачи излишним)
+DEBUG_MODE = True
+DEBUG_COMPANY_NAMES = ["IBM", "Facebook", "Google", "Yandex", "Coca Cola"]
+
+print("Введите количество предприятий для расчета прибыли: ", end="")
+if DEBUG_MODE:
+    COMPANY_CNT = randint(3, 5)
+    print(COMPANY_CNT)
+else:
+    COMPANY_CNT = int(input())
+
+COMPANY = namedtuple('company', 'name quarter_1 quarter_2 quarter_3 quarter_4 year')
+
+COMPANIES = []
+
+for i in range(COMPANY_CNT):
+    print(f"Введите наименование предприятия № {i+1}: ", end="")
+    if DEBUG_MODE:
+        COMPANY_NAME = DEBUG_COMPANY_NAMES[i]
+        print(COMPANY_NAME)
+    else:
+        COMPANY_NAME = input()
+    print("через пробел введите прибыль данного предприятия")
+    print(f"за каждый квартал(Всего 4 квартала):", end="")
+    if DEBUG_MODE:
+        PROFIT_STR = f"{randint(100, 1000)} {randint(100, 1000)} "\
+                     f"{randint(100, 1000)} {randint(100, 1000)}"
+        print(PROFIT_STR)
+    else:
+        PROFIT_STR = input()
+    PROFIT = list(map(int, PROFIT_STR.split()))
+    COMPANIES.append(COMPANY(
+        name=COMPANY_NAME,
+        quarter_1=PROFIT[0],
+        quarter_2=PROFIT[1],
+        quarter_3=PROFIT[2],
+        quarter_4=PROFIT[3],
+        year=sum(PROFIT)
+    ))
+
+print(f"Введены следующие данные:")
+for company in COMPANIES:
+    print(company)
+
+PROFIT_TOTAL = 0
+for company in COMPANIES:
+    PROFIT_TOTAL += company.year
+PROFIT_AVG = PROFIT_TOTAL / COMPANY_CNT
+
+print()
+print(f"Средняя годовая прибыль всех предприятий: {PROFIT_AVG:.2f}")
+
+print()
+print("Предприятия, с прибылью выше среднего значения: ")
+for company in COMPANIES:
+    if company.year > PROFIT_AVG:
+        print(f"{company.name} {company.year}")
+
+print()
+print("Предприятия, с прибылью ниже среднего значения: ")
+for company in COMPANIES:
+    if company.year < PROFIT_AVG:
+        print(f"{company.name} {company.year}")
--- Урок 5. Практическое задание/task_2.py
+++ Урок 5. Практическое задание/task_2.py
@@ -11,3 +11,45 @@
 Также попробуйте решить задачу вообще без collections и применить только ваши знания по ООП
 (в частности по перегрузке методов)
 """
+
+from collections import deque
+
+
+class HexNum:
+    """
+    Hexadecimal
+    """
+    def __init__(self, hex_num):
+        self.hex_num = deque(hex_num)
+
+    def __str__(self):
+        return ''.join(self.hex_num)
+
+    def __add__(self, other):
+        # производим поразрядное сложение
+        transfer = 0
+        result = deque()
+        for i in range(max(len(self.hex_num), len(other.hex_num))):
+            value_a = int(self.hex_num[-i-1], 16) if len(self.hex_num) > i else 0
+            value_b = int(other.hex_num[-i-1], 16) if len(other.hex_num) > i else 0
+            value_c = value_a + value_b + transfer
+            transfer = 1 if value_c > 16 else 0
+            value_c = value_c - 16 if value_c > 16 else value_c
+            result.appendleft(f"{value_c:X}")
+        if transfer:
+            result.appendleft('1')
+        return HexNum("".join(result))
+
+    def __mul__(self, other):
+        # здесь порязрядное умножение не стал реализовывать - использовал простое преобразование
+        # предположил что для целей задачи достаточно реализации поразрядного __add__:
+        # 1) применение коллекции из модуля collections
+        # 2) применение знаний по перегрузке методов в ООП
+        result = int(''.join(self.hex_num), 16) * int(''.join(other.hex_num), 16)
+        return HexNum(f"{result:X}")
+
+
+NUM_1 = HexNum('A2')
+NUM_2 = HexNum('C4F')
+print(f"{NUM_1} + {NUM_2} = {NUM_1 + NUM_2}")
+print(f"{NUM_1} * {NUM_2} = {NUM_1 * NUM_2}")
