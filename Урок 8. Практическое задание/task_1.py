7c09ae13a7c47bc6cee0e11fad25906b89c802c7
anna <anna@rcbd.org>
2020-05-05 21:52:00 -0400

    task_1-2

--- Урок 8. Практическое задание/task_1.py
+++ Урок 8. Практическое задание/task_1.py
@@ -16,3 +16,27 @@
 
 Итог: 6 подстрок
 """
+import hashlib
+
+
+def main():
+    """поехали"""
+    string = input('Введите строку, состоящую только из маленьких латинских букв: ')
+    substrings_hash = set()
+    substrings_full = set()
+
+    for i in range(len(string)):
+        for j in range(len(string), i, -1):
+            sub_str = string[i:j]
+            hash_str = hashlib.sha1(sub_str.encode('utf-8')).hexdigest()
+            if hash_str not in substrings_hash:
+                substrings_hash.add(hash_str)
+                substrings_full.add(sub_str)
+
+    print(f'Итого: {len(substrings_hash) - 1} различных подстрок в строке {string}')
+    for i in substrings_full:
+        print(i)
+
+
+# поехали
+main()
--- Урок 8. Практическое задание/task_2.py
+++ Урок 8. Практическое задание/task_2.py
@@ -8,3 +8,89 @@ s = "beep boop beer!"
 Результат:
 00 11 11 101 010 00 011 011 101 010 00 11 11 1000 1001
 """
+import heapq
+from collections import Counter
+from collections import namedtuple
+
+
+class Node(namedtuple("Node", ["left", "right"])):
+    """класс для ветвей дерева - внутренних узлов; у них есть потомки"""
+    def walk(self, code, acc):
+        """чтобы обойти дерево нам нужно"""
+        self.left.walk(code, acc + "0")  # пойти в левого потомка, добавив к префиксу "0"
+        self.right.walk(code, acc + "1")  # затем пойти в правого потомка, добавив к префиксу "1"
+
+
+class Leaf(namedtuple("Leaf", ["char"])):
+    """класс для листьев дерева, у него нет потомков, но есть значение символа"""
+    def walk(self, code, acc):
+        """чтобы обойти лист делать ничего не нужно"""
+        code[self.char] = acc or "0"
+
+
+def huffman_encode(s):
+    """функция кодирования строки в коды Хаффмана"""
+    h = []
+    for ch, freq in Counter(s).items():
+        h.append((freq, len(h), Leaf(ch)))
+
+    heapq.heapify(h)
+    count = len(h)
+    while len(h) > 1:
+        freq1, _count1, left = heapq.heappop(h)
+        freq2, _count2, right = heapq.heappop(h)
+        heapq.heappush(h, (freq1 + freq2, count, Node(left, right)))
+        count += 1
+
+    code = {}
+    if h:
+        [(_freq, _count, root)] = h
+        root.walk(code, "")
+
+    encoded = "".join(code[ch] for ch in s)
+    return code, encoded  # возвращаем словарь символов и соответствующих им кодов и результ
+
+
+def huffman_decode(encoded, code):
+    """функция декодирования исходной строки по кодам Хаффмана"""
+    sx = []
+    enc_ch = ""
+    for ch in encoded:
+        enc_ch += ch
+        for dec_ch in code:
+            if code.get(dec_ch) == enc_ch:
+                sx.append(dec_ch)
+                enc_ch = ""
+                break
+    return "".join(sx)  # вернем значение раскодированной строки
+
+
+def main():
+    """main"""
+    s = input()
+    code, encoded = huffman_encode(s)
+    print_code(code, s, encoded)
+
+
+def test():
+    """тест для проверки алгоритма"""
+    for s in ["beep boop beer!", "python python", "a", "qqqqq"]:
+        code, encoded = huffman_encode(s)
+        print_code(code, s, encoded)
+        # раскодируем строку и сравним ее с исходной строкой
+        assert huffman_decode(encoded, code) == s
+
+
+def print_code(code, original, encoded):
+    """Вывод результатов на экран"""
+    encoded_visual = "".join(
+        code[ch] + " " for ch in original)
+
+    print("{} ({}) -> {} ({})".format(original, len(original), encoded_visual, len(encoded) // 8 + 1))
+    for ch in sorted(code):
+        print("{}: {}".format(ch, code[ch]))  # выведем символ и соответствующий ему код
+
+
+# запускаем
+# main()
+test()
