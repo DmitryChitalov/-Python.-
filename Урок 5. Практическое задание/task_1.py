d6bcf8891c48d1c636ed94bdc064dbb36d4c5071
monstric93 <64038672+monstric93@users.noreply.github.com>
2020-05-07 10:56:23 +0300

    Домашнее задание №5

--- Урок 5. Практическое задание/task_1.py
+++ Урок 5. Практическое задание/task_1.py
@@ -25,3 +25,95 @@
 
 Предприятия, с прибылью ниже среднего значения: Копыта
 """
+from collections import defaultdict
+
+"""
+Функция расчета среднегодовой прибыли всех предприятий
+и определения предприятий получивших годовую прибыль
+выше и ниже среднегодовой
+"""
+def calc_average(kwargs):
+    """
+    :param kwargs:
+    :return:
+    """
+    sum_all = 0
+    average = 0.0
+    list_more_average = []
+    list_little_average = []
+    """
+    Вычисление среднегодовой прибыли всех предприятий
+    """
+    for k, val in kwargs.items():
+        for element in val:
+            sum_all += element
+    average = sum_all / 4 / len(kwargs)
+
+    """
+    Определение какие предприятия получили годовую прибыль выше и ниже среднегодовой
+    прибыли всех предприятий
+    """
+    for k, val in kwargs.items():
+        sum_all = 0
+        for element in val:
+            sum_all += element
+        if sum_all / 4 > average:
+            list_more_average.append(k)
+        elif sum_all / 4 < average:
+            list_little_average.append(k)
+    return average, list_more_average, list_little_average
+
+
+# Создание коллекции с названиями фирм и их прибылью за каждый квартал (всего 4 квартала)
+FIRM_DATA = defaultdict(list)
+while True:
+    try:
+        FIRM_COUNTS = int(input('Введите количество предприятий для расчета прибыли:\n'))
+        if FIRM_COUNTS < 0:
+            raise IOError
+    except (ValueError, TypeError, IOError):
+        print(f'Ошибка ввода количества предприятий.\n'
+              f'Необходимо ввести целое положительное число.')
+    else:
+        while FIRM_COUNTS > 0:
+            while True:
+                try:
+                    FIRM_NAME = input(f'Введите название предприятия:\n')
+                    if FIRM_NAME.isdigit():
+                        raise IOError
+                except (ValueError, TypeError, IOError):
+                    print(f'Ошибка ввода названия предприятия.\n'
+                          f'Название предприятия не может состоять только из цифр.')
+                else:
+                    while True:
+                        try:
+                            FIRM_DATA[FIRM_NAME] = list(map(float, input('Введите через пробел прибыль предприятия'
+                                                                         ' за каждый квартал:\n').split()))
+                            for key, value in FIRM_DATA.items():
+                                if key == FIRM_NAME:
+                                    for el in value:
+                                        if el < 0 or len(value) < 4:
+                                            raise IOError
+                        except (ValueError, IOError, TypeError):
+                            print(f'Ошибка ввода квартальной прибыли предприятия.\n'
+                                  f'Необходимо ввести 4 неотрицательных числа через пробел.')
+                        else:
+                            break
+                FIRM_COUNTS -= 1
+                break
+        break
+
+print(f'=' * 50)
+for key, value in FIRM_DATA.items():
+    print(f'Название фирмы: {key}  Прибыль за каждый квартал: {value}')
+print(f'=' * 50)
+print(f'\nСредняя годовая прибыль всех предприятий за год: {calc_average(FIRM_DATA)[0]:.02f}')
+print(f'=' * 50)
+print(f'Предприятия, с прибылью выше среднего значения:')
+for el in calc_average(FIRM_DATA)[1]:
+      print(f'{el}')
+print(f'=' * 50)
+print(f'Предприятия, с прибылью ниже среднего значения:')
+for el in calc_average(FIRM_DATA)[2]:
+      print(f'{el}')
+print(f'=' * 50)
--- Урок 5. Практическое задание/task_2.py
+++ Урок 5. Практическое задание/task_2.py
@@ -11,3 +11,60 @@
 Также попробуйте решить задачу вообще без collections и применить только ваши знания по ООП
 (в частности по перегрузке методов)
 """
+from collections import defaultdict
+from collections import deque
+
+# Коллекция для сохранения введеных пользователем чисел
+NUMBERS_COLLECTION = deque()
+# Коллекция для сохранения суммы введеных пользователем чисел
+SUM_NUMBERS = deque()
+# Коллекция для сохранения произведения введеных пользователем чисел
+MUL_NUMBERS = deque()
+
+while True:
+    # Запрос пользователя на ввод чисел в шестнадцатиричном формате
+    NUMBERS = input('Введите 2 шестнадцатиричных числа через пробел:\n').split()
+    try:
+        # Проверка, что введено 2 числа
+        if len(NUMBERS) != 2:
+            raise IOError
+        else:
+            # Попытка перевода в шестнадцатиричный формат введенных чисел
+            [hex(int(float.fromhex(elem))) for elem in NUMBERS]
+    except (ValueError, TypeError, IOError):
+        print(f'Ошибка ввода чисел.\n'
+              f'Необходимо ввести 2 числа в шестнадцатиричном формате через пробел.')
+    else:
+        j = 0
+        # Сохранение чисел посимвольно в коллекцию
+        for elements in NUMBERS:
+            NUMBERS_COLLECTION.append([])
+            for i in elements:
+                NUMBERS_COLLECTION[j].append(i)
+            j += 1
+
+        # Инициализация значений суммы и произведения чисел
+        SUM = '0.0'
+        MUL_NUM = '0.0'
+        MUL = '0.0'
+
+        # Цикл восстановления первого числа в коллекции из цифр числа.
+        for i, element in enumerate(NUMBERS_COLLECTION[0]):
+            SUM = hex(int(float.fromhex(element)*pow(16, len(NUMBERS_COLLECTION[0]) - 1 - i) + float.fromhex(SUM)))
+        # Цикл восстановления второго числа в коллекции из цифр числа.
+        for i, element in enumerate(NUMBERS_COLLECTION[1]):
+            SUM = hex(int(float.fromhex(element)*pow(16, len(NUMBERS_COLLECTION[1]) - 1 - i) + float.fromhex(SUM)))
+        # Цикл нахождения произведения двух чисел
+        for i, element in enumerate(NUMBERS_COLLECTION[0]):
+            for j, value in enumerate(NUMBERS_COLLECTION[1]):
+                MUL_NUM = hex(int(float.fromhex(element)*pow(16, len(NUMBERS_COLLECTION[0]) - 1 - i) *
+                              float.fromhex(value)*pow(16, len(NUMBERS_COLLECTION[1]) - 1 - j)))
+                MUL = hex(int(float.fromhex(MUL_NUM) + float.fromhex(MUL)))
+        # Добавление результатов суммы и произведения в соответствующие коллекции
+        for element in SUM[2:]:
+            SUM_NUMBERS.append(element)
+        for element in MUL[2:]:
+            MUL_NUMBERS.append(element)
+        print(f'Сумма чисел: {SUM_NUMBERS}')
+        print(f'Произведение чисел: {MUL_NUMBERS}')
+        break
