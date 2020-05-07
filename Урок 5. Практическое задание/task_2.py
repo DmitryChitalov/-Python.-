2088273fff48144424d9efe5a72064eb427a746f
Nikita Shataev <shataev.nikita5@gmail.com>
2020-05-07 15:24:43 +0700

    Задания 5 урока

--- Урок 5. Практическое задание/task_2.py
+++ Урок 5. Практическое задание/task_2.py
@@ -11,3 +11,27 @@
 Также попробуйте решить задачу вообще без collections и применить только ваши знания по ООП
 (в частности по перегрузке методов)
 """
+from functools import reduce
+from collections import defaultdict
+import collections
+"""
+NUMB_1 = list(input('Введите первое число в 16-ричном формате: ').upper())
+NUMB_2 = list(input('Введите второе число в 16-ричном формате: ').upper())
+"""
+
+
+def dex_numbers():
+    dd_nums = defaultdict(list)
+    nums = input('Введите числа в 16-тиричном виде через пробел: ').split()
+    for i, num in enumerate(nums):
+        dd_nums[i] = list(num.upper())
+        print(f'Введенное число {i + 1}: {dd_nums[i]}')
+        nums_oct = [int(''.join(num), 16) for num in dd_nums.values()]
+        nums_sum_hex = hex(sum(nums_oct))[2:].upper()
+        nums_mult_hex = hex(reduce(lambda x, y: x * y, nums_oct))[2:].upper()
+
+    print(f'Сумма чисел: {list(nums_sum_hex)}')
+    print(f'Произведение: {list(nums_mult_hex)}')
+
+
+dex_numbers()
