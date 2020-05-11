45c1ed93751c73a8c57bb7b5227b01622612451a
Mariia <58525490+MariiaShara@users.noreply.github.com>
2020-05-11 03:11:00 -0400

    Домашнее задание к уроку 5

--- Урок 5. Практическое задание/task_1.py
+++ Урок 5. Практическое задание/task_1.py
@@ -25,3 +25,80 @@
 
 Предприятия, с прибылью ниже среднего значения: Копыта
 """
+# ВАРИАНТ 1
+from collections import defaultdict
+
+n = int(input("Введите количество предприятий для расчета прибыли: "))
+
+FIRMS = defaultdict()
+for i in range(n):
+    NAME = input(f"Введите название {i+1}-го предприятия: ")
+    PROFITS = list(map(int, input(f"Через пробел введите прибыль {i+1}-го предприятия "\
+                         "за каждый квартал(Всего 4 квартала): ").split(" ")))
+    FIRMS[NAME] = PROFITS
+print(FIRMS)
+
+TTL_PROFITS = 0
+for key in FIRMS:
+    TTL_PROFITS += sum(FIRMS.get(key))
+
+AVER_PROFITS = round(TTL_PROFITS / n, 2)
+
+LESS = []
+MORE = []
+for key in FIRMS:
+    if (sum(FIRMS.get(key))) < AVER_PROFITS:
+        LESS.append(key)
+    elif (sum(FIRMS.get(key))) > AVER_PROFITS:
+        MORE.append(key)
+LESS = ", ".join(LESS)
+MORE = ", ".join(MORE)
+
+print(f"Средняя годовая прибыль всех предприятий: {AVER_PROFITS}")
+print(f"Предприятия, с прибылью выше среднего значения: {MORE}")
+print(f"Предприятия, с прибылью ниже среднего значения: {LESS}")
+
+
+# ВАРИАНТ 2
+from collections import namedtuple
+
+n = int(input("Введите количество предприятий для расчета прибыли: "))
+
+FIRMS = namedtuple(
+    "FIRM",
+    "NAME QUARTER_1 QUARTER_2 QUARTER_3 QUARTER_4"
+)
+YEAR_PROFITS = {}
+
+for i in range(n):
+    NAME = input(f"Введите название {i + 1}-го предприятия: ")
+    PROFITS = list(map(int, input(f"Через пробел введите прибыль {i+1}-го предприятия "\
+                         "за каждый квартал(Всего 4 квартала): ").split(" ")))
+    FIRM = FIRMS(
+        NAME=NAME,
+        QUARTER_1=PROFITS[0],
+        QUARTER_2=PROFITS[1],
+        QUARTER_3=PROFITS[2],
+        QUARTER_4=PROFITS[3]
+    )
+    YEAR_PROFITS[FIRM.NAME] = FIRM.QUARTER_1 + FIRM.QUARTER_2 + FIRM.QUARTER_3 + FIRM.QUARTER_4
+
+TTL_PROFITS = 0
+for value in YEAR_PROFITS.values():
+    TTL_PROFITS += value
+
+AVER_PROFITS = round(TTL_PROFITS / n, 2)
+
+LESS = []
+MORE = []
+for key, value in YEAR_PROFITS.items():
+    if value < AVER_PROFITS:
+        LESS.append(key)
+    elif value > AVER_PROFITS:
+        MORE.append(key)
+LESS = ", ".join(LESS)
+MORE = ", ".join(MORE)
+
+print(f"Средняя годовая прибыль всех предприятий: {AVER_PROFITS}")
+print(f"Предприятия, с прибылью выше среднего значения: {MORE}")
+print(f"Предприятия, с прибылью ниже среднего значения: {LESS}")
--- Урок 5. Практическое задание/task_2.py
+++ Урок 5. Практическое задание/task_2.py
@@ -11,3 +11,93 @@
 Также попробуйте решить задачу вообще без collections и применить только ваши знания по ООП
 (в частности по перегрузке методов)
 """
+
+# ВАРИАНТ 1
+from collections import defaultdict, deque
+
+
+NUMBERS = defaultdict(list)
+for i in range(2):
+    NUMBER = input(f"Введите {i + 1}-е  шестнадцатеричное число: ")
+    NUMBERS[NUMBER] = list(NUMBER)
+
+sum = 0
+for value in NUMBERS.values():
+    sum += int("".join(value), 16)
+sum = list(map(str.upper, hex(sum).lstrip("0x")))
+
+prod = 1
+for value in NUMBERS.values():
+    prod *= int("".join(value), 16)
+prod = list(map(str.upper, hex(prod).lstrip("0x")))
+
+print(f"Сумма чисел: {sum}")
+print(f"Произведение чисел: {prod}")
+
+
+# ВАРИАНТ ООП
+class Hex:
+    def __init__(self, n):
+        self.n = n
+
+    def __add__(self, obj):
+        return list(map(str.upper, hex(self.n + obj.n).lstrip("0x")))
+
+    def __mul__(self, obj):
+        return list(map(str.upper, hex(self.n * obj.n).lstrip("0x")))
+
+x = int(input("Введите первое  шестнадцатеричное число: "), 16)
+a = Hex(x)
+y = int(input("Введите второе шестнадцатеричное число: "), 16)
+b = Hex(y)
+
+print(f"Сумма: {a + b}")
+print(f"Произведение: {a * b}")
+
+
+
+# Вариант 3 с deque осилила только сложение
+
+def get_value(num):
+    if num in ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9"):
+        return int(num)
+    if num in ("A", "B", "C", "D", "E", "F"):
+        return ord(num) - ord('A') + 10
+
+def get_numeral(val):
+    if 0 <= val <= 9:
+        return str(val)
+    if 10 <= val <= 15:
+        return chr(val + ord('A') - 10)
+
+def sum_base_16(x, y):
+    x = deque(x)
+    y = deque(y)
+
+    if len(x) > len(y):
+        for i in range(len(x) - len(y)):
+            y.appendleft("0")
+    elif len(x) < len(y):
+        for j in range(len(y) - len(x)):
+            x.appendleft("0")
+    carry = 0
+    res = deque()
+
+    for i in range(-1, -(len(x)+1), -1):
+        nm_1 = get_value(x[i])
+        nm_2 = get_value(y[i])
+        res_nm = nm_1 + nm_2 + carry
+        if res_nm >= 16:
+            carry = 1
+            res_nm -= 16
+        else:
+            carry = 0
+        res.appendleft(get_numeral(res_nm))
+
+    if carry == 1:
+        res.appendleft("1")
+
+    return res
+
+sum = sum_base_16(input("Введите первое  шестнадцатеричное число: "), input("Введите второе шестнадцатеричное число: "))
+print(f"Сумма чисел: {list(sum)}")
