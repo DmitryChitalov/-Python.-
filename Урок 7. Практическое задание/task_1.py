12e1353384f0cd56ab4dec0e8a22ad874a813b14
mtbrother <mtbrothers@outlook.com>
2020-05-14 00:29:24 +0500

    home_work_7 - done

--- Урок 7. Практическое задание/task_1.py
+++ Урок 7. Практическое задание/task_1.py
@@ -3,9 +3,53 @@
 заданный случайными числами на промежутке [-100; 100). Выведите на экран
 исходный и отсортированный массивы. Сортировка должна быть реализована в
 виде функции. Обязательно доработайте алгоритм (сделайте его умнее).
-Идея доработки: если за проход по списку не совершается ни одной сортировки, то завершение
+
 Обязательно сделайте замеры времени обеих реализаций
 
 Подсказка: обратите внимание, сортируем не по возрастанию, как в примере,
 а по убыванию
 """
+import random
+import timeit
+
+
+def bubble_sort_no_smart(numbers):
+    for i in range(len(numbers) - 1, 0, -1):
+        for n in range(i):
+            if numbers[n] < numbers[n + 1]:
+                numbers[n], numbers[n + 1] = numbers[n + 1], numbers[n]
+
+    return numbers
+
+
+def bubble_sort(numbers):
+    for i in range(len(numbers) - 1, 0, -1):
+        flag = True
+        for n in range(i):
+            if numbers[n] < numbers[n + 1]:
+                numbers[n], numbers[n + 1] = numbers[n + 1], numbers[n]
+                flag = False
+
+        if flag is True:
+            break
+    return numbers
+
+
+numbers = [random.randint(-100, 100) for _ in range(10)]
+print(numbers)
+print(bubble_sort(numbers))
+
+print(timeit.timeit('bubble_sort_no_smart(numbers)',
+                    setup='from __main__ import bubble_sort_no_smart, numbers', number=1000))
+print(timeit.timeit('bubble_sort(numbers)',
+                    setup='from __main__ import bubble_sort, numbers', number=1000))
+
+
+'''
+Цель оптимизации: если за проход по списку не совершается ни одной сортировки,
+то происходит завершение операции.
+По результатам замеров выяснили, что оптимизация существенно уменьшила
+время выполнения алгоритма сортировки "пузырьком":
+	-- до оптимизации: 0.0380156
+	-- после оптимизации: 0.0070906999999999915
+'''
--- Урок 7. Практическое задание/task_3.py
+++ Урок 7. Практическое задание/task_3.py
@@ -6,3 +6,31 @@
 массива. Но если это слишком сложно, то используйте метод сортировки,
 который не рассматривался на уроках
 """
+import random
+
+# Можно отсортировать при помощи встроенной функции:
+#
+# def sorted_array(array):
+#     ordered_list = sorted(array)
+#     return ordered_list
+
+
+def sorted_array(array):
+    if len(array) < 2:
+        return array
+    else:
+        pivot = array[0]
+        less = [i for i in array[1:] if i <= pivot]
+        greater = [i for i in array[1:] if i > pivot]
+
+        return sorted_array(less) + [pivot] + sorted_array(greater)
+
+
+m = random.randint(5, 10)
+size = 2 * m + 1
+array = [random.randint(0, 50) for _ in range(size)]
+sort_list = sorted_array(array)
+
+print(f'Массив размером {size} элементов : {array}')
+print(f'Отсортированный массив: {sort_list}')
+print(f'Медиана : {sort_list[len(sort_list) // 2 ]}')
