de0be57d0e12101123af0a2f6aae19dd0d13d983
Begemotina <Begemotina@nightmail.ru>
2020-05-14 07:04:19 +0300

    2+3 - готово

--- Урок 7. Практическое задание/task_2.py
+++ Урок 7. Практическое задание/task_2.py
@@ -8,3 +8,42 @@
 Исходный - [46.11436617832828, 41.62921998361278, 18.45859540989644, 12.128870723745806, 8.025098788570562]
 Отсортированный - [8.025098788570562, 12.128870723745806, 18.45859540989644, 41.62921998361278, 46.11436617832828]
 """
+import random
+
+def merge_sort(orig_list):
+    if len(orig_list) > 1:
+        center = len(orig_list) // 2
+        left = orig_list[:center]
+        right = orig_list[center:]
+
+        merge_sort(left)
+        merge_sort(right)
+        i, j, k = 0, 0, 0
+
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                orig_list[k] = left[i]
+                i += 1
+            else:
+                orig_list[k] = right[j]
+                j += 1
+            k += 1
+
+        while i < len(left):
+            orig_list[k] = left[i]
+            i += 1
+            k += 1
+
+        while j < len(right):
+            orig_list[k] = right[j]
+            j += 1
+            k += 1
+        return orig_list
+
+n = int(input("cколько чисел будет? \n"))
+
+orig_list = [random.random() * 50 for _ in range(n)]
+
+print(orig_list)
+
+print(merge_sort(orig_list))
--- Урок 7. Практическое задание/task_3.py
+++ Урок 7. Практическое задание/task_3.py
@@ -6,3 +6,35 @@
 массива. Но если это слишком сложно, то используйте метод сортировки,
 который не рассматривался на уроках
 """
+import random
+from statistics import median
+
+
+def my_mediana(list, lenght):
+    """поска медианы"""
+
+    if len(list) == 1:
+        return list[0]
+
+    point = random.choice(list)
+
+    left_el = [el for el in list if el < point]
+    right_el = [el for el in list if el > point]
+    center = [el for el in list if el == point]
+
+    if lenght < len(left_el):
+        return my_mediana(left_el, lenght)
+    elif lenght < len(left_el) + len(center):
+        return center[0]
+    else:
+        return my_mediana(right_el, lenght - len(left_el) - len(center))
+
+
+m = int(input("введите m для массива: "))
+orig_list = [random.randint(0, 100) for _ in range(2 * m + 1)]
+print(orig_list)
+
+list_median = my_mediana(orig_list, len(orig_list) / 2)
+print(f'Медиана в массиве: {list_median}')
+
+print(f' проверка {median(orig_list)}')
