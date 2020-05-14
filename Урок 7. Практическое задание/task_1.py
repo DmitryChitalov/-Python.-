9040a8213d3037c0c5b13044aa9ad0e6846dbeeb
dakostyleva <dashatkacheva@gmail.com>
2020-05-14 15:35:14 +0300

    Lesson 7_T1

--- Урок 7. Практическое задание/task_1.py
+++ Урок 7. Практическое задание/task_1.py
@@ -9,3 +9,73 @@
 Подсказка: обратите внимание, сортируем не по возрастанию, как в примере,
 а по убыванию
 """
+'''
+Comment:
+Я попробовала для наглядности эксперимента интервал от -1000 до 1000 и различные длины массива (10, 100 и 500). 
+Результаты с добавлением заметно сокращали время. Чем больше входные данные, тем сильнее эффект.
+'''
+import random
+import timeit
+
+
+def bubble(ls):
+    n = 1
+    while n < len(ls):
+        k = 0
+        for i in range(len(ls) - n):
+            if ls[i] < ls[i + 1]:
+                ls[i], ls[i + 1] = ls[i + 1], ls[i]
+                k += 1
+        if k == 0:
+            break
+        n += 1
+    return ls
+
+
+# замеры 10
+
+my_list = [random.randint(-1000, 1000) for i in range(10)]
+print(my_list)
+print(bubble(my_list))
+
+print(timeit.timeit("bubble(my_list)", setup="from __main__ import bubble, my_list", number=1000))
+
+'''
+Без добавления:
+0.01072485799999999
+
+С добавлением:
+0.002004884999999998
+'''
+
+# замеры 100
+
+my_list = [random.randint(-1000, 1000) for i in range(100)]
+print(my_list)
+print(bubble(my_list))
+
+print(timeit.timeit("bubble(my_list)", setup="from __main__ import bubble, my_list", number=1000))
+
+'''
+Без добавления:
+0.396699641
+
+С добавлением:
+0.016689071
+'''
+
+# замеры 500
+
+my_list = [random.randint(-1000, 1000) for i in range(500)]
+print(my_list)
+print(bubble(my_list))
+
+print(timeit.timeit("bubble(my_list)", setup="from __main__ import bubble, my_list", number=1000))
+
+'''
+Без добавления:
+10.223768448
+
+С добавлением:
+0.044398813999999995
+'''
