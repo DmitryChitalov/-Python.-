a2c0ef640c6c38f30616955c934b9ed46ce7cbf2
dakostyleva <dashatkacheva@gmail.com>
2020-05-14 15:35:24 +0300

    Lesson 7_T3

--- Урок 7. Практическое задание/task_3.py
+++ Урок 7. Практическое задание/task_3.py
@@ -6,3 +6,28 @@
 массива. Но если это слишком сложно, то используйте метод сортировки,
 который не рассматривался на уроках
 """
+import random
+
+m = int(input("Введите число m: "))
+n = 2 * m + 1
+array = [random.randint(0, 10) for i in range(n)]
+print(array)
+
+
+def median(arr):
+    k = 0
+    while k < n:
+        arr_part_1 = []
+        arr_part_2 = []
+        for i in arr:
+            if i > arr[k]:
+                arr_part_2.append(i)
+            else:
+                arr_part_1.append(i)
+        if len(arr_part_1) == (len(arr_part_2) + 1):
+            return arr[k]
+        k += 1
+    return
+
+
+print(f'Медиана списка: {median(array)}')
