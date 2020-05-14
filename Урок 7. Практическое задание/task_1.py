71aa5cbf6f350350cb1354cd1ba72ea0505fd8b0
Nikita Shataev <shataev.nikita5@gmail.com>
2020-05-14 17:18:45 +0700

    homework 7

--- Урок 7. Практическое задание/task_1.py
+++ Урок 7. Практическое задание/task_1.py
@@ -9,3 +9,56 @@
 Подсказка: обратите внимание, сортируем не по возрастанию, как в примере,
 а по убыванию
 """
+from random import randint
+from timeit import timeit
+
+MAX_SIZE = int(input('Введите размер массива: '))
+
+
+def bubble_sort(array):
+    for i in range(len(array) - 1, 0, -1):
+        flag = True
+        for n in range(i):
+            if array[n] < array[n+1]:
+                array[n], array[n+1] = array[n+1], array[n]
+                flag = False
+
+        if flag == True:
+            break
+    return array
+
+
+def bubble_sort_no_smart(array):
+    for i in range(len(array) - 1, 0, -1):
+        for n in range(i):
+            if array[n] < array[n+1]:
+                array[n], array[n+1] = array[n+1], array[n]
+
+    return array
+
+
+numbers = [randint(-100, 100) for _ in range(MAX_SIZE)]
+
+print(bubble_sort(numbers))
+
+time1 = timeit(f'bubble_sort({numbers})',
+               setup='from __main__ import bubble_sort',
+               number=1000)
+
+numbers = [randint(-100, 100) for _ in range(MAX_SIZE)]
+
+time2 = timeit(f'bubble_sort_no_smart({numbers})',
+               setup='from __main__ import bubble_sort_no_smart',
+               number=1000)
+print(time1)
+print(time2)
+
+"""
+Время работы функции без улучшения с увеличением кол-ва элементов массива стремительно увеличивается
+    на 100 элементах:
+        с улучшением: 0.009916695
+        без улучшения: 0.6977301520000001
+    на 1000 элементов:
+        с улучшением: 0.09072035
+        без улучшения: 77.99184451800001
+"""
--- Урок 7. Практическое задание/task_3.py
+++ Урок 7. Практическое задание/task_3.py
@@ -6,3 +6,29 @@
 массива. Но если это слишком сложно, то используйте метод сортировки,
 который не рассматривался на уроках
 """
+import statistics
+from random import randint
+
+m = int(input('Введите m: '))
+
+numbers = [randint(0, 50) for _ in range(2*m+1)]
+
+
+def median(nums):
+    nums.sort()
+    mid = len(nums)//2
+    return nums[mid]
+
+
+def no_sort(nums):
+    while len(nums) >1:
+        min_numb = min(nums)
+        max_numb = max(nums)
+        nums.remove(min_numb)
+        nums.remove(max_numb)
+    return nums[0]
+
+print('Исходный массив:', numbers)
+print(f'Медиана: {median(numbers)}')
+print(f'Медиана statistics: {statistics.median(numbers)}')
+print(f'Медиана без сортировки: {no_sort(numbers)}')
