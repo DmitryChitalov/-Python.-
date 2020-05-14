77dfaff8b4c96b43c635a9ec7f9ffcea8e876d1c
AlexeyDubrov <dubrov.aleksei@gmail.com>
2020-05-14 14:04:47 +0300

    Задания урока 7

--- Урок 7. Практическое задание/task_2.py
+++ Урок 7. Практическое задание/task_2.py
@@ -8,3 +8,43 @@
 Исходный - [46.11436617832828, 41.62921998361278, 18.45859540989644, 12.128870723745806, 8.025098788570562]
 Отсортированный - [8.025098788570562, 12.128870723745806, 18.45859540989644, 41.62921998361278, 46.11436617832828]
 """
+import random
+
+LST_LEN = 10
+random_lst = [round(random.uniform(0, 49.99), 2) for i in range(LST_LEN)]
+
+
+def merge_sort(lst):
+
+    if len(lst) <= 1:
+        return lst
+
+    left_side = merge_sort(lst[:len(lst) // 2])
+    right_side = merge_sort(lst[len(lst) // 2:])
+
+    i = j = 0
+    res = []
+
+    while i < len(left_side) or j < len(right_side):
+
+        if i >= len(left_side):
+            res.append(right_side[j])
+            j += 1
+
+        elif j >= len(right_side):
+            res.append(left_side[i])
+            i += 1
+
+        elif left_side[i] < right_side[j]:
+            res.append(left_side[i])
+            i += 1
+
+        else:
+            res.append(right_side[j])
+            j += 1
+
+    return res
+
+
+print(f'Исходный массив:\n{random_lst}\n')
+print(f'Сортированный массив:\n{merge_sort(random_lst)}')
