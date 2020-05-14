9e44e2a851a3fd10163d303c44c79935f553e101
dakostyleva <dashatkacheva@gmail.com>
2020-05-14 15:35:20 +0300

    Lesson 7_T2

--- Урок 7. Практическое задание/task_2.py
+++ Урок 7. Практическое задание/task_2.py
@@ -8,3 +8,44 @@
 Исходный - [46.11436617832828, 41.62921998361278, 18.45859540989644, 12.128870723745806, 8.025098788570562]
 Отсортированный - [8.025098788570562, 12.128870723745806, 18.45859540989644, 41.62921998361278, 46.11436617832828]
 """
+
+#Comment: Хотелось придумать что-то свое, но не получилось:)
+
+import random
+
+n = int(input('Введите количество элементов: '))
+my_array = [random.random() * 50 for i in range(n)]
+print(my_array)
+
+
+def merge(array):
+    if len(array) > 1:
+        k = len(array) // 2
+        arr1 = array[:k]
+        arr2 = array[k:]
+        merge(arr1)
+        merge(arr2)
+        i = 0
+        j = 0
+        l = 0
+        while i < len(arr1) and j < len(arr2):
+            if arr1[i] < arr2[j]:
+                array[l] = arr1[i]
+                i += 1
+            else:
+                array[l] = arr2[j]
+                j += 1
+            l += 1
+
+        while i < len(arr1):
+            array[l] = arr1[i]
+            i += 1
+            l += 1
+
+        while j < len(arr2):
+            array[l] = arr2[j]
+            j += 1
+            l += 1
+        return array
+
+print(merge(my_array))
