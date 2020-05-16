0138e78cf09b47dc75acf90f3d7272853bb9f7bd
alex vydrin <tapat@mail.ru>
2020-05-16 19:00:28 +0300

    py_algos_lesson_8

--- Урок 8. Практическое задание/task_1.py
+++ Урок 8. Практическое задание/task_1.py
@@ -16,3 +16,55 @@
 
 Итог: 6 подстрок
 """
+
+
+from hashlib import sha1
+
+
+def get_dict_substring(string):
+    """
+    Функция ищет различные подстроки в строке и возвращает словарь хешей подстрок
+    :param string: строка для поиска подстрок
+    :return dict_substring: словарь хешей подстрок
+    """
+    len_string = len(string)  # вычисляем длину строки только один раз (для оптимизации)
+    dict_substr = {}
+    # ищем все возможные варианты подстрок
+    for i in range(0, len_string + 1):
+        for j in range(i + 1, len_string + 1):
+            substr = string[i:j]
+            # if substr == string: - для оптимизации перепишем условие
+            if (not i) and j == len_string:  # не включаем строку целиком
+                continue
+            dict_substr[sha1(substr.encode('utf-8')).hexdigest()] = substr
+    return dict_substr
+
+
+def main():
+    """
+    основной код
+    """
+    string = input("Введите строку для поиска подстрок: ")
+    dict_substring = get_dict_substring(string)
+    print("Словарь хешей подстрок:")
+    for k, val in dict_substring.items():
+        print(f"{k} : {val}")
+    print(f"Итог: {len(dict_substring)} подстрок")
+
+
+# Понравилась идея из примера haffman_4.py всегда выделять код в отдельную функцию
+# во-первых, прочитал о том, что такой код выполняется быстрее
+# из-за особенностей хранения глобальных и локальных переменных в оперативной памяти
+# во-вторых, pylint не ругается на наименования переменных в нижнем регистре
+main()
+
+# Результат выполнения программы:
+# Введите строку для поиска подстрок: papa
+# Словарь хешей подстрок:
+# 516b9783fca517eecbd1d064da2d165310b19759 : p
+# 379fc0d5299a71ac0f171fbb5afb262829b4e765 : pa
+# 627a19572de5279b23ee9767fc5cf4b270625ac6 : pap
+# 86f7e437faa5a7fce15d1ddcb9eaeaea377667b8 : a
+# ac78b022715c5b8357b4dca8045e8463b4de2124 : ap
+# 313212a1870215e863a9da1859fbaa6e53f50670 : apa
+# Итог: 6 подстрок
--- Урок 8. Практическое задание/task_2.py
+++ Урок 8. Практическое задание/task_2.py
@@ -8,3 +8,104 @@ s = "beep boop beer!"
 Результат:
 00 11 11 101 010 00 011 011 101 010 00 11 11 1000 1001
 """
+
+# Решил использовать инструменты ООП, мне кажется для данной задачи это наиболее наглядно
+# 1) Вычислить частоту использований символов в строке
+# 2) Реализовать построение дерева на основании частоты использований символов
+# 3) На основании дерева закодировать строку
+
+from collections import Counter, deque
+
+
+class Node:
+
+    """
+    узел дерева
+    weight - вес (сумма частот использований символов во всех входящих узлах
+    left - левый узел дерева
+    right - правый узел дерева
+    """
+
+    def __init__(self, weight=None, left=None, right=None):
+        self.weight = weight
+        self.left = left
+        self.right = right
+
+    def __repr__(self):
+        return f"Node[{self.left}&{self.right}={self.weight}]"
+
+
+def make_haffman_tree(deque_string):
+    """
+    формируем дерево на основании очереди
+    :param deque_string: очередь кортежей (частоты символов) для формирования дерева
+    :return: корневой узел дерева
+    """
+    while len(deque_string) > 1:
+        print(deque_string)  # для наглядности процесса формирования дерева
+        # формируем очередной узел из двух левых элементов очереди и удаляем эти два элемента
+        weight = deque_string[0][1] + deque_string[1][1]
+        node = Node(weight=weight, left=deque_string.popleft()[0], right=deque_string.popleft()[0])
+        # вставляем новый элемент в отсортированную очередь
+        for i, item in enumerate(deque_string):
+            if weight > item[1]:
+                continue
+            deque_string.insert(i, (node, weight))
+            break
+        else:
+            deque_string.append((node, weight))
+
+    return deque_string[0][0]
+
+
+def haffman_code(tree, path=''):
+    """
+    рекурсивная функция кодирования на основании дерева
+    таблицу кодировки записываем в глобальную переменную CODE_TABLE
+    """
+    if not isinstance(tree, Node):
+        CODE_TABLE[tree] = path
+    else:
+        haffman_code(tree.left, path=f'{path}0')
+        haffman_code(tree.right, path=f'{path}1')
+
+
+def main():
+    """
+    основной код
+    """
+    string = 'beep boop beer!'
+    print(f'Строка для кодирования: {string}')
+    # 1) Вычислить частоту использований символов в строке
+    counter_string = Counter(string)
+    print(counter_string)
+    # 2) Реализовать построение дерева на основании частоты использований символов
+    deque_string = deque(sorted(counter_string.items(), key=lambda item: item[1]))
+    node_0 = make_haffman_tree(deque_string)
+    # 3) На основании дерева закодировать строку
+    haffman_code(node_0)
+    print("Таблица кодирования:")
+    print(CODE_TABLE)
+    print("Закодированная строка:")
+    for i in string:
+        print(CODE_TABLE[i], end=' ')
+
+
+# Таблица кодировки символов
+CODE_TABLE = dict()
+
+main()
+
+# Результат выполнения программы:
+# Строка для кодирования: beep boop beer!
+# Counter({'e': 4, 'b': 3, 'p': 2, ' ': 2, 'o': 2, 'r': 1, '!': 1})
+# deque([('r', 1), ('!', 1), ('p', 2), (' ', 2), ('o', 2), ('b', 3), ('e', 4)])
+# deque([(Node[r&!=2], 2), ('p', 2), (' ', 2), ('o', 2), ('b', 3), ('e', 4)])
+# deque([(' ', 2), ('o', 2), ('b', 3), (Node[Node[r&!=2]&p=4], 4), ('e', 4)])
+# deque([('b', 3), (Node[ &o=4], 4), (Node[Node[r&!=2]&p=4], 4), ('e', 4)])
+# deque([(Node[Node[r&!=2]&p=4], 4), ('e', 4), (Node[b&Node[ &o=4]=7], 7)])
+# deque([(Node[b&Node[ &o=4]=7], 7), (Node[Node[Node[r&!=2]&p=4]&e=8], 8)])
+# Таблица кодирования:
+# {'b': '00', ' ': '010', 'o': '011', 'r': '1000', '!': '1001', 'p': '101', 'e': '11'}
+# Закодированная строка:
+# 00 11 11 101 010 00 011 011 101 010 00 11 11 1000 1001
