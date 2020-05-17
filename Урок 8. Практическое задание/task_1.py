bc15165f0555ec3c83449e1e33910a4bd3df306e
Mariia <58525490+MariiaShara@users.noreply.github.com>
2020-05-17 19:11:45 -0400

    Домашнее задание к уроку 8

--- Урок 8. Практическое задание/task_1.py
+++ Урок 8. Практическое задание/task_1.py
@@ -16,3 +16,27 @@
 
 Итог: 6 подстрок
 """
+
+import hashlib
+
+def substr_count(S):
+
+    N = len(S)
+    MY_SET = set()
+
+    for i in range(N):
+        for j in range(N, i, -1):
+            S_1 = S[i:j]
+            S_2 = hashlib.sha1(S_1.encode("utf-8"))
+            S_3 = S_2.hexdigest()
+            MY_SET.add(S_3)
+    MY_SET.remove(hashlib.sha1(S.encode("utf-8")).hexdigest())
+    return len(MY_SET)
+
+S = input("Введите строку: ")
+print(f"Количество подстрок в строке {S}: {substr_count(S)}.")
+
+
+
+
+
--- Урок 8. Практическое задание/task_2.py
+++ Урок 8. Практическое задание/task_2.py
@@ -8,3 +8,46 @@ s = "beep boop beer!"
 Результат:
 00 11 11 101 010 00 011 011 101 010 00 11 11 1000 1001
 """
+
+from collections import Counter, deque
+
+def haffman_tree(my_str):
+
+    frq_count = Counter(my_str)
+    frq = deque(sorted(frq_count.items(), key=lambda i: i[1]))
+
+    if len(frq) != 1:
+        while len(frq) > 1:
+            weight = frq[0][1] + frq[1][1]
+            united_el = {0: frq.popleft()[0], 1: frq.popleft()[0]}
+            for i, _frequency in enumerate(frq):
+                if weight > _frequency[1]:
+                    continue
+                else:
+                    frq.insert(i, (united_el, weight))
+                    break
+            else:
+                frq.append((united_el, weight))
+    else:
+        weight = frq[0][1]
+        united_el = {0: frq.popleft()[0], 1: None}
+        frq.append((united_el, weight))
+    return frq[0][0]
+
+haffman_table = dict()
+
+def haffman_code(tree, code=''):
+
+    if not isinstance(tree, dict):
+        haffman_table[tree] = code
+    else:
+        haffman_code(tree[0], code=f'{code}0')
+        haffman_code(tree[1], code=f'{code}1')
+
+
+my_str = input("Введите строку из трех слов для кодирования: ")
+haffman_code(haffman_tree(my_str))
+
+for i in my_str:
+    print(haffman_table[i], end=' ')
+print()
