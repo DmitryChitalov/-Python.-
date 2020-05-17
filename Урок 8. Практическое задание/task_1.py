2dee8c32dc3f7f1c6f7cd937f7303d523fb42479
AlexeyDubrov <dubrov.aleksei@gmail.com>
2020-05-17 17:52:21 +0300

    ДЗ урока 8

--- Урок 8. Практическое задание/task_1.py
+++ Урок 8. Практическое задание/task_1.py
@@ -16,3 +16,22 @@
 
 Итог: 6 подстрок
 """
+s = 'papa'
+
+
+def search_strings(s):
+    n = len(s)
+    arr_str = set()
+    for i in range(1, n):
+
+        for j in range(n - i + 1):
+
+            k = hash(s[j:j+i])
+
+            if k not in arr_str:
+                arr_str.add(k)
+
+    return len(arr_str)
+
+
+print(f'Количество подстрок: {search_strings(s)}')
