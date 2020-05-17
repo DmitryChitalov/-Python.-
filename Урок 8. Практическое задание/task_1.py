6132555b3c676ad556ef38d8bc8c3fba11e3a00c
monstric93 <64038672+monstric93@users.noreply.github.com>
2020-05-17 14:33:42 +0300

    Домашнее задание №8

--- Урок 8. Практическое задание/task_1.py
+++ Урок 8. Практическое задание/task_1.py
@@ -2,10 +2,8 @@
 Определение количества различных подстрок с использованием хеш-функции.
 Пусть дана строка S длиной N, состоящая только из маленьких латинских букв.
 Требуется найти количество различных подстрок в этой строке.
-
 Пример:
 строка: рара
-
 подстроки:
 рар
 ра
@@ -13,6 +11,57 @@
 а
 ар
 ара
-
 Итог: 6 подстрок
 """
+
+
+# Функция извлечения подстрок из строки
+def find_substrings(string, substring='', start_index=0):
+    """
+    :param string: строка введеная пользователем
+    :param substring: подстрока
+    :param start_index: стартовый индекс в строке для поиска подстроки
+    :return: список подстрок, количество подстрок
+    """
+    list_substrings = []
+    length_string = len(string)
+    while start_index != length_string:
+        for alpha in string[start_index:]:
+            substring = ''.join((substring, alpha))
+            if substring not in list_substrings and substring != string:
+                list_substrings.append(substring)
+        start_index += 1
+        substring = ''
+    return list_substrings, len(list_substrings)
+
+
+""" --------------------------------------------------- """
+
+
+# Функция извлечения подстрок из строки с несколькими словами
+def find_substrings_in_string(string, substring='', start_index=0):
+    """
+    :param string: строка введеная пользователем
+    :param substring: подстрока
+    :param start_index: стартовый индекс в строке для поиска подстроки
+    :return: список подстрок, количество подстрок
+    """
+    list_substrings = []
+    for word in string.split():
+        while start_index != len(word):
+            for alpha in word[start_index:]:
+                substring = ''.join((substring, alpha))
+                if substring not in list_substrings and substring != word:
+                    list_substrings.append(substring)
+            start_index += 1
+            substring = ''
+    return list_substrings, len(list_substrings)
+
+
+""" --------------------------------------------------- """
+STRING = input('Введите строку:\n').lower()
+
+print(f'Список подстрок:')
+for element in find_substrings(STRING)[0]:
+    print(f'{element}')
+print(f'Итого подстрок: {find_substrings(STRING)[1]}')
--- Урок 8. Практическое задание/task_2.py
+++ Урок 8. Практическое задание/task_2.py
@@ -1,10 +1,53 @@
 """
 Закодируйте любую строку из трех слов по алгоритму Хаффмана.
-
 Пример:
 строка для кодирования
 s = "beep boop beer!"
-
 Результат:
 00 11 11 101 010 00 011 011 101 010 00 11 11 1000 1001
 """
+from collections import Counter, deque
+
+
+def haffman_encode(param):
+    count_param = Counter(param)
+    if len(count_param) == 1:
+        return {0: list(count_param)[0], 1: None}
+    sort = sorted(count_param.items(), key=lambda x: x[1])
+    while len(sort) > 1:
+        weight = sort[0][1] + sort[1][1]
+        element = {0: sort[0][0], 1: sort[1][0]}
+        sort = sort[2:]
+        for i, item in enumerate(sort):
+            if weight <= item[1]:
+                sort.insert(i, (element, weight))
+                break
+        else:
+            sort.append((element, weight))
+    return sort[0][0]
+
+
+def haffman_freq(tree, path=''):
+    if not isinstance(tree, dict):
+        ENCODE_DICT[tree] = path
+    else:
+        haffman_freq(tree[0], path=f'{path}0')
+        haffman_freq(tree[1], path=f'{path}1')
+
+
+while True:
+    STRING = input('Введите любую строку из трех слов:\n')
+    if len(STRING.split()) == 3:
+        break
+    else:
+        print(f'Ошибка ввода строки.\nНеобходимо ввести строку из ТРЕХ слов!')
+ENCODE_DICT = {}
+haffman_freq(haffman_encode(STRING))
+
+print(f'\nРезультат:')
+for alpha in STRING:
+    print(f'{ENCODE_DICT[alpha]}', end=' ')
+
+print(f'\n\nКоды символов:')
+for key, value in ENCODE_DICT.items():
+    print(f'{key} : {value}')
