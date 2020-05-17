03003e0e5b5f97395559e939935e9448b6d479bc
Begemotina <Begemotina@nightmail.ru>
2020-05-18 01:17:23 +0300

    лишнее

--- Урок 8. Практическое задание/task_2.py
+++ Урок 8. Практическое задание/task_2.py
@@ -19,17 +19,7 @@ def haffman_tree(s):
     if len(sorted_elements) != 1:
         while len(sorted_elements) > 1:
             knot = sorted_elements[0][1] + sorted_elements[1][1]
-            # Словарь из 2 крайних левых элементов, попутно вырезаем их
-            # из "sorted_elements" (из очереди).
-            # comb - объединенный элемент
-            '''
-            {0: 'r', 1: '!'}
-            {0: {0: 'r', 1: '!'}, 1: 'p'}
-            {0: ' ', 1: 'o'}
-            {0: 'b', 1: {0: ' ', 1: 'o'}}
-            {0: {0: {0: 'r', 1: '!'}, 1: 'p'}, 1: 'e'}
-            {0: {0: 'b', 1: {0: ' ', 1: 'o'}}, 1: {0: {0: {0: 'r', 1: '!'}, 1: 'p'}, 1: 'e'}}
-            '''
+
             comb = {0: sorted_elements.popleft()[0],
                     1: sorted_elements.popleft()[0]}
 
@@ -59,7 +49,7 @@ def haffman_code(tree, path=''):
         haffman_code(tree[1], path=f'{path}1')
 
 
-s = "глокая куздра будланула бокра"
+s = "глокая куздра будланула"
 
 haffman_code(haffman_tree(s))
 
