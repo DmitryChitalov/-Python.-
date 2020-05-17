9d068a3bfebc035b01fc51beaec29d4460ca7c93
Puff <puffvl@gmail.com>
2020-05-17 22:54:50 +1000

    Lesson7

--- Урок 7. Практическое задание/task_2.py
+++ Урок 7. Практическое задание/task_2.py
@@ -8,3 +8,35 @@
 Исходный - [46.11436617832828, 41.62921998361278, 18.45859540989644, 12.128870723745806, 8.025098788570562]
 Отсортированный - [8.025098788570562, 12.128870723745806, 18.45859540989644, 41.62921998361278, 46.11436617832828]
 """
+
+from random import uniform
+
+source = [uniform(0, 50) for i in range(5)]
+print('Исходный - ', source)
+
+
+def merge(left, right):
+    result = []
+    i = 0
+    y = 0
+    while i < len(left) and y < len(right):
+        if left[i] <= right[y]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[y])
+            y += 1
+    result += left[i:] + right[y:]
+    return result
+
+
+def sort(source):
+    if len(source) <= 1:
+        return source
+    else:
+        left = source[:len(source) // 2]
+        right = source[len(source) // 2:]
+    return merge(sort(left), sort(right))
+
+
+print('Отсортированный -', sort(source))
--- Урок 7. Практическое задание/task_3.py
+++ Урок 7. Практическое задание/task_3.py
@@ -6,3 +6,36 @@
 массива. Но если это слишком сложно, то используйте метод сортировки,
 который не рассматривался на уроках
 """
+from random import randint
+import statistics
+
+m = 4
+y = 2 * m + 1
+x = [randint(1, 50) for i in range(y)]
+n = len(x) // 2
+
+def median(n, x):
+    pivot = x[randint(0, len(x)-1)]
+    left = []
+    for i in x:
+        if i < pivot:
+            left.append(i)
+
+    if len(left) > n:
+        return median(n, left)
+    n -= len(left)
+
+    pivot_equal = x.count(pivot)
+    if pivot_equal > n:
+        return pivot
+    n -= pivot_equal
+    right = []
+    for i in x:
+        if i > pivot:
+            right.append(i)
+    return median(n, right)
+
+
+print('Исходный список - ',x)
+print('Найденная медиана - ' ,median(n, x))
+print('Проверка готовой функцией , медиана - ' ,statistics.median(x))
