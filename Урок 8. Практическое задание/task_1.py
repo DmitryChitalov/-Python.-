4c703ef129018df291bfe016eb9dfd8cf72cc054
liliyakozlova <l.kozlova89@gmail.com>
2020-05-18 13:28:55 +0300

    lesson8

--- Урок 8. Практическое задание/task_1.py
+++ Урок 8. Практическое задание/task_1.py
@@ -16,3 +16,23 @@
 
 Итог: 6 подстрок
 """
+import hashlib
+
+
+def unique_substr(s):
+    l = len(s)
+    substr_hash = set()
+    substr = set()
+    for i in range(l):
+        if i == 0:
+            l = len(s) - 1
+        else:
+            l = len(s)
+        for n in range(l, i, -1):
+            substr.add(s[i:n])
+            substr_hash.add(hashlib.sha1(s[i:n].encode('utf-8')).hexdigest())
+    return f'В строке {s} количество уникальных подстрок - {len(substr_hash)}:\n{substr}'
+
+
+my_str = input('Введите строку из маленьких латинских букв: ')
+print(unique_substr(my_str))
--- Урок 8. Практическое задание/task_2.py
+++ Урок 8. Практическое задание/task_2.py
@@ -8,3 +8,41 @@ s = "beep boop beer!"
 Результат:
 00 11 11 101 010 00 011 011 101 010 00 11 11 1000 1001
 """
+from collections import Counter, deque
+
+my_str = input('Введите строку из трех слов: ')
+
+def tree(new_str):
+    count_str = Counter(new_str)
+    sort_el = deque(sorted(count_str.items(), key=lambda item: item[1]))
+    if len(sort_el) != 1:
+        while len(sort_el) > 1:
+            sum_freq = sort_el[0][1] + sort_el[1][1]
+            union_el = {0: sort_el.popleft()[0], 1: sort_el.popleft()[0]}
+            for i, count in enumerate(sort_el):
+                if sum_freq > count[1]:
+                    continue
+                else:
+                    sort_el.insert(i, (union_el, sum_freq))
+                    break
+            else:
+                sort_el.append((union_el, sum_freq))
+    else:
+        sum_freq = sort_el[0][1]
+        union_el = {0: sort_el.popleft()[0], 1: None}
+        sort_el.append((union_el, sum_freq))
+    return sort_el[0][0]
+
+table = dict()
+
+def write_code(tree, path=''):
+    if not isinstance(tree, dict):
+        table[tree] = path
+    else:
+        write_code(tree[0], path=f'{path}0')
+        write_code(tree[1], path=f'{path}1')
+
+
+write_code(tree(my_str))
+
+print(list(table.values()))
