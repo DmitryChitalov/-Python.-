2919fb5474897d40976f6a460bbf7e6d200f70f7
SkalyauhDP <dmitriy.skalyauh@gmail.com>
2020-05-19 08:46:58 +0400

    Lesson-8

--- Урок 8. Практическое задание/task_1.py
+++ Урок 8. Практическое задание/task_1.py
@@ -16,3 +16,20 @@
 
 Итог: 6 подстрок
 """
+
+import hashlib
+
+S = input("Введите строку из маленьких латинских букв: ")
+
+substrings = {S[0]: hashlib.sha1(S[0].encode('utf-8')).hexdigest()}
+for i in range(1, len(S)):  # длина подстроки
+    for j in range(len(S) - i + 1):  # с каких позиций искать подстроку
+        substrings[S[j: j + i]] = hashlib.sha1(S[j: j + i].encode('utf-8')).hexdigest()
+
+substrings_count = 0
+for keys in substrings.keys():
+    print(keys)
+    substrings_count += 1
+
+print()
+print(f"Итог: {substrings_count} подстрок")
--- Урок 8. Практическое задание/task_2.py
+++ Урок 8. Практическое задание/task_2.py
@@ -8,3 +8,48 @@ s = "beep boop beer!"
 Результат:
 00 11 11 101 010 00 011 011 101 010 00 11 11 1000 1001
 """
+
+from collections import Counter, deque
+
+def tree_by_haffman(string_for_hashing):
+    counts = Counter(string_for_hashing)
+    sorted_elements = deque(sorted(counts.items(), key=lambda item: item[1]))
+    if len(sorted_elements) != 1:
+        while len(sorted_elements) > 1:
+            el_weight = sorted_elements[0][1] + sorted_elements[1][1]
+            combination = {0: sorted_elements.popleft()[0],
+                    1: sorted_elements.popleft()[0]}
+            for i, _count in enumerate(sorted_elements):
+                if el_weight > _count[1]:
+                    continue
+                else:
+                    sorted_elements.insert(i, (combination, el_weight))
+                    break
+            else:
+                sorted_elements.append((combination, el_weight))
+    else:
+        el_weight = sorted_elements[0][1]
+        combination = {0: sorted_elements.popleft()[0], 1: None}
+        sorted_elements.append((combination, el_weight))
+    return sorted_elements[0][0]
+
+
+table_of_hashes = dict()
+
+def hash_by_haffman(tree, path=''):
+    if not isinstance(tree, dict):
+        table_of_hashes[tree] = path
+    else:
+        hash_by_haffman(tree[0], path=f'{path}0')
+        hash_by_haffman(tree[1], path=f'{path}1')
+
+
+#string_for_hashing = "beep boop beer!"
+string_for_hashing = input("Введите строку для хэширования: ")
+
+
+hash_by_haffman(tree_by_haffman(string_for_hashing))
+
+for i in string_for_hashing:
+    print(table_of_hashes[i], end=' ')
+print()
