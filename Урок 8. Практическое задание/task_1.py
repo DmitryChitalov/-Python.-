b850fe2626b663b472214c64e1d3e27c072d0491
Puff <puffvl@gmail.com>
2020-05-20 17:35:47 +1000

    Lesson№8

--- Урок 8. Практическое задание/task_1.py
+++ Урок 8. Практическое задание/task_1.py
@@ -16,3 +16,16 @@
 
 Итог: 6 подстрок
 """
+import hashlib
+ready_list = []
+d = set()
+s = 'трава'
+n = len(s)
+for i in range(n):
+    for y in range(i, n + 1):
+        if i == y or i == 0 and y == n:
+            continue
+        ready_list.append(s[i:y])
+        d.add(hashlib.sha1(s[i:y].encode('utf-8')).hexdigest())
+print(ready_list)
+print(d)
