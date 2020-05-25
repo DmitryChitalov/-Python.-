f8002ae996a41787fa5a9166e9b9edf2f012b04f
dakostyleva <dashatkacheva@gmail.com>
2020-05-25 17:55:59 +0300

    Lesson 8_1

--- Урок 8. Практическое задание/task_1.py
+++ Урок 8. Практическое задание/task_1.py
@@ -16,3 +16,23 @@
 
 Итог: 6 подстрок
 """
+
+import hashlib
+
+
+string = input("Введите строку из маленьких латинских букв: ")
+num = set()
+
+n = len(string)
+for i in range(n):
+    if i == 0:
+        n = len(string) - 1
+    else:
+        n = len(string)
+    for j in range(n, i, -1):
+        #print(string[i:j])
+        num.add(hashlib.sha1(string[i:j].encode('utf-8')).hexdigest())
+#print(num)
+
+
+print(f"Количество подстрок в строке равно {len(num)}")
