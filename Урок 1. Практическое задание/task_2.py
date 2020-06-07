b218be895b252e998969df7ce565d28b6f6dcbee
Farrukhraz <raz_far@mail.ru>
2020-06-08 01:10:23 +0300

    Первые 4 задания

--- Урок 1. Практическое задание/task_2.py
+++ Урок 1. Практическое задание/task_2.py
@@ -6,3 +6,20 @@
 Подсказка: это стандартные операции, которые осуществляются с помощью
 стандартных операторов. Попробуйте найти каких именно.
 """
+
+n1 = 5
+n2 = 6
+
+bit_or = n1 | n2
+bit_and = n1 & n2
+bit_xor = n1 ^ n2
+
+print(f"Bit or of 5 and 6 = {bit_or}")
+print(f"Bit and of 5 and 6 = {bit_and}")
+print(f"Bit xor of 5 and 6 = {bit_xor}")
+
+print(f"{n1} bit shift to left by 2 places {n1 << 2}")
+print(f"{n1} bit shift to left by 2 places {n1 >> 2}")
+
+print(f"{n2} bit shift to left by 2 places {n2 << 2}")
+print(f"{n2} bit shift to left by 2 places {n2 >> 2}")
--- Урок 1. Практическое задание/task_3.py
+++ Урок 1. Практическое задание/task_3.py
@@ -13,3 +13,26 @@ k – угловой коэффициент (действительное чис
 X1_VAL = 2, Y1_VAL = 3, X2_VAL = 4, Y2_VAL = 5
 Уравнение прямой, проходящей через эти точки: y = 1.0x + 1.0
 """
+
+try:
+    x1 = int(input("Enter x1. It should be a number:"))
+    y1 = int(input("Enter y1. It should be a number:"))
+    x2 = int(input("Enter x2. It should be a number:"))
+    y2 = int(input("Enter y2. It should be a number:"))
+
+    k = (y2 - y1) / (x2 - x1)
+    b = (x2 * y1 - y2 * x1) / (x2 - x1)
+
+    if k < 0:
+        k = f"({str(k)})"
+    if b < 0:
+        b = f"({str(b)})"
+
+    print(f"Equation of a Straight Line: y={k}x+{b}")
+
+except ValueError:
+    print("Only numbers should be entered.")
+except Exception as ex:
+    print(ex)
+
+
