b4dc360d8c9adc8605addc5ec056619452e032c5
Alex <hlbu@yandex.ru>
2020-06-09 22:23:14 +0300

    Урок 1. added corrections elif/

--- Урок 1. Практическое задание/task_9.py
+++ Урок 1. Практическое задание/task_9.py
@@ -10,12 +10,9 @@ c = int(input('Enter 3  number : '))
 
 if a == b == c:
     print('числа равны')
-else:
-    if a < b < c or c < b < a:
-        print(f'число {b} среднее')
-    else:
-        if b < a < c or c < a < b:
-            print(f'число {a} среднее')
-        else:
-            if a < c < b or b < c < a:
-                print(f'число {c} среднее')
+elif a < b < c or c < b < a:
+    print(f'число {b} среднее')
+elif b < a < c or c < a < b:
+    print(f'число {a} среднее')
+elif a < c < b or b < c < a:
+    print(f'число {c} среднее')
