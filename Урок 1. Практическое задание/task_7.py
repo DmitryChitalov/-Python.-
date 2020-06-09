bc95917c9673969cdc7189d22a551db802a72776
leget <lobanov.leget@yandex.ru>
2020-06-09 22:12:15 +0300

    Сделал 7 задание

--- Урок 1. Практическое задание/task_7.py
+++ Урок 1. Практическое задание/task_7.py
@@ -4,3 +4,22 @@
 составленного из этих отрезков. Если такой треугольник существует,
 то определить, является ли он разносторонним, равнобедренным или равносторонним.
 """
+
+print('Введите длинну 3х отрезков по очереди')
+side_of_triangle_a = float(input('Введите сторону а '))
+side_of_triangle_b = float(input('Введите сторону b '))
+side_of_triangle_c = float(input('Введите сторону c '))
+if side_of_triangle_a + side_of_triangle_b <= side_of_triangle_c \
+    or side_of_triangle_a + side_of_triangle_c <= side_of_triangle_b \
+    or side_of_triangle_c + side_of_triangle_b <= side_of_triangle_a:
+    print("С такими сторонами треугольник невозможен")
+elif side_of_triangle_a == side_of_triangle_b == side_of_triangle_c:
+    print('Это равносторонний треугольник')
+elif side_of_triangle_a != side_of_triangle_b != side_of_triangle_c:
+    print('Это разносторонний треугольник')
+elif side_of_triangle_a == side_of_triangle_b and side_of_triangle_a != side_of_triangle_c \
+    or side_of_triangle_a == side_of_triangle_c and side_of_triangle_c != side_of_triangle_b \
+    or side_of_triangle_b == side_of_triangle_c and side_of_triangle_c != side_of_triangle_a:
+    print('Это равнобедренный треугольник')
+
+
