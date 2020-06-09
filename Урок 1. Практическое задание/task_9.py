4b03db75877ebf824e65fbc3e90d3defd6bf0279
R F <R@gmail.com>
2020-06-09 16:10:04 +0200

    Task 9 - Intermediate result

--- Урок 1. Практическое задание/task_9.py
+++ Урок 1. Практическое задание/task_9.py
@@ -4,3 +4,38 @@
 
 Подсказка: можно добавить проверку, что введены равные числа
 """
+
+int_1 = int(input('Enter first number: '))
+int_2 = int(input('Enter second number: '))
+int_3 = int(input('Enter third number: '))
+
+if int_1 > int_2:
+    if int_2 > int_3:
+        print(f'Average number is {int_2}   marker 1')
+    else:
+        print(f'Average number is {int_3}   marker 2')
+elif int_2 > int_1:
+    if int_1 > int_3:
+        print(f'Average number is {int_1}   marker 3')
+    else:
+        print(f'Average number is {int_3}   marker 4')
+elif int_3 > int_1:
+    if int_1 > int_2:
+        print(f'Average number is {int_1}   marker 5')
+    else:
+        print(f'Average number is {int_2}   marker 6')
+elif int_3 > int_2:
+    if int_2 > int_1:
+        print(f'Average number is {int_2}   marker 7')
+    else:
+        print(f'Average number is {int_1}   marker 8')
+elif int_2 > int_3:
+    if int_3 > int_1:
+        print(f'Average number is {int_3}   marker 9')
+    else:
+        print(f'Average number is {int_1}   marker 10')
+
+
+
+
+
