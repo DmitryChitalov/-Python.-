044911d8a28f7f1bd986a05d4f24063073c5e207
Andrey L <alizenberg@yandex.ru>
2020-06-10 15:48:47 +0300

    Девятое задание первого урока

--- Урок 1. Практическое задание/task_9.py
+++ Урок 1. Практическое задание/task_9.py
@@ -4,3 +4,21 @@
 
 Подсказка: можно добавить проверку, что введены равные числа
 """
+print("Данная программа определяет среднее число из трёх. Приготовьтесь ввести 3 числа.")
+number1 = int(input("Введите первое число: "))
+number2 = int(input("Введите второе число: "))
+number3 = int(input("Введите третье число: "))
+if number1 > number2 and number1 < number3:
+    print(f"Среднее число: {number1}")
+elif number1 > number3 and number1 < number2:
+    print(f"Среднее число: {number1}")
+elif number2 > number3 and number2 < number1:
+    print(f"Среднее число: {number2}")
+elif number2 > number1 and number2 < number3:
+    print(f"Среднее число: {number2}")
+elif number3 > number1 and number3 < number2:
+    print(f"Среднее число: {number3}")
+elif number3 > number2 and number3 < number1:
+    print(f"Среднее число: {number3}")
+elif number3 == number1 or number3 == number2 or number1 == number2:
+    print(f"Как минимум 2 числа равны. Среднее число не определяется.")
