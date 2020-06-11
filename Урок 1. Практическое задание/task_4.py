b35c4ab4606321c20c6b4ae1504641fb319a5822
VerkhVektor <59034328+VerkhVektor@users.noreply.github.com>
2020-06-11 13:22:04 +0500

    1 homework

--- Урок 1. Практическое задание/task_4.py
+++ Урок 1. Практическое задание/task_4.py
@@ -14,3 +14,22 @@
 Функцию random() использовать можно
 Опирайтесь на пример к уроку
 """
+
+from random import random
+
+user_edge_min = int(input('Введите нижнюю границу диапаона для случайного целого числа '))
+user_edge_max = int(input('Введите верхнюю границу диапаона для случайного целого числа '))
+diff_edge = user_edge_max - user_edge_min
+rand_int = user_edge_min + round(random()*diff_edge)
+print(rand_int)
+
+edge_float_min = float(input('Введите нижнюю границу диапаона для случайного вещественного числа '))
+edge_float_max = float(input('Введите верхнюю границу диапаона для случайного вещественного числа '))
+diff_edge_float = edge_float_max - edge_float_min
+rand_float = edge_float_min + random()*diff_edge_float
+print(rand_float)
+
+edge_symbol_min = ord(input('Введите нижнюю границу диапаона для случайного символа '))
+edge_symbol_max = ord(input('Введите верхнюю границу диапаона для случайного символа '))
+rand_symbol = edge_symbol_min + round(random()*(edge_symbol_max-edge_symbol_min))
+print(chr(rand_symbol))
