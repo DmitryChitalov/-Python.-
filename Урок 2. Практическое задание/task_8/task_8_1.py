a91290f564f622249cd827b6c9b2cff95e0a6c2c
leget <lobanov.leget@yandex.ru>
2020-06-13 17:09:07 +0300

    Отправил окончательный вариант 8 задания

--- Урок 2. Практическое задание/task_8/task_8_1.py
+++ Урок 2. Практическое задание/task_8/task_8_1.py
@@ -14,44 +14,21 @@
 """
 
 
-#
-# def counter_number():
-#     try:
-#         count_number = int(input('Введите количество чисел: '))
-#         check_number = int(input('Какую цифру считать:  '))
-#         counter = 0
-#         for i in range(1, count_number + 1):
-#             user_number = int(input(f'Введите {i} число: '))
-#             while user_number > 0:
-#                 if user_number % 10 == check_number:
-#                     counter += 1
-#                 user_number //= 10
-#         print(f'Было введено {counter} цифр "{check_number}"')
-#     except ValueError:
-#         print('Вводите корректные данные')
-#         counter_number()
-#
-#
-# counter_number()
+def counter_number():
+    try:
+        count_number = int(input('Введите количество чисел: '))
+        check_number = int(input('Какую цифру считать:  '))
+        counter = 0
+        for i in range(1, count_number + 1):
+            user_number = int(input(f'Введите {i} число: '))
+            while user_number > 0:
+                if user_number % 10 == check_number:
+                    counter += 1
+                user_number //= 10
+        print(f'Было введено {counter} цифр "{check_number}"')
+    except ValueError:
+        print('Вводите корректные данные')
+        counter_number()
 
 
-def user_input():
-    count_number_user = int(input('Введите количество чисел: '))
-    check_number_user = int(input('Какую цифру считать:  '))
-
-    def count_numbers(count_number, check_number, cnt=1, counter=0):
-        if count_number == 0:
-            return print(f'Было введено {counter} цифр "{check_number}"')
-        user_number = int(input(f'Введите {cnt} число: '))
-        while user_number > 0:
-            if user_number % 10 == check_number:
-                counter += 1
-            user_number //= 10
-        count_number -= 1
-        cnt += 1
-        count_numbers(count_number, check_number, cnt, counter)
-
-    count_numbers(count_number_user, check_number_user)
-
-
-user_input()
+counter_number()
