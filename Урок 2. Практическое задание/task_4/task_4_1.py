929d075d1856a1857a37b40d0595f921994c56de
R F <R@gmail.com>
2020-06-14 14:42:50 +0200

    Lesson 2 - HW task 4 for cycle - done

--- Урок 2. Практическое задание/task_4/task_4_1.py
+++ Урок 2. Практическое задание/task_4/task_4_1.py
@@ -8,3 +8,21 @@
 
 ЗДЕСЬ ДОЛЖНА БЫТЬ РЕАЛИЗАЦИЯ ЧЕРЕЗ ЦИКЛ
 """
+
+
+#Solution 1
+n = int(input('Enter how many elements to sum:'))
+digit = 1
+summary = 0
+
+while n > 0:
+    summary += digit
+    digit /= -2
+    n -= 1
+print(summary)
+
+
+#Solution 2
+# n = int(input('Enter how many elements to sum:'))
+# sum_ = 1 * (1 - (-0.5)**n) / (1 - (-0.5))
+# print(sum_)
