e172ea1958dfb2ebeb8bf10c26f8f95c8771fe4a
R F <R@gmail.com>
2020-06-14 16:25:36 +0200

    Lesson 2 - HW task 8 for cycle

--- Урок 2. Практическое задание/task_8/task_8_1.py
+++ Урок 2. Практическое задание/task_8/task_8_1.py
@@ -12,3 +12,18 @@
 
 ЗДЕСЬ ДОЛЖНА БЫТЬ РЕАЛИЗАЦИЯ ЧЕРЕЗ ЦИКЛ
 """
+
+BASE = 10
+
+num = int(input('How many numbers: '))
+digit = int(input('Which digit need to count?: '))
+counter = 0
+
+for i in range(1, num + 1):
+    ans = int(input(f'Enter number {i}: '))
+    while ans > 0:
+        if ans % BASE == digit:
+            counter += 1
+        ans //= BASE
+
+print(f'Were entered {counter} digits of {digit}')
