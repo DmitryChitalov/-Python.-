054f388b40b33e52764fbdf142f2d2dea559fec5
R F <R@gmail.com>
2020-06-14 16:12:23 +0200

    Lesson 2 - HW 7 tasks 7 with recursion

--- Урок 2. Практическое задание/task_7/task_7_2.py
+++ Урок 2. Практическое задание/task_7/task_7_2.py
@@ -5,3 +5,23 @@
 
 ЗДЕСЬ ДОЛЖНА БЫТЬ РЕАЛИЗАЦИЯ ЧЕРЕЗ РЕКУРСИЮ
 """
+
+# Подсмотренное решение, не засчитывайте его
+
+
+def sum_natural(n):
+    assert n < 999, 'To big number'
+    if n == 1:
+        return n
+    sum_n = n + sum_natural(n -1)
+    return  sum_n
+
+
+n = int(input('Enter any natural number: '))
+
+left = sum_natural(n)
+right = n * (n + 1) // 2
+
+print(f'left {left}')
+print(f'right {right}')
+print('Right' if left == right else 'Wrong')
