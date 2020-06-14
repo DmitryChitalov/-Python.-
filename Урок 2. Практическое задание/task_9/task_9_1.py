23558400fd9edf7683ef37ef45dcfc29b17dcaf7
R F <R@gmail.com>
2020-06-14 16:53:04 +0200

    Lesson 2 - HW task 9 for cycle - not finished tasks

--- Урок 2. Практическое задание/task_9/task_9_1.py
+++ Урок 2. Практическое задание/task_9/task_9_1.py
@@ -10,3 +10,24 @@
 
 ЗДЕСЬ ДОЛЖНА БЫТЬ РЕАЛИЗАЦИЯ ЧЕРЕЗ ЦИКЛ
 """
+
+#Не законченное решение. Не хватило времени
+
+num = int(input('How many numbers: '))
+max_number = 0
+sum_digits = 0
+
+for i in range(1, num + 1):
+    user_number = int(input(f"Enter your {i} number: "))
+    list_digits = list(map(int, str(user_number)))
+    inter_number = 0
+    for _ in list_digits:
+        inter_number += _
+    if inter_number > max_number:
+        max_number = user_number
+        sum_digits = inter_number
+    else:
+        continue
+print(f'The max number is {max_number} and the sum of it digits is {sum_digits}')
+
+
