f28874a5783653df26abe159c7e2cc654de75a00
R F <R@gmail.com>
2020-06-14 16:01:54 +0200

    Lesson 2 - HW task 6 for cycle - second solution

--- Урок 2. Практическое задание/task_6/task_6_1.py
+++ Урок 2. Практическое задание/task_6/task_6_1.py
@@ -8,7 +8,7 @@
 ЗДЕСЬ ДОЛЖНА БЫТЬ РЕАЛИЗАЦИЯ ЧЕРЕЗ ЦИКЛ
 """
 
-
+# Solution 1
 import random
 
 random_digit = random.randint(0, 100)
@@ -18,6 +18,10 @@ while counter > 0:
     users_number = int(input('Please try to guess a number: '))
     if users_number != random_digit:
         print('Wrong number')
+        if users_number > random_digit:
+            print('The digit is less than entered. Try again')
+        else:
+            print('The digit is bigger than entered. Try again')
     elif users_number == random_digit:
         print('You won!')
         break
@@ -26,6 +30,20 @@ print(f'The right number was {random_digit}')
 
 
 
-
+# Solution 2
+# import random
+# num = random.randint(0, 100)
+# print('Guess a number during 10 attempts')
+# for i in range(1,11):
+#     answer = int(input(f'Attempt {i}: '))
+#     if num < answer:
+#         print('The number is less')
+#     elif num > answer:
+#         print('The number is bigger')
+#     else:
+#         print(f'You guessed from {i} attempt! You won!')
+#         break
+# else:
+#     print(f'You failed. it was {num}')
 
 
