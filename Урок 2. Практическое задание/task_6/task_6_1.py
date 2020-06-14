ac09961cf84219997675bfe6b71b21af12ef496d
R F <R@gmail.com>
2020-06-14 15:53:30 +0200

    Lesson 2 - HW task 6 with cycle - done

--- Урок 2. Практическое задание/task_6/task_6_1.py
+++ Урок 2. Практическое задание/task_6/task_6_1.py
@@ -7,3 +7,25 @@
 
 ЗДЕСЬ ДОЛЖНА БЫТЬ РЕАЛИЗАЦИЯ ЧЕРЕЗ ЦИКЛ
 """
+
+
+import random
+
+random_digit = random.randint(0, 100)
+counter = 10
+
+while counter > 0:
+    users_number = int(input('Please try to guess a number: '))
+    if users_number != random_digit:
+        print('Wrong number')
+    elif users_number == random_digit:
+        print('You won!')
+        break
+    counter -= 1
+print(f'The right number was {random_digit}')
+
+
+
+
+
+
