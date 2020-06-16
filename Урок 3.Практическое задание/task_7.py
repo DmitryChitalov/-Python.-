4c4228e922fdeb99b1517f51a518ac9481f6c1ba
leget <lobanov.leget@yandex.ru>
2020-06-16 23:06:16 +0300

    Сделал 7 задание

--- Урок 3.Практическое задание/task_7.py
+++ Урок 3.Практическое задание/task_7.py
@@ -7,3 +7,23 @@
 Наименьший элемент: -86, встречается в этом массиве 1 раз
 Второй наименьший элемент: -73
 """
+from random import randint
+
+a = [randint(-10, 10) for x in range(1, 10)]
+print(a)
+cnt = 0
+min_one = 99
+min_two = 100
+for i in a:
+    if i < min_one:
+        min_one = i
+a.remove(min_one)
+for i in a:
+    if i == min_one:
+        cnt = 2
+    elif i < min_two:
+        min_two = i
+if cnt == 2:
+    print(f'Минимальное число {min_one}, встречается в массиве 2 раза')
+else:
+    print(f'Минимальное число {min_one}, второе минимальное число {min_two}')
