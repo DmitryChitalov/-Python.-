5088ea6494454103fbb76250d0537bd11325cfcb
Тимофей Луценко <moja_pochta@bk.ru>
2020-06-16 17:23:43 +0300

    Сознательно не использовал матрицы запрошенные у пользователя, концентрировался на работе с методами списков и циклами внутри функций.

--- Урок 3.Практическое задание/task_3.py
+++ Урок 3.Практическое задание/task_3.py
@@ -11,3 +11,31 @@
 6 позиции, а минимальное число  -49 стоит на    0 позиции
 [-49, 26, 41, 75, 23, 52, 88, 60, 69, -18]
 """
+def ar_switch(values, args):
+    tmp_arr = args
+    i = 0
+    q = 1
+    for el in values:
+        tmp_arr[tmp_arr.index(values[q])] = values[i]
+        i += 1
+        q -= 1
+    return tmp_arr
+
+
+preset_arr = [
+    10, 2, 45,
+    78, 4, 1024,
+    78, 69, 7
+]
+
+
+min = preset_arr[0]
+max = preset_arr[0]
+for el in preset_arr:
+    if el > max:
+        max = el
+    if el <= min:
+        min = el
+min_max = [min, max]
+
+print(ar_switch(min_max, preset_arr))
