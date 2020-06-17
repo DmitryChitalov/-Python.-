183ce50ce88e9e8e90f46f5a6824827b804824e4
Alex <hlbu@yandex.ru>
2020-06-17 18:04:43 +0300

    Урок\ 3.Практическое\ задание/task_7.py addition

--- Урок 3.Практическое задание/task_7.py
+++ Урок 3.Практическое задание/task_7.py
@@ -7,7 +7,7 @@
 Наименьший элемент: -86, встречается в этом массиве 1 раз
 Второй наименьший элемент: -73
 """
-qwe = [28, -86, 44, -37, -7, -52, -445, -19, -3, -777, -15, -73]
+qwe = [28, 34, -86, -1000, 44, -37, -7, -52, -452, -19, -3, -15, -73]
 print(f'Исходный массив 1: {qwe}')
 mi_two = []
 for i in range(2):
@@ -23,7 +23,6 @@ print(f'Исходный  массив 2: {qwe}')
 min_fir = 0
 min_sec = 1
 
-
 for i in qwe:
     if qwe[min_fir] > i:
         min_sec = min_fir
@@ -31,6 +30,22 @@ for i in qwe:
     elif qwe[min_sec] > i:
         min_sec = qwe.index(i)
 
-
 print(f'Первый Наименьший элемент: {qwe[min_fir]} ,'
       f' Второй наименьший элемент: {qwe[min_sec]} ')
+# 3 в ариант
+qqq = [28, 34, -86, -1000, 44, -37, -7, -52, -452, -19, -3, -15, -73]
+
+fel = qqq[0]
+fii = 0
+sel = qqq[1]
+sii = 1
+aaa = len(qqq)
+for i in range(len(qqq)):
+    if qqq[i] < fel:
+        fel = qqq[i]
+        fii = i
+    if fel < qqq[i] < sel:
+        sel = qqq[i]
+        sii = i
+print(fii, fel)
+print(sii, sel)
