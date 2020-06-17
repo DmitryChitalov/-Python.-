1d20447f97413d7f1a596ccb602b79fcc2467cff
Alex <hlbu@yandex.ru>
2020-06-17 12:35:32 +0300

    Урок 3.Практическое задание_кортежи_списки

--- Урок 3.Практическое задание/task_3.py
+++ Урок 3.Практическое задание/task_3.py
@@ -11,3 +11,22 @@
 6 позиции, а минимальное число  -49 стоит на    0 позиции
 [-49, 26, 41, 75, 23, 52, 88, 60, 69, -18]
 """
+
+qwe = [88, 26, 41, 75, 23, 52, -49, 60, 69, -18]
+print(qwe)
+ma = qwe[5]
+mi = qwe[3]
+for p in qwe:
+    if p > ma:
+        ma = p
+    elif p < mi:
+        mi = p
+
+max_ind = qwe.index(ma)
+min_ind = qwe.index(mi)
+
+qwe[max_ind], qwe[min_ind] = qwe[min_ind], qwe[max_ind]
+print(f'В данном массиве чисел максимальное число  {ma}  стоит на '
+      f'{min_ind}  позиции,'
+      f' а минимальное число {mi}  стоит на  {max_ind}  позиции:  ')
+print(qwe)
--- Урок 3.Практическое задание/task_7.py
+++ Урок 3.Практическое задание/task_7.py
@@ -7,3 +7,30 @@
 Наименьший элемент: -86, встречается в этом массиве 1 раз
 Второй наименьший элемент: -73
 """
+qwe = [28, -86, 44, -37, -7, -52, -445, -19, -3, -777, -15, -73]
+print(f'Исходный массив 1: {qwe}')
+mi_two = []
+for i in range(2):
+    mi_two.append(qwe.pop(qwe.index(min(qwe))))
+
+aaa, zzz = mi_two
+
+print(f'Первый Наименьший элемент: {aaa} ,'
+      f' Второй наименьший элемент: {zzz} ')
+print()
+# 2 вариант # сортировкой
+print(f'Исходный массив 2: {qwe}')
+min_fir = 0
+min_sec = 1
+
+
+for i in qwe:
+    if qwe[min_fir] > i:
+        min_sec = min_fir
+        min_fir = qwe.index(i)
+    elif qwe[min_sec] > i:
+        min_sec = qwe.index(i)
+
+
+print(f'Первый Наименьший элемент: {qwe[min_fir]} ,'
+      f' Второй наименьший элемент: {qwe[min_sec]} ')
