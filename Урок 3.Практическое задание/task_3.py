3bf44bc5c4c63982bf8395d800009764783a1ccc
R F <R@gmail.com>
2020-06-17 15:07:36 +0200

    Lesson_3 - task 3 - done

--- Урок 3.Практическое задание/task_3.py
+++ Урок 3.Практическое задание/task_3.py
@@ -11,3 +11,18 @@
 6 позиции, а минимальное число  -49 стоит на    0 позиции
 [-49, 26, 41, 75, 23, 52, 88, 60, 69, -18]
 """
+initial_list = [88, 26, 41, 75, 23, 52, -49, 60, 69, -18]
+
+in_max = 0
+in_min = 0
+
+for x in range(len(initial_list)):
+    if initial_list[x] < initial_list[in_min]:
+        in_min = x
+    elif initial_list[x] > initial_list[in_max]:
+        in_max = x
+print(f"Minimal number is: {initial_list[in_min]} index was {in_min}")
+print(f"Maximum number is: {initial_list[in_max]} index was {in_max}")
+initial_list[in_max], initial_list[in_min] = initial_list[in_min], initial_list[in_max]
+print(initial_list)
+
