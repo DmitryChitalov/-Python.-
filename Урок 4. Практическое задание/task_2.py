cc7e3294bc235f8b4a6d49ea2b44807da22a228e
Тимофей Луценко <moja_pochta@bk.ru>
2020-06-20 17:31:40 +0300

    Сознательно не использовал матрицы запрошенные у пользователя, концентрировался на работе с методами списков и циклами внутри функций.

--- Урок 4. Практическое задание/task_2.py
+++ Урок 4. Практическое задание/task_2.py
@@ -1,14 +1,54 @@
 """
-Написать два алгоритма нахождения i-го по счёту простого числа.
-Без использования «Решета Эратосфена»;
-Используя алгоритм «Решето Эратосфена»
-
-Подсказка:
-Сравните алгоритмы по времени на разных порядковых номерах чисел:
-10, 100, 1000
-Опишите результаты, сделайте выводы, где и какой алгоритм эффективнее
-Подумайте и по возможности определите сложность каждого алгоритма
-
-ВНИМАНИЕ: ЗАДАНИЯ, В КОТОРЫХ БУДУТ ГОЛЫЕ ЦИФРЫ ЗАМЕРОВ (БЕЗ АНАЛИТИКИ)
-БУДУТ ПРИНИМАТЬСЯ С ОЦЕНКОЙ УДОВЛЕТВОРИТЕЛЬНО
+Скорость работы скрипта не использующий список
+гораздо выше, так как любые операции со списками
+будут тратить время.
 """
+
+import timeit
+
+
+def no_list(num):
+    cutter = 0
+    stop_count = num
+    count_divider = 0
+    i = 0
+    while not cutter == stop_count:
+        i += 1
+        if count_divider == 2:
+            cutter += 1
+            if cutter == stop_count:
+                return el
+        count_divider = 0
+        for el in range(1, i + 1):
+            if i % el == 0 and i >= el:
+                count_divider += 1
+
+
+def generator_func():
+    div_counter = 0
+    base_num = [x for x in range(2, 30)]  # range of index
+    result = []
+    for i in base_num:
+        for y in base_num:
+            if i % y == 0 and i >= y:
+                div_counter += 1
+        if div_counter == 1:
+            result.append(i)
+        div_counter = 0
+    return result[-1]
+
+
+user_num = 10
+user_num_2 = 10
+print(f'{user_num} natural number = {no_list(user_num)}')
+print(timeit.timeit(
+    "no_list",
+    setup="from __main__ import no_list",
+    number=100)
+    )
+print(f'{user_num_2} natural number = {generator_func()}')
+print(timeit.timeit(
+    "generator_func",
+    setup="from __main__ import generator_func",
+    number=100)
+    )
