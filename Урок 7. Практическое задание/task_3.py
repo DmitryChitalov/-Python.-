5d2eb3014ff5a1c8cba9f50d32ad19aa28f93841
leget <lobanov.leget@yandex.ru>
2020-07-01 10:29:46 +0300

    сделал ДЗ

--- Урок 7. Практическое задание/task_3.py
+++ Урок 7. Практическое задание/task_3.py
@@ -6,3 +6,28 @@
 массива. Но если это слишком сложно, то используйте метод сортировки,
 который не рассматривался на уроках
 """
+from random import randint
+
+m = int(input('Введите число m, что бы сфрормировать массив по формуле 2m + 1: '))
+orig_list = [randint(-100, 100) for _ in range(2 * m + 1)]
+print(f'не отсортированный массив: \n {orig_list}')
+
+
+def cocktail_sort(orig_list):
+    left = 0
+    right = len(orig_list) - 1
+    while left <= right:
+        for i in range(left, right):
+            if orig_list[i] > orig_list[i + 1]:
+                orig_list[i], orig_list[i + 1] = orig_list[i + 1], orig_list[i]
+        right -= 1
+        for i in range(right, left, -1):
+            if orig_list[i - 1] > orig_list[i]:
+                orig_list[i], orig_list[i - 1] = orig_list[i - 1], orig_list[i]
+        left += 1
+    return orig_list
+
+
+cocktail_sort(orig_list)
+print(f'отсортированный массив: \n {orig_list}')
+print(f'Медиана заданного массива число {orig_list[m]}')
