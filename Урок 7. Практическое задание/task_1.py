fd0be30e4cb987af2c3cdf6b0bb9dcf1fc067d75
Vladimir <mikulex@mail.ru>
2020-07-01 17:59:15 +0500

    DZ7

--- Урок 7. Практическое задание/task_1.py
+++ Урок 7. Практическое задание/task_1.py
@@ -9,3 +9,48 @@
 Подсказка: обратите внимание, сортируем не по возрастанию, как в примере,
 а по убыванию
 """
+from random import randint
+from timeit import timeit
+
+LST = [randint(-100, 100) for _ in range(30)]
+print(LST)
+
+
+def bubble_1(old_lst, cnt=0):
+    lst = old_lst[:]
+    for _ in range(len(lst) - 1):
+        for i in range(len(lst)):
+            if lst[i] > lst[i - 1] and i > 0:
+                lst[i], lst[i - 1] = lst[i - 1], lst[i]
+    return lst
+
+
+print(timeit("bubble_1(LST)", \
+             setup="from __main__ import bubble_1, LST", number=100))
+print(bubble_1(LST))
+
+
+def bubble_2(old_lst, sort=1):
+    lst = old_lst[:]
+    while sort > 0:
+        sort = 0
+        for i in range(len(lst)):
+            if lst[i] > lst[i - 1] and i > 0:
+                lst[i], lst[i - 1] = lst[i - 1], lst[i]
+                sort += 1
+    return lst
+
+
+print(timeit("bubble_2(LST)", \
+             setup="from __main__ import bubble_2, LST", number=100))
+
+print(bubble_2(LST))
+
+"""
+Доработка скрипта привела к тому что в большинстве случаев скорость выполнения увеличилась
+один из вариантов
+0.015818744000000003 - старый вариант
+0.009746643000000013 - новый
+Я пробовал ставить счетчики на циклы, вышло что в наилучшем случае при списке длиной в 10 элементов
+улучшеный скрипт справлялся за 5 проходов вместо 9. Но чаще это 8 или 7.
+"""
