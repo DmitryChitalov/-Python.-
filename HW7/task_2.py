c0918f8e6e3aeb80ccb5e2c35add330ed51a51ce
Chilly-WIlly-TD <denis-ufs@mail.ru>
2020-07-03 00:30:27 +0200

    ДЗ к 7 Уроку

---
+++ HW7/task_2.py
@@ -0,0 +1,52 @@
+"""
+2. Отсортируйте по возрастанию методом слияния одномерный вещественный массив,
+заданный случайными числами на промежутке [0; 50). Выведите на экран исходный
+и отсортированный массивы.
+Пример:
+Введите число элементов: 5
+Исходный - [46.11436617832828, 41.62921998361278, 18.45859540989644, 12.128870723745806, 8.025098788570562]
+Отсортированный - [8.025098788570562, 12.128870723745806, 18.45859540989644, 41.62921998361278, 46.11436617832828]
+"""
+
+
+import numpy
+import timeit
+
+
+def merge_sort(orig_list):
+    to_be_sorted = orig_list[:]
+    if len(orig_list) > 1:
+        center = len(orig_list) // 2
+        left = orig_list[:center]
+        right = orig_list[center:]
+
+        merge_sort(left)
+        merge_sort(right)
+
+        i = 0
+        j = 0
+        k = 0
+
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                orig_list[k] = left[i]
+                i += 1
+            else:
+                orig_list[k] = right[j]
+                j += 1
+            k += 1
+
+        while i < len(left):
+            orig_list[k] = left[i]
+            i += 1
+            k += 1
+
+        while j < len(right):
+            orig_list[k] = right[j]
+            j += 1
+            k += 1
+        return to_be_sorted
+
+orig_list = [numpy.random.uniform(0.000000000000000, 50.000000000000000) for _ in range(5)]
+print(f"Исходный массив:\n{orig_list}\n")
+print(f"Отсортированный массив:\n{merge_sort(orig_list)}\n")
