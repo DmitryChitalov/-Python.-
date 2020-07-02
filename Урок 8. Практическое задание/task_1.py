686b1b6cd47ff17dc097a925cec1a81ec442fcd2
Subbotinkom <subbotinkom@gmail.com>
2020-07-02 11:32:33 +0300

    task 1 done

--- Урок 8. Практическое задание/task_1.py
+++ Урок 8. Практическое задание/task_1.py
@@ -16,3 +16,24 @@
 
 Итог: 6 подстрок
 """
+import hashlib
+
+s1 = 'qwerty'
+s = s1[:]
+l = []
+n = len(s)
+print(f'Строка: {s1}\nПодстроки:')
+
+while n != 0:
+    for i in range(1, len(s) + 1):
+        if s[0:i] != s1 and s[0:i] not in l:
+            l.append(s[0:i])
+            print(s[0:i])
+    n -= 1
+    s = s[1:]
+
+r = set([hashlib.sha1(el.encode('utf-8')).hexdigest() for el in l])
+print(f'Итог: {len(r)} подстрок')
+print(r)
+
+del l, s1, s, n
