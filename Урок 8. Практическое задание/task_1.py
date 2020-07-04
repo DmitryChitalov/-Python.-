ad24e1bef775497b9fbed091279651ed48471f0c
Тимофей Луценко <moja_pochta@bk.ru>
2020-07-04 12:36:26 +0300

    В целом - все понятно, о затруднениях оставил комментарий в коде.

--- Урок 8. Практическое задание/task_1.py
+++ Урок 8. Практическое задание/task_1.py
@@ -1,18 +1,27 @@
-"""
-Определение количества различных подстрок с использованием хеш-функции.
-Пусть дана строка S длиной N, состоящая только из маленьких латинских букв.
-Требуется найти количество различных подстрок в этой строке.
+import hashlib
 
-Пример:
-строка: рара
 
-подстроки:
-рар
-ра
-р
-а
-ар
-ара
+def substrcounter(preset_string, elements):
+    encoded_string = set()
+    for el in range(elements):
+        if el == 0:
+            elements = len(preset_string) - 1
+        else:
+            elements = len(preset_string)
+        for i in range(elements, el, -1):
+            print(preset_string[el:i])
+        encoded_string.add(
+            hashlib.sha1(
+                preset_string[el:i].encode('utf-8')
+            ).hexdigest())
+    return encoded_string
 
-Итог: 6 подстрок
-"""
+
+preset_string = 'data'
+elements = len(preset_string)
+
+
+print(f'The number of '
+      f'different substrings in string '
+      f'{preset_string} is '
+      f'{len(substrcounter(preset_string, elements))}')
--- Урок 8. Практическое задание/task_2.py
+++ Урок 8. Практическое задание/task_2.py
@@ -1,10 +1,50 @@
+from collections import Counter, deque
+
+
+def treezator(args):
+    uniq_sym = Counter(args)
+    sorted_el = deque(sorted(uniq_sym.items(),
+                             key=lambda item: item[1]))
+    if len(sorted_el) != 1:
+        while len(sorted_el) > 1:
+            weight = sorted_el[0][1] + sorted_el[1][1]
+            combinated_element = {0: sorted_el.popleft()[0],
+                                  1: sorted_el.popleft()[0]}
+            for el, uniq_sym in enumerate(sorted_el):
+                if weight > uniq_sym[1]:
+                    continue
+                else:
+                    sorted_el.insert(el,
+                                     (combinated_element,
+                                      weight))
+                    break
+            else:
+                sorted_el.append(
+                    (combinated_element, weight)
+                )
+    return sorted_el[0][0]
+
+
+encoded_table = dict()
+
+
+def encoder(args, path=' '):
+    if not isinstance(args, dict):
+        encoded_table[args] = path
+    else:
+        encoder(args[0], path=f'{path}0')
+        encoder(args[1], path=f'{path}1')
+
+
+preset_string = 'руку ыщьу вфеф'
+encoder(treezator(preset_string))
+
+
+for el in preset_string:
+    print(encoded_table[el], end=' ')
+
 """
-Закодируйте любую строку из трех слов по алгоритму Хаффмана.
-
-Пример:
-строка для кодирования
-s = "beep boop beer!"
-
-Результат:
-00 11 11 101 010 00 011 011 101 010 00 11 11 1000 1001
+Именно материал этого урока пошел особенно тяжело, в сравнении с остальными.
+Долго ковырял примеры, смог сделать копию, изредка подглядывая. Были не очень понятны
+телодвижения с Path на 35/36 строках.
 """
