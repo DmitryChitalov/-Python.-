72614119875fd72cd36f341a1de53ca972f9e86f
danil chuzhikov <danil.chuzhikov@gmail.com>
2020-07-04 15:53:02 +0300

    hw7

--- Урок 7. Практическое задание/task_1.py
+++ Урок 7. Практическое задание/task_1.py
@@ -9,3 +9,36 @@
 Подсказка: обратите внимание, сортируем не по возрастанию, как в примере,
 а по убыванию
 """
+
+
+from random import randint
+import timeit
+
+
+def bubble(lst):
+    n = 1
+    while n < len(lst):
+        for i in range(len(lst)-n):
+            if lst[i] < lst[i+1]:
+                lst[i], lst[i+1] = lst[i+1], lst[i]
+        n += 1
+    return lst
+
+
+def new_bubble(lst):
+    n = 1
+    sort = 0
+    while n < len(lst):
+        for i in range(len(lst)-n):
+            if lst[i] < lst[i+1]:
+                lst[i], lst[i+1] = lst[i+1], lst[i]
+                sort = 1
+        if sort == 0:
+            break
+        n +=1
+    return lst
+
+lst = [randint(-100, 100) for i in range(100)]
+
+print(timeit.timeit("bubble(lst)", setup="from __main__ import bubble, lst"))
+print(timeit.timeit("new_bubble(lst)", setup="from __main__ import new_bubble, lst"))
--- Урок 7. Практическое задание/task_2.py
+++ Урок 7. Практическое задание/task_2.py
@@ -8,3 +8,50 @@
 Исходный - [46.11436617832828, 41.62921998361278, 18.45859540989644, 12.128870723745806, 8.025098788570562]
 Отсортированный - [8.025098788570562, 12.128870723745806, 18.45859540989644, 41.62921998361278, 46.11436617832828]
 """
+
+import random
+
+
+def merge(lst):
+
+    if len(lst) > 1:
+        mid = len(lst) // 2
+        left_half = lst[:mid]
+        right_half = lst[mid:]
+
+        merge(left_half)
+        merge(right_half)
+
+        i = 0
+        j = 0
+        k = 0
+        while i < len(left_half) and j < len(right_half):
+            if left_half[i] < right_half[j]:
+                lst[k] = left_half[i]
+                i = i + 1
+            else:
+                lst[k] = right_half[j]
+                j = j + 1
+            k = k + 1
+
+        while i < len(left_half):
+            lst[k] = left_half[i]
+            i = i + 1
+            k = k + 1
+
+        while j < len(right_half):
+            lst[k] = right_half[j]
+            j = j + 1
+            k = k + 1
+    return lst
+
+try:
+    n = int(input("Введите число элементов: "))
+except ValueError:
+    print('Ошибка ввода, Введите цифру')
+
+LST = [random.random()*50 for i in range(n)]
+
+print(f"Стартовый массив - {LST}")
+NEW_LST = merge(LST)
+print(f"Исходный массив - {NEW_LST}")
